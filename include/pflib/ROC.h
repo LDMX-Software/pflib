7c46d9df1c9702efd4ac0470f5a4389e4dcbf9bf
Jeremiah Mans <jmmans@umn.edu>
2021-10-10 16:14:35 -0500

    Adding basic ROC block

---
+++ include/pflib/ROC.h
@@ -0,0 +1,28 @@
+#ifndef PFLIB_ROC_H_INCLUDED
+#define PFLIB_ROC_H_INCLUDED
+
+#include "pflib/I2C.h"
+#include <vector>
+
+namespace pflib {
+
+  /**
+   * @class ROC setup
+   *
+   */
+class ROC {
+ public:
+  ROC(I2C& i2c, int ibus=0);
+
+  std::vector<uint8_t> getChannelParameters(int ichan);
+  void setChannelParameters(int ichan, std::vector<uint8_t>& values);
+
+
+ private:
+  I2C& i2c_;
+  int ibus_;
+};      
+  
+}
+
+#endif // PFLIB_ROC_H_INCLUDED
---
+++ src/pflib/ROC.cxx
@@ -0,0 +1,43 @@
+#include "pflib/ROC.h"
+
+namespace pflib {
+
+static const int block_for_chan[] = {261, 260, 259, 258, // 0-3
+  265, 264, 263, 262, // 4-7
+  269, 268, 267, 266, // 8-11
+  273, 272, 271, 270, // 12-15
+  294, 256, 277, 295, // 16-19
+  278, 279, 280, 281, // 20-23
+  282, 283, 284, 285, // 24-27
+  286, 287, 288, 289, // 28-31
+  290, 291, 292, 293, // 32-35
+  5, 4, 3, 2, // 36-39
+  9, 8, 7, 6, // 40-43
+  13, 12, 11, 10, // 44-47
+  17, 16, 15, 14, // 48-51
+  38, 0, 21, 39, // 51-55
+  22, 23, 24, 25, // 56-59
+  26, 27, 28, 29, // 60-63
+  30, 31, 32, 33, // 64-67
+  34, 35, 36, 37}; // 68-71
+
+ROC::ROC(I2C& i2c, int ibus) : i2c_{i2c},ibus_{ibus} {
+}
+
+std::vector<uint8_t> ROC::getChannelParameters(int ichan) {
+  i2c_.set_active_bus(ibus_);
+  i2c_.set_bus_speed(1000);
+
+  std::vector<uint8_t> retval;
+  for (int i=0; i<14; i++) {
+    // set the address
+    uint16_t fulladdr=(block_for_chan[i]<<5)|i;
+    i2c_.write_byte(0,fulladdr&0xFF);
+    i2c_.write_byte(1,(fulladdr>>8)&0xFF);
+    // now read
+    retval.push_back(i2c_.read_byte(2));
+  }
+  return retval;
+}
+
+}
