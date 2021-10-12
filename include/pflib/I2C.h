bde1480da945a1a30989cb78e17abe5a25a78a6e
Jeremiah Mans <jmmans@umn.edu>
2021-10-12 16:18:29 -0500

    Add control of backplane hack

--- include/pflib/I2C.h
+++ include/pflib/I2C.h
@@ -2,6 +2,7 @@
 #define PFLIB_I2C_H_
 
 #include "pflib/WishboneTarget.h"
+#include <vector>
 
 namespace pflib {
 
@@ -47,12 +48,26 @@ class I2C : public WishboneTarget {
    */
   uint8_t read_byte(uint8_t i2c_dev_addr);
 
+  /** 
+   * Carry out a write of zero or more bytes followed a by a read of zero or more bytes.  If either write or read is zero bytes long, it is omitted.  Currently implemented using software (slow), while waiting for better support in the firmware.
+   * @throws pflib::Exception in the case of I2C communication failure
+   */
+  std::vector<uint8_t> general_write_read(uint8_t i2c_dev_addr, const std::vector<uint8_t>& wdata, int nread=0);
+
+
+  /** 
+   * Turn on/off the horrible hack for the V1 backplane
+   */
+  void backplane_hack(bool enable);
+
  private:
   /**
    * microseconds per byte
    */
   int usec_per_byte_;
   
+  /** utility for software-mode I2C */
+  bool i2c_wait(bool nack_ok=false);
   
 };
 
