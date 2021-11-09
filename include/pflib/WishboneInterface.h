d57f762a8c5f07c9f52b2a71cecd60ae880ccd18
Tom Eichlersmith <eichl008@umn.edu>
2021-11-09 10:15:20 -0600

    pure virtual functions are a mess to bind, so we don't

--- include/pflib/WishboneInterface.h
+++ include/pflib/WishboneInterface.h
@@ -10,6 +10,10 @@ namespace pflib {
  * @class WishboneInterface
  * @brief Abstract interface for wishbone transactions, used by ~all classes
  * in pflib.
+ *
+ * Perfectly C++ abstract base classes are very difficult to bind
+ * to python, so we simply define the base class to have nothing
+ * happen in all of these functions.
  */
 class WishboneInterface {
  public:
@@ -17,16 +21,16 @@ class WishboneInterface {
    * write a 32-bit word to the given target and address
    * @throws pflib::Exception in the case of a timeout or wishbone error
    */
-  virtual void wb_write(int target, uint32_t addr, uint32_t data) = 0;
+  virtual void wb_write(int target, uint32_t addr, uint32_t data) {}
   /**
    * read a 32-bit word from the given target and address
    * @throws pflib::Exception in the case of a timeout or wishbone error
    */
-  virtual uint32_t wb_read(int target, uint32_t addr) = 0;
+  virtual uint32_t wb_read(int target, uint32_t addr) { return uint32_t(); }
   /**
    * reset the wishbone bus (on/off cycle)
    */
-  virtual void wb_reset() = 0;
+  virtual void wb_reset() {}
 
   /** 
    * Read the monitoring counters
@@ -34,14 +38,15 @@ class WishboneInterface {
    *  crcdn_errors -- CRC errors from the downlink reported on the uplink (wraps)
    *  wb_errors -- Wishbone errors indicated by the Polarfire
    */
-  virtual void wb_errors(uint32_t& crcup_errors, uint32_t& crcdn_errors, uint32_t& wb_errors) = 0;
+  virtual void wb_errors(uint32_t& crcup_errors, uint32_t& crcdn_errors, uint32_t& wb_errors) {}
 
   /**
    * Clear the monitoring counters
    */
-  virtual void wb_clear_counters() = 0;  
+  virtual void wb_clear_counters() {}
   
 };
+
 }
 
 #endif // PFLIB_WISHBONEINTERFACE_H_
