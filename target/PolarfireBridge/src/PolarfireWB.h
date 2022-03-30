9c30e9cfc8dd50211fd1c6f65638b1f0bc9e1d75
Tom Eichlersmith <eichl008@umn.edu>
2022-03-30 08:47:14 -0500

    break DMAReader into its own file

--- target/PolarfireBridge/src/PolarfireWB.h
+++ target/PolarfireBridge/src/PolarfireWB.h
@@ -1,5 +1,5 @@
-#ifndef POLARFIREWB_H_
-#define POLARFIREWB_H_ 1
+#ifndef POLARFIREWB_H
+#define POLARFIREWB_H
 
 #include <rogue/interfaces/memory/Constants.h>
 #include <rogue/interfaces/memory/Slave.h>
@@ -13,57 +13,33 @@
 #include <rogue/Queue.h>
 
 class PolarfireWB : public rogue::interfaces::memory::Slave {
-public:
-  
-  // Create a static class creator to return our custom class
-  // wrapped with a shared pointer
-  static std::shared_ptr<PolarfireWB> create(uint64_t baseAddress) {
-    static std::shared_ptr<PolarfireWB> ret =
-      std::make_shared<PolarfireWB>(baseAddress);
-    return(ret);
-  }
-  
-  // Standard class creator which is called by create
-  // The baseAddr is used to memory-map the necessary page for communicating with
-  // the Wishbone bridge firmware
+ public:
   PolarfireWB(uint64_t baseAddr);
-
   // map the fast control page
   void map_fc(uint64_t baseAddr_fc);
   // map the DAQ pages
   void map_daq(uint64_t baseAddr_daq);
-  
   // close and cleanup
   virtual ~PolarfireWB();
-  
   // Entry point for incoming transaction
   virtual void doTransaction(rogue::interfaces::memory::TransactionPtr tran);
-
   // stop the interface
   void stop();
-  
-private:
+ private:
   // file descriptor used for mem-map
   int fd_;
   // base pointer for mem-map
   uint32_t* base_;
   uint32_t* base_fc_;
   uint32_t* base_daq_;
-
   // Is the thread running?
   bool threadEn_;
-
   // Logging
   std::shared_ptr<rogue::Logging> log_;
-  
   std::thread* thread_;
   // Queue
   rogue::Queue<std::shared_ptr<rogue::interfaces::memory::Transaction>> queue_;
   void runThread();
-  
-
 };
 
-typedef std::shared_ptr<PolarfireWB> PolarfireWBPtr;
-
-#endif // 
+#endif // POLARFIREWB_H
