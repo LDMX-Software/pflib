f4a28f6d3d94edd6b99fb8071f4d480f34ec7ace
Tom Eichlersmith <eichl008@umn.edu>
2022-04-06 11:16:50 -0500

    reformat, track link_len to skip junk words at end of sample

--- tool/pfdecoder.cc
+++ tool/pfdecoder.cc
@@ -97,10 +97,10 @@ int main(int argc, char* argv[]) {
   enum WordType { wt_unknown, wt_superheader, wt_packetheader, wt_linkheader, wt_data, wt_junk } word_type;
   std::vector<int> packet_pointers;
   std::vector<int> link_pointers;
+  std::vector<int> link_len;
   std::vector<uint32_t> link_data;
   
   for (int i=0; i<int(data.size()); i++) {
-
     word_type=wt_unknown;
     if (data[i]==0x11111111 && data[i+1]==0xbeef2021) { // detect master header
       superpacket_header=i;
@@ -120,38 +120,32 @@ int main(int argc, char* argv[]) {
       word_type=wt_superheader;
       link_data.clear();
       samples=(data[i]>>16)&0xF;
-      if (verbosity>0)
-	printf("%04d S%02d %08x  FPGA=%3d  SAMPLES=%d  Total Length=%d (0x%x)\n",i,rel_super,data[i],(data[i]>>20)&0xFF,samples,(data[i])&0xFFFF,(data[i])&0xFFFF);
+      if (verbosity>0) printf("%04d S%02d %08x  FPGA=%3d  SAMPLES=%d  Total Length=%d (0x%x)\n",
+          i,rel_super,data[i],(data[i]>>20)&0xFF,samples,(data[i])&0xFFFF,(data[i])&0xFFFF);
       packet_pointers.clear();
-      if (fmt==1) 
-        packet_pointers.push_back(i+(samples+1)/2+1);
-      else if (fmt==2) 
-        packet_pointers.push_back(i+8+4+1); // hardcoded for four tagging words
+      if (fmt==1) packet_pointers.push_back(i+(samples+1)/2+1);
+      else if (fmt==2) packet_pointers.push_back(i+8+4+1); // hardcoded for four tagging words
       isample=-1;
       packet_header=-1;
       link_header=-1;
     } else if (rel_super>2 && rel_super<=2+(samples+1)/2) {
       word_type=wt_superheader;
-      if (verbosity>0)
-	printf("%04d S%02d %08x  ",i,rel_super,data[i]);
+      if (verbosity>0) printf("%04d S%02d %08x  ",i,rel_super,data[i]);
       int jsample=(rel_super-3)*2;
       int len=(data[i]&0xFFF);
       
-      if (verbosity>0)
-	printf("Sample %d length=%d (0x%x)",jsample, len, len);
+      if (verbosity>0) printf("Sample %d length=%d (0x%x)",jsample, len, len);
       if (fmt>=2 && (len%2==1)) len++; // 64-bit alignment
       packet_pointers.push_back(packet_pointers.back()+len);
       
       if (samples>jsample+1) {
-	jsample++;
-	len=(data[i]>>16)&0xFFF;
-	if (verbosity>0)
-	  printf("  Sample %d length=%d (0x%x)",jsample,len,len);
+	      jsample++;
+	      len=(data[i]>>16)&0xFFF;
+	      if (verbosity>0) printf("  Sample %d length=%d (0x%x)",jsample,len,len);
         if (fmt>=2 && (len%2==1)) len++; // 64-bit alignment
         packet_pointers.push_back(packet_pointers.back()+len);
       }
-      if (verbosity>0)
-	printf("\n");
+      if (verbosity>0) printf("\n");
     } else if (rel_super>2+(samples+1)/2 && rel_super<=2+8 && fmt==2) {
       word_type=wt_junk; // empty sample words
     } else if (int(packet_pointers.size())>isample+1 && i==packet_pointers[isample+1]) {
@@ -159,54 +153,59 @@ int main(int argc, char* argv[]) {
       isample++;
       word_type=wt_packetheader;
       links=(data[i]>>14)&0x3F;
+      link_len.clear();
       link_pointers.clear();
       link_pointers.push_back(packet_header+2+(links+3)/4);
-      if (verbosity>1)
-	printf("%04d P%02d %08x  FPGA=%3d  LINKS=%d  Length=%d (0x%x)\n",i,0,data[i],(data[i]>>20)&0x3F,links,(data[i])&0xFFF,(data[i])&0xFFF);
+      if (verbosity>1) printf("%04d P%02d %08x  FPGA=%3d  LINKS=%d  Length=%d (0x%x)\n",
+          i,0,data[i],(data[i]>>20)&0x3F,links,(data[i])&0xFFF,(data[i])&0xFFF);
       ilink=-1;
     } else if (fmt==2 && rel_super>2+8 && packet_header<0) { // extended superheader
       int rel_super_tag=rel_super-(2+8+1);
       if (verbosity>1) {
-      if (rel_super_tag==0) printf("%04d S%02d %08x    Spill = %d  BX = %d (0x%x)\n",i,rel_super,data[i],(data[i]>>12)&0xFFF,data[i]&0xFFF, data[i]&0xFFF);
-      else if (rel_super_tag==1) printf("%04d S%02d %08x    Time since start of spill %.5fs (%d 5 Mhz tics)\n",i,rel_super,data[i],data[i]/5e6,data[i]);
-      else if (rel_super_tag==2) printf("%04d S%02d %08x    Evt Number = %d \n",i,rel_super,data[i],data[i]);
-      else if (rel_super_tag==3) printf("%04d S%02d %08x    Run %d start DD-MM hh:mm : %2d-%d %02d:%02d\n",i,rel_super,data[i],(data[i]&0xFFF),(data[i]>>23)&0x1F,(data[i]>>28)&0xF,(data[i]>>18)&0x1F, (data[i]>>12)&0x3F);      
+        if (rel_super_tag==0) printf("%04d S%02d %08x    Spill = %d  BX = %d (0x%x)\n",
+            i,rel_super,data[i],(data[i]>>12)&0xFFF,data[i]&0xFFF, data[i]&0xFFF);
+        else if (rel_super_tag==1) printf("%04d S%02d %08x    "
+            "Time since start of spill %.5fs (%d 5 Mhz tics)\n",
+            i,rel_super,data[i],data[i]/5e6,data[i]);
+        else if (rel_super_tag==2) printf("%04d S%02d %08x    "
+            "Evt Number = %d \n",i,rel_super,data[i],data[i]);
+        else if (rel_super_tag==3) printf("%04d S%02d %08x    "
+            "Run %d start DD-MM hh:mm : %2d-%d %02d:%02d\n",
+            i,rel_super,data[i],(data[i]&0xFFF),(data[i]>>23)&0x1F,
+            (data[i]>>28)&0xF,(data[i]>>18)&0x1F, (data[i]>>12)&0x3F);      
       }
     } else if (isample>=0 && i+1==packet_pointers[isample+1]) { // packet trailer
-      if (verbosity>1)
-	printf("%04d PXX %08x  CRC\n",i,0,data[i]);
+      if (verbosity>1) printf("%04d PXX %08x  CRC\n",i,0,data[i]);
       if (isample+1==samples) {
-	superpacket_header=-10;
-	if (prettyadc) renderADC(link_data);
+	      superpacket_header=-10;
+	      if (prettyadc) renderADC(link_data);
       }
     } else if (packet_header>=0 && i==packet_header+1) { // second packet header word
       word_type=wt_packetheader;
       packet_bx=(data[i]>>20)&0xFFF;
-      if (verbosity>1)
-	printf("%04d P%02d %08x  BX=%3d  RREQ=%d  OR=%d\n",i,rel_packet,data[i],packet_bx,(data[i]>>10)&0x3FF,(data[i])&0x3FF);
+      if (verbosity>1) printf("%04d P%02d %08x  BX=%3d  RREQ=%d  OR=%d\n",
+          i,rel_packet,data[i],packet_bx,(data[i]>>10)&0x3FF,(data[i])&0x3FF);
     } else if (packet_header>0 && i>(packet_header+1) && i<link_pointers[0]) { // link lens
       int ilink=(i-packet_header-2)*4;
       word_type=wt_packetheader;
-      if (verbosity>1)
-	printf("%04d P%02d %08x",i,rel_packet,data[i]);
+      if (verbosity>1) printf("%04d P%02d %08x",i,rel_packet,data[i]);
       for (int j=0; j<4 && ilink+j<links; j++) {
-	int alen=((data[i]>>(j*8))&0x3F);
-	link_pointers.push_back(link_pointers.back()+alen);
-	if (verbosity>1)
-	  printf("  Len[%2d]=%2d",j+ilink,alen);
+	      int alen=((data[i]>>(j*8))&0x3F);
+        link_len.push_back(alen);
+	      link_pointers.push_back(link_pointers.back()+alen);
+	      if (verbosity>1) printf("  Len[%2d]=%2d",j+ilink,alen);
       }
-      if (verbosity>1)
-	printf("\n");
+      if (verbosity>1) printf("\n");
     } else if (links>ilink+1 && i==link_pointers[ilink+1]) { // start of link headers
       word_type=wt_linkheader;
       ilink++;
       link_header=i;
-      if (verbosity>2)
-	printf("%04d L%02d %08x  Link %d ID=0x%03x\n",i,0,data[i],ilink,data[i]>>16);
+      if (verbosity>2) printf("%04d L%02d %08x  Link %d ID=0x%03x\n",i,0,data[i],ilink,data[i]>>16);
+    } else if (rel_link >= link_len[ilink]) {
+      word_type=wt_junk;
     } else if (rel_link==1) {
       word_type=wt_linkheader;
-      if (verbosity>2)
-	printf("%04d L%02d %08x  \n",i,rel_link,data[i]);         
+      if (verbosity>2) printf("%04d L%02d %08x  \n",i,rel_link,data[i]);         
     } else if (rel_link==2) {
       word_type=wt_linkheader;
       bool bad=false;
@@ -224,12 +223,10 @@ int main(int argc, char* argv[]) {
     } else if (link_header>0 && rel_link<41) {
       if (rel_link!=21 && ilink==rocfocus) link_data.push_back(data[i]);
       word_type=wt_data;
-      if (showdata) {
-	printf("%04d L%02d %08x  \n",i,rel_link,data[i]);
-      }
+      if (showdata) printf("%04d L%02d %08x  \n",i,rel_link,data[i]);
     } else {
-      if (verbosity>=10 || (word_type!=wt_junk && (word_type!=wt_data || showdata)))
-	printf("%04d %08x\n",i,data[i]);
+      if (verbosity>=10 || (word_type!=wt_junk && (word_type!=wt_data || showdata))) 
+        printf("%04d %08x\n",i,data[i]);
     }
   }
 
