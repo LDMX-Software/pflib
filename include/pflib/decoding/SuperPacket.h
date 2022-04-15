031f35334a5398155a7fba65c465d45f6cf61067
Jeremiah Mans <jmmans@umn.edu>
2022-04-15 16:08:34 -0500

    Initial coding on merge

--- include/pflib/decoding/SuperPacket.h
+++ include/pflib/decoding/SuperPacket.h
@@ -17,13 +17,24 @@ class SuperPacket {
   int fpgaid() const {if (length_ == 0) return -1; return (data_[0]>>20)&0xFF;}
   int nsamples() const {if (length_ == 0) return -1; return (data_[0]>>16)&0xF;}
   int formatversion() const {if (length_ == 0) return -1; return (data_[0]>>28)&0xF;}
+  int event_tag_length() const;
+  int spill() const;
+  int bxid() const;
+  uint32_t time_in_spill() const;
+  uint32_t eventid() const;
+  int runid() const;
+  void run_start(int& month, int& day, int& hour, int& minute);
+  
   int length32_for_sample(int isample) const;
     
   PolarfirePacket sample(int isample) const;
+
+  int offset_to_header() const { return offset_; }
 private:
   const uint32_t* data_;
   int length_;
   int version_;
+  int offset_;
 };
   
 }
