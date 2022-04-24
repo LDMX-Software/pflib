18df7dff0414a92ba99f9045aa5d18f0f110e3b1
Einar Elén <einar.elen@gmail.com>
2022-04-24 19:13:54 +0200

    More verbose threshold checks

--- tool/pftool_headers.h
+++ tool/pftool_headers.h
@@ -47,10 +47,17 @@ struct HeaderCheckResults {
     bool is_acceptable (const double threshold) {
         for (auto status : res) {
             std::cout << "Testing link " << status.link << "... ";
-            if (status.percent_bad_headers() > threshold ||
-                status.percent_bad_idles() > threshold) {
-                std::cout << "bad!" << std::endl;
-                return false;
+            if (status.percent_bad_headers() > threshold )
+                 {
+                     std::cout << "bad headers!,  "
+                               << status.percent_bad_headers()
+                               << " %" << std::endl;
+                     return false;
+                 } else if (status.percent_bad_idles() > threshold) {
+                     std::cout << "bad idles!,  "
+                               << status.percent_bad_idles()
+                               << " %" << std::endl;
+                     return false;
             }
             std::cout << " ok!" << std::endl;
         }
