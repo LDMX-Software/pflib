aedf9542a5d60fb5d76cb4757dd973eefca49a3a
Einar Elén <einar.elen@gmail.com>
2022-04-24 17:39:10 +0200

    Add a struct representing the status of one link

--- tool/pftool_headers.h
+++ tool/pftool_headers.h
@@ -5,5 +5,13 @@
 #include "pflib/decoding/SuperPacket.h"
 #include <iostream>
 #include <vector>
+struct HeaderStatus {
+    public:
+        int n_good_bxheaders = 0;
+        int n_bad_bxheaders = 0;
+        int n_good_idles = 0;
+        int n_bad_idles = 0;
+        int link;
+};
 
 #endif /* PFTOOL_HEADERS_H */
