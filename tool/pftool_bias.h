8de306ea98772d39b91bfa47e405fd43c2edd04a
Einar Elén <einar.elen@gmail.com>
2022-05-30 15:07:23 +0200

    Factor out setting bias on all  16 connectors at once

--- tool/pftool_bias.h
+++ tool/pftool_bias.h
@@ -21,6 +21,10 @@
  */
 void bias( const std::string& cmd, pflib::PolarfireTarget* pft );
 
+void set_bias_on_all_connectors(pflib::PolarfireTarget* pft,
+                                const int board,
+                                const bool set_led,
+                                const int dac_value);
 void set_bias_on_all_active_boards(pflib::PolarfireTarget* pft);
 void set_bias_on_all_active_boards(pflib::PolarfireTarget* pft,
                                    const bool set_led,
