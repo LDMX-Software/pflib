6e19a65dde8662b3b8c98b8d9dd535fbb663836d
Einar Elén <einar.elen@gmail.com>
2022-05-30 14:42:05 +0200

    Add a function that sets bias on each board with at least one active link

--- tool/pftool_bias.cc
+++ tool/pftool_bias.cc
@@ -16,7 +16,20 @@ void initialize_bias(PolarfireTarget* pft,
     pflib::Bias bias=pft->hcal.bias(iboard);
     bias.initialize();
 }
-
+void set_bias_on_all_active_boards(PolarfireTarget* pft,
+                                   const bool set_led,
+                                   const int dac_value)
+{
+  std::vector<int> active_boards{get_rocs_with_active_links(pft)};
+  for (const auto board : active_boards) {
+    const int num_connections = 16;
+    const int led_bias{0};
+    initialize_bias(pft, board);
+    for (int connection{0}; connection < num_connections; ++connection) {
+      pft->setBiasSetting(board, set_led, connection, dac_value);
+    }
+  }
+}
 void set_bias_on_all_connectors(PolarfireTarget* pft,
                                 const int num_boards,
                                 const bool set_led,
