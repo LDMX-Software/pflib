692fd60b64acc3af2720d1f690176497ede9a0ae
tomeichlersmith <eichl008@umn.edu>
2025-03-19 13:15:19 -0500

    add some more testing of yaml loading

--- test/compile.cxx
+++ test/compile.cxx
@@ -1,6 +1,8 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
+#include <fstream>
+
 #include "pflib/Compile.h"
 #include "pflib/Exception.h"
 
@@ -51,6 +53,59 @@ BOOST_AUTO_TEST_CASE( one_param_multi_registers ) {
   expected[89][26] = 0b00001101;
   BOOST_CHECK_MESSAGE( registers == expected ,
       "silently trim parameter if larger than number of bits available");
+}
+
+BOOST_AUTO_TEST_CASE( glob_pages ) {
+  pflib::Compiler c = pflib::Compiler::get("sipm_rocv3");
+  std::string test_filepath{"settings.yaml"};
+
+  {
+    std::ofstream test_yaml{"settings.yaml"};
+    test_yaml
+      << "ch_*:\n"
+      << "    channel_off: 1\n"
+      << std::endl;
+  }
+
+  std::map<std::string,std::map<std::string,int>> settings, expected;
+  
+  for (int ch{0}; ch < 72; ch++) {
+    expected["CH_"+std::to_string(ch)]["CHANNEL_OFF"] = 1;
+  }
+  c.extract({test_filepath}, settings);
+  BOOST_CHECK_MESSAGE( settings == expected,
+      "broadcast single parameter to all channel pages" );
+}
+
+BOOST_AUTO_TEST_CASE( overwrite_with_later_settings ) {
+  pflib::Compiler c = pflib::Compiler::get("sipm_rocv3");
+  std::string test_filepath{"settings.yaml"},
+              test_overwrite{"overwrite.yaml"};
+
+  {
+    std::ofstream test_yaml{"settings.yaml"};
+    test_yaml
+      << "ch_*:\n"
+      << "    channel_off: 1\n"
+      << std::endl;
+  }
+
+  {
+    std::ofstream test_yaml{"overwrite.yaml"};
+    test_yaml
+      << "ch_42:\n"
+      << "    channel_off: 0\n"
+      << std::endl;
+  }
+
+  std::map<std::string,std::map<std::string,int>> settings, expected;
+  
+  for (int ch{0}; ch < 72; ch++) {
+    expected["CH_"+std::to_string(ch)]["CHANNEL_OFF"] = (ch == 42) ? 0 : 1;
+  }
+  c.extract({test_filepath, test_overwrite}, settings);
+  BOOST_CHECK_MESSAGE( settings == expected,
+      "overwrite parameter with subsequent settings file" );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
