4d953cc72eec3ba41323de07f58a5c40fcf42106
Jeremiah Mans <jmmans@umn.edu>
2025-04-14 11:13:21 -0500

    Add utility functions to handle CSV files

---
+++ include/pflib/lpgbt/lpGBT_Utility.h
@@ -0,0 +1,33 @@
+#ifndef pflib_lpGBT_Utility_h_included
+#define pflib_lpGBT_Utility_h_included
+
+#include <string>
+#include <stdint.h>
+
+#include "pflib/lpGBT.h"
+
+namespace pflib {
+namespace lpgbt {
+
+/** Load a CSV file containing register names or addresses and values.
+ * Converts into a RegisterValueVector, which can use only full
+ * registers, not partial registers.
+ * Expects two columns, register and value.
+ * Ignores hash signs and anything following on the same line.
+ */
+lpGBT::RegisterValueVector loadRegisterCSV(const std::string& filename);
+
+/** Apply a CSV containing register names or addresses and values.
+ *  Requires an lpGBT object to operate on, can use partial registers.
+ * Expects two columns, register and value.
+ * Ignores hash signs and anything following on the same line.
+ */
+void applylpGBTCSV(const std::string& filename, lpGBT& lpgbt);
+
+
+}
+}
+
+
+
+#endif // pflib_lpGBT_Utility_h_included
---
+++ src/pflib/lpgbt/lpGBT_Utility.cxx
@@ -0,0 +1,104 @@
+#include "pflib/lpgbt/lpGBT_Utility.h"
+#include "pflib/lpgbt/lpGBT_Registers.h"
+#include "pflib/Compile.h"
+#include <stdio.h>
+#include <string.h>
+
+namespace pflib {
+namespace lpgbt {
+
+typedef std::vector<std::pair<std::string, uint8_t> > CSVDecodedLines;
+
+static CSVDecodedLines decode_file(const std::string& fname) {
+  CSVDecodedLines lines;
+  char buffer[1024];
+  
+  FILE* f=fopen(fname.c_str(),"r");
+  if (f==0) {
+    snprintf(buffer,1024,"Unable to open CSV file '%s'",fname.c_str());
+    PFEXCEPTION_RAISE("FileOpenException",buffer);
+  }
+  const char* delimiters=",;\t";
+  while (!feof(f)) {
+    buffer[0]=0;
+    fgets(buffer,1000,f);
+    
+    // truncate after any hash character
+    char* hashpoint=strchr(buffer,'#');
+    if (hashpoint) *hashpoint=0;
+
+    std::string reg;
+    uint8_t val;
+    char* token;
+    char* ptr=buffer;
+    for (int ifield=0; (token = strsep(&ptr,delimiters)); ifield++) {
+      if (ifield==0) {
+        for (int i=0; token[i]!=0; i++)
+          if (!isspace(token[i])) reg+=token[i];
+      } else if (ifield==1) {
+        std::string sfield(token);
+        val=str_to_int(sfield);
+        lines.push_back(std::make_pair(reg,val));
+      }
+    }
+    
+    
+  }
+  fclose(f);
+  return lines;
+}
+
+lpGBT::RegisterValueVector loadRegisterCSV(const std::string& filename) {
+  char mesg[120];
+  CSVDecodedLines lines=decode_file(filename);
+  lpGBT::RegisterValueVector retval;
+  for (auto line : lines) {
+    // see if this is a valid register...
+    uint16_t addr;
+    if (line.first.find("0x")==0 || line.first.find("0b")==0 || line.first.find_first_not_of("0123456789")==std::string::npos) {
+      addr=str_to_int(line.first);
+    } else {
+      uint8_t mask, offset;
+      if (!findRegister(line.first,addr,mask,offset)) {
+        snprintf(mesg,120,"Unknown register '%s'",line.first.c_str());
+        PFEXCEPTION_RAISE("UnknownRegisterException",mesg);
+      }
+      if (mask!=0xFF) {
+        snprintf(mesg,120,"Invalid register for loadRegisterCSV, must be full register '%s'",line.first.c_str());
+        PFEXCEPTION_RAISE("InvalidRegisterException",mesg);
+      }
+    }
+    retval.push_back(std::make_pair(addr,line.second));
+  }
+  return retval;
+}
+
+void applylpGBTCSV(const std::string& filename, lpGBT& lpgbt) {
+  char mesg[120];
+  CSVDecodedLines lines=decode_file(filename);
+  lpGBT::RegisterValueVector retval;
+  for (auto line : lines) {
+    // see if this is a valid register...
+    uint16_t addr;
+    uint8_t mask=0xff, offset=0;
+    if (line.first.find("0x")==0 || line.first.find("0b")==0 || line.first.find_first_not_of("0123456789")==std::string::npos) {
+      addr=str_to_int(line.first);
+    } else {
+      if (!findRegister(line.first,addr,mask,offset)) {
+        snprintf(mesg,120,"Unknown register '%s'",line.first.c_str());
+        PFEXCEPTION_RAISE("UnknownRegisterException",mesg);
+      }
+    }
+    if (mask==0xff) {
+      lpgbt.write(addr,line.second);
+    } else {
+      uint8_t val=lpgbt.read(addr);
+      uint8_t antimask=0xFF^mask;
+      val=(val&antimask)|((line.second<<offset)&mask);
+      lpgbt.write(addr,val);
+    }
+  }
+}
+
+}
+}
