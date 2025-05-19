da729fe2b976ee760c8e75e57d6b6b0d31833494
tomeichlersmith <eichl008@umn.edu>
2025-05-19 14:37:24 +0000

    move version header production to compile time
    hopefully makes it automatic

---
+++ GenerateVersionHeader.cmake
@@ -0,0 +1,30 @@
+# CMake script to generate version and insert it into source code via configure_file
+# original: https://github.com/nocnokneo/cmake-git-versioning-example
+if(GIT_EXECUTABLE)
+  get_filename_component(SRC_DIR ${SRC} DIRECTORY)
+  # Generate a git-describe version string from Git repository tags
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match "v*"
+    WORKING_DIRECTORY ${SRC_DIR}
+    OUTPUT_VARIABLE GIT_DESCRIBE_VERSION
+    RESULT_VARIABLE GIT_DESCRIBE_ERROR_CODE
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  if(NOT GIT_DESCRIBE_ERROR_CODE)
+    string(REGEX REPLACE "\n$" "" GIT_DESCRIBE_VERSION ${GIT_DESCRIBE_VERSION})
+    set(PFLIB_VERSION ${GIT_DESCRIBE_VERSION})
+    string(REGEX REPLACE "^v" "" PFLIB_VERSION ${PFLIB_VERSION})
+    string(REGEX REPLACE "-.*$" "" PFLIB_VERSION ${PFLIB_VERSION})
+  endif()
+endif()
+
+# Final fallback: Just use a bogus version string that is semantically older
+# than anything else and spit out a warning to the developer.
+if(NOT DEFINED PFLIB_VERSION)
+  set(PFLIB_VERSION v0.0.0-unknown)
+  message(WARNING "Failed to determine PFLIB_VERSION from Git tags. Using default version \"${PFLIB_VERSION}\".")
+endif()
+
+message(STATUS "Deduced pflib version ${PFLIB_VERSION} (${GIT_DESCRIBE_VERSION})")
+
+configure_file(${SRC} ${DST} @ONLY)
