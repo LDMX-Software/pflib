07581f9c7f803ba77fa2abf312a4b29f0cd86ba3
tomeichlersmith <eichl008@umn.edu>
2025-06-02 19:55:40 +0000

    add accessor to retrieve channel sample by roc number

--- include/pflib/packing/SingleROCEventPacket.h
+++ include/pflib/packing/SingleROCEventPacket.h
@@ -48,6 +48,16 @@ class SingleROCEventPacket {
    * @param[in,out] f file to write CSV to
    */
   void to_csv(std::ofstream& f) const;
+  /**
+   * Get a specific Sample from a channel
+   *
+   * The channel index input here is relative to the ROC
+   * [0,72). If you have the channel index within a daq
+   * link (or ROC "half"), just access the daq links directly
+   * ```cpp
+   * ep.daq_links[i_link]
+   */
+  Sample channel(int ch) const;
   /// default constructor that does nothing
   SingleROCEventPacket() = default;
 };
