4a6f7596ac3ab71adbe6a9423da8cbfa8766dec5
tomeichlersmith <eichl008@umn.edu>
2025-06-10 14:12:26 -0500

    put register/detailed interaction into expert submenu

--- app/tool/roc.cxx
+++ app/tool/roc.cxx
@@ -1,3 +1,7 @@
+/**
+ * @file roc.cxx
+ * ROC menu commands and support functions
+ */
 #include "tool.h"
 
 /**
@@ -11,6 +15,76 @@ static void roc_render(Target* pft) {
 }
 
 /**
+ * ROC.EXPERT menu commands
+ *
+ * Detailed interaction with ROC doing things like interacting
+ * with the registers that hold the HGCROC parameters without
+ * using the compiler
+ *
+ * ## Commands
+ * - PAGE : print a page of raw registers pflib::ROC::readPage
+ * - POKE : set a specific register to a specific value pflib::ROC::setValue
+ * - LOAD : load a CSV of register values onto chip pflib::ROC::loadRegisters
+ * - DUMP : dump HGCROC registers to CSV pflib::ROC::dumpSettings with decompile=false
+ * - DIRECT_ACCESS_PARAMETERS : prints all direct access parameters in the ROC
+ * - GET_DIRECT_ACCESS : print direct access parameters and their values
+ * - SET_DIRECT_ACCESS : set direct access parameter to specific value
+ */
+static void roc_expert(const std::string& cmd, Target* tgt) {
+  auto roc = tgt->hcal().roc(pftool::state.iroc, pftool::state.type_version);
+  if (cmd == "PAGE") {
+    int page = pftool::readline_int("Which page? ", 0);
+    int len = pftool::readline_int("Length?", 8);
+    std::vector<uint8_t> v = roc.readPage(page, len);
+    for (int i = 0; i < int(v.size()); i++) printf("%02d : %02x\n", i, v[i]);
+  }
+  if (cmd == "POKE") {
+    int page = pftool::readline_int("Which page? ", 0);
+    int entry = pftool::readline_int("Offset: ", 0);
+    int value = pftool::readline_int("New value: ");
+
+    roc.setValue(page, entry, value);
+  }
+  if (cmd == "LOAD") {
+    std::cout << "\n"
+                 " --- This command expects a CSV file with columns "
+                 "[page,offset,value].\n"
+              << std::flush;
+    std::string fname = pftool::readline("Filename: ");
+    roc.loadRegisters(fname);
+  }
+  if (cmd == "DUMP") {
+    std::string fname = pftool::readline_path("hgcroc_"+std::to_string(pftool::state.iroc)+"_settings", ".csv");
+    roc.dumpSettings(fname, false);
+  }
+  if (cmd == "DIRECT_ACCESS_PARAMETERS") {
+    for (const auto& name : roc.getDirectAccessParameters()) {
+      std::cout << "  " << name << "\n";
+    }
+    std::cout << std::flush;
+  }
+  if (cmd == "GET_DIRECT_ACCESS") {
+    auto options = roc.getDirectAccessParameters();
+    auto name =
+        pftool::readline("Direct Access Parameter to Read: ", options, "all");
+    if (name == "all") {
+      for (auto name : roc.getDirectAccessParameters()) {
+        printf("  %10s = %d\n", name.c_str(), roc.getDirectAccess(name));
+      }
+    } else {
+      printf("  %10s = %d\n", name.c_str(), roc.getDirectAccess(name));
+    }
+  }
+  if (cmd == "SET_DIRECT_ACCESS") {
+    auto options = roc.getDirectAccessParameters();
+    // TODO filter out the readonly DA parameters on register 7
+    auto name = pftool::readline("Direct Access Parameter to Set: ", options);
+    bool val = pftool::readline_bool("On/Off: ", true);
+    roc.setDirectAccess(name, val);
+  }
+}
+
+/**
  * ROC menu commands
  *
  * When necessary, the ROC interaction object pflib::ROC is created
@@ -19,16 +93,13 @@ static void roc_render(Target* pft) {
  * ## Commands
  * - HARDRESET : pflib::Hcal::hardResetROCs
  * - SOFTRESET : pflib::Hcal::softResetROC
- * - RESYNCLOAD : pflib::Hcal::resyncLoadROC
- * - RUNMODE :
+ * - RUNMODE : enable run mode on the ROC
  * - IROC : Change which ROC to focus on
  * - PAGE : pflib::ROC::readPage
  * - PARAM_NAMES : Use pflib::parameters to get list ROC parameter names
- * - POKE_REG : pflib::ROC::setValue
- * - POKE_PARAM : pflib::ROC::applyParameter
- * - LOAD_REG : pflib::Target::loadROCRegisters
- * - LOAD_PARAM : pflib::Target::loadROCParameters
- * - DUMP : pflib::Target::dumpSettings
+ * - POKE : pflib::ROC::setValue
+ * - LOAD : pflib::ROC::loadParameters
+ * - DUMP : pflib::ROC::dumpSettings with decompile=true
  *
  * @param[in] cmd ROC command
  * @param[in] pft active target
@@ -78,31 +149,6 @@ static void roc(const std::string& cmd, Target* pft) {
     isRunMode = pftool::readline_bool("Set ROC runmode: ", isRunMode);
     roc.setRunMode(isRunMode);
   }
-  if (cmd == "DIRECT_ACCESS_PARAMETERS") {
-    for (const auto& name : roc.getDirectAccessParameters()) {
-      std::cout << "  " << name << "\n";
-    }
-    std::cout << std::flush;
-  }
-  if (cmd == "GET_DIRECT_ACCESS") {
-    auto options = roc.getDirectAccessParameters();
-    auto name =
-        pftool::readline("Direct Access Parameter to Read: ", options, "all");
-    if (name == "all") {
-      for (auto name : roc.getDirectAccessParameters()) {
-        printf("  %10s = %d\n", name.c_str(), roc.getDirectAccess(name));
-      }
-    } else {
-      printf("  %10s = %d\n", name.c_str(), roc.getDirectAccess(name));
-    }
-  }
-  if (cmd == "SET_DIRECT_ACCESS") {
-    auto options = roc.getDirectAccessParameters();
-    // TODO filter out the readonly DA parameters on register 7
-    auto name = pftool::readline("Direct Access Parameter to Set: ", options);
-    bool val = pftool::readline_bool("On/Off: ", true);
-    roc.setDirectAccess(name, val);
-  }
   if (cmd == "PAGE") {
     int page = pftool::readline_int("Which page? ", 0);
     int len = pftool::readline_int("Length?", 8);
@@ -125,14 +171,7 @@ static void roc(const std::string& cmd, Target* pft) {
     }
     std::cout << std::endl;
   }
-  if (cmd == "POKE_REG") {
-    int page = pftool::readline_int("Which page? ", 0);
-    int entry = pftool::readline_int("Offset: ", 0);
-    int value = pftool::readline_int("New value: ");
-
-    roc.setValue(page, entry, value);
-  }
-  if (cmd == "POKE" || cmd == "POKE_PARAM") {
+  if (cmd == "POKE") {
     std::string page = pftool::readline("Page: ", page_names);
     if (param_names.find(page) == param_names.end()) {
       PFEXCEPTION_RAISE("BadPage", "Page name " + page + " not recognized.");
@@ -142,15 +181,7 @@ static void roc(const std::string& cmd, Target* pft) {
 
     roc.applyParameter(page, param, val);
   }
-  if (cmd == "LOAD_REG") {
-    std::cout << "\n"
-                 " --- This command expects a CSV file with columns "
-                 "[page,offset,value].\n"
-              << std::flush;
-    std::string fname = pftool::readline("Filename: ");
-    roc.loadRegisters(fname);
-  }
-  if (cmd == "LOAD" || cmd == "LOAD_PARAM") {
+  if (cmd == "LOAD") {
     std::cout << "\n"
                  " --- This command expects a YAML file with page names, "
                  "parameter names and their values.\n"
@@ -163,15 +194,8 @@ static void roc(const std::string& cmd, Target* pft) {
     roc.loadParameters(fname, prepend_defaults);
   }
   if (cmd == "DUMP") {
-    std::string fname = pftool::readline_path("hgcroc_"+std::to_string(pftool::state.iroc)+"_settings");
-    bool decompile =
-        pftool::readline_bool("Decompile register values? ", true);
-    if (decompile) {
-      fname += ".yaml";
-    } else {
-      fname += ".csv";
-    }
-    roc.dumpSettings(fname, decompile);
+    std::string fname = pftool::readline_path("hgcroc_"+std::to_string(pftool::state.iroc)+"_settings", ".yaml");
+    roc.dumpSettings(fname, true);
   }
 }
 
@@ -181,30 +205,28 @@ auto menu_roc =
     pftool::menu("ROC", "Read-Out Chip Configuration", roc_render)
         ->line("HARDRESET", "Hard reset to all rocs", roc)
         ->line("SOFTRESET", "Soft reset to all rocs", roc)
-        //->line("RESYNCLOAD","ResyncLoad to specified roc to help maintain link
-        //stability", roc)
-        ->line("DIRECT_ACCESS_PARAMETERS",
-               "list the direct access parameters we know about", roc)
-        ->line("GET_DIRECT_ACCESS", "print a direct access parameter", roc)
-        ->line("SET_DIRECT_ACCESS", "set a direct access parameter", roc)
         ->line("IROC", "Change the active ROC number", roc)
         ->line("RUNMODE", "set/clear the run mode", roc)
-        ->line("CHAN", "Dump link status", roc)
-        ->line("PAGE", "Dump a page", roc)
+        ->line("PAGE", "a page of the parameters on the chip", roc)
         ->line("PARAM_NAMES", "Print a list of parameters on a page", roc)
-        ->line("POKE_REG", "Change a single register value", roc)
-        ->line("POKE_PARAM", "Change a single parameter value", roc)
-        ->line("POKE", "Alias for POKE_PARAM", roc)
-        //->line("POKE_ALL_ROCHALVES", "Like POKE_PARAM, but applies parameter
-        //to both halves of all ROCs", roc)
-        //->line("POKE_ALL_CHANNELS", "Like POKE_PARAM, but applies parameter to
-        //all channels of the all ROCs", roc)
-        ->line("LOAD_REG", "Load register values onto the chip from a CSV file",
-               roc)
-        ->line("LOAD_PARAM",
-               "Load parameter values onto the chip from a YAML file", roc)
+        ->line("POKE", "change a single parameter value", roc)
+        ->line("LOAD", "Load parameter values onto the chip from a YAML file", roc)
         ->line("LOAD", "Alias for LOAD_PARAM", roc)
         ->line("DUMP", "Dump hgcroc settings to a file", roc)
-    //->line("DEFAULT_PARAMS", "Load default YAML files", roc)
+    ;
+
+auto menu_roc_expert =
+    menu_roc->submenu("EXPERT", "expert interaction with ROC", [](Target* tgt) {
+          std::cout
+            << "This menu avoids using the 'compiler' to translate parameter names into\n"
+               "register values and instead allows you to read/write registers directly.\n";
+        })
+        ->line("PAGE", "view a page of register values", roc_expert)
+        ->line("POKE", "change a single register's value", roc_expert)
+        ->line("LOAD", "load many register values from a CSV", roc_expert)
+        ->line("DUMP", "write out all the register values into a CSV", roc_expert)
+        ->line("DIRECT_ACCESS_PARAMETERS", "print out the names of the direct access parameters", roc_expert)
+        ->line("GET_DIRECT_ACCESS", "print out values of direct access parameters", roc_expert)
+        ->line("SET_DIRECT_ACCESS", "set direct access parameter bits", roc_expert)
     ;
 }
