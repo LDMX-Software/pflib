d775af090b331a0ef705273891c7c473edf0c0a3
tomeichlersmith <eichl008@umn.edu>
2025-06-16 10:39:34 -0500

    mock up example decode and write run summary
    storing table of channel Samples from the entire run, allowing user to
    decide how to write the row and header

--- include/pflib/DecodeAndWrite.h
+++ include/pflib/DecodeAndWrite.h
@@ -20,6 +20,7 @@ namespace pflib {
 class DecodeAndWrite : public Target::DAQRunConsumer {
  public:
   virtual ~DecodeAndWrite() = default;
+ 
   /**
    * Decode the input event packet into our pflib::packing::SingleROCEventPacket
    * and then call write_event on it.
@@ -56,6 +57,43 @@ class DecodeAndWriteToCSV : public DecodeAndWrite {
   virtual void write_event(const pflib::packing::SingleROCEventPacket& ep) final;
 };
 
+/**
+ * Write all channels into a CSV
+ */
 DecodeAndWriteToCSV all_channels_to_csv(const std::string& file_name);
+
+/**
+ * Specialization of DecodeAndWrite which holds a table of all samples
+ * and then allows the user decide how to write this table at the end of
+ * the run.
+ *
+ * If you want to write out all the samples anyways, it is more efficient
+ * to use DecodeAndWriteToCSV. This is helpful for if you want to condense
+ * the samples with some summary statistics (e.g. median or mean).
+ */
+class DecodeAndWriteRunSummary : public DecodeAndWrite {
+  /// output file writing to
+  std::ofstream file_;
+  /// function that writes the rows given the table of samples from the last run
+  std::function<void(std::ofstream&, const std::array<std::vector<pflib::packing::Sample>, 72>&)> write_row_;
+  /// table of samples we accrue with data
+  std::array<std::vector<pflib::packing::Sample>, 72> data_;
+  /// index of current event in table
+  std::size_t i_event_;
+ public:
+  DecodeAndWriteRunSummary(
+      const std::string& file_name,
+      int n_events,
+      std::function<void(std::ofstream&)> write_header,
+      std::function<void(std::ofstream&, const std::array<std::vector<pflib::packing::Sample>, 72>&)> write_row
+  );
+  virtual ~DecodeAndWriteRunSummary() = default;
+  /// reset table of samples
+  virtual void start_run() final;
+  /// add event into our table of samples
+  virtual void write_event(const pflib::packing::SingleROCEventPacket& ep) final;
+  /// call write_row on the table of samples
+  virtual void end_run() final;
+};
 
 }
--- src/pflib/DecodeAndWrite.cxx
+++ src/pflib/DecodeAndWrite.cxx
@@ -47,4 +47,41 @@ DecodeAndWriteToCSV all_channels_to_csv(const std::string& file_name) {
   );
 }
 
+DecodeAndWriteRunSummary::DecodeAndWriteRunSummary(
+    const std::string& file_name,
+    int n_events,
+    std::function<void(std::ofstream&)> write_header,
+    std::function<void(std::ofstream&, const std::array<std::vector<pflib::packing::Sample>, 72>&)> write_row
+) : DecodeAndWrite(), file_{file_name}, write_row_{write_row}, data_{}, i_event_{0} {
+  if (not file_) {
+    PFEXCEPTION_RAISE("FileOpen", "unable to open "+file_name+" for writing");
+  }
+  write_header(file_);
+
+  // allocate table of samples
+  for (auto& ch: data_) {
+    ch.resize(n_events);
+  }
+}
+
+void DecodeAndWriteRunSummary::start_run() {
+  i_event_ = 0;
+  for (auto& ch: data_) {
+    for (auto& sample: ch) {
+      sample.word = 0;
+    }
+  }
+}
+
+void DecodeAndWriteRunSummary::write_event(const pflib::packing::SingleROCEventPacket& ep) {
+  for (int i_ch{0}; i_ch < data_.size(); i_ch++) {
+    data_[i_ch][i_event_] = ep.channel(i_ch);
+  }
+  i_event_++;
+}
+
+void end_run() {
+  write_row_(file_, data_);
+}
+
 }
