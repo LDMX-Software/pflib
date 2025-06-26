dec4629ec87f3cf20fabfb92d4f074141974ed48
Andreas Pettersson <andreas.pettersson.6826@student.lu.se>
2025-06-26 15:03:44 +0200

    fixed some errors

--- ana/charge/parameter-time-scan.py
+++ ana/charge/parameter-time-scan.py
@@ -57,7 +57,6 @@ def plt_gen(
     #Run the specified plot
     ax = plt.gca()
     plotting_func(samples, ax)
-
     plt.savefig(args.output, bbox_inches='tight')
     plt.clf()
 
@@ -69,135 +68,136 @@ def get_params(
         if column.find('.') != -1:
             parameter_names.append(column)
     groups = samples.groupby(parameter_names[0])
-    return groups
-
-###################################################
+    return groups, parameter_names[0]
 
 
 """
     Plot the requested parameter vs time.
+
+    Takes input csv from tasks.parameter_timescan
 """
 
 def plt_time(
     samples,
     ax
 ):
-    groups = get_params(samples)
+    groups, param_name = get_params(samples)
     cmap = plt.get_cmap('viridis')
-    if args.plot_type == 'SCATTER':
+    n = len(groups)
     for i, (group_id, group_df) in enumerate(groups):
-        val = group_df[parameter_names[0]].iloc[0]
+        val = group_df[param_name].iloc[0]
         color = cmap(i/n)
         plt.scatter(group_df['time'], group_df['adc'], label=f'CALIB = {val}', s=5, color=color)
         plt.legend()
 
-if args.plot_function = 'TIME' and args.plot_type = 'SCATTER':
+if args.plot_function == 'TIME' and args.plot_type == 'SCATTER':
     plt_gen(plt_time, samples)
 
 """
     Plot the ADC vs a requested parameter
+
+    Takes input csv from tasks.parameter_timescan
 """
 
 def plt_param(
     samples,
     ax
 ):
-    groups = get_params(samples)
+    groups, param_name = get_params(samples)
     x = []
     y = []
     for _, group_df in groups:
         y.append(group_df['adc'].max())
-        val = group_df[parameter_names[0]].iloc[0]
+        val = group_df[param_name].iloc[0]
         x.append(val)
     ax.scatter(x,y)
 
-if args.plot_function = 'PARAMS':
-    plt_gen(plt_ADC, samples)
+if args.plot_function == 'PARAMS':
+    plt_gen(plt_param, samples)
 
 """
-Two options for heatmaps, when having multiple events per timepoint.
-The first is creating a heatmap for each individual CALIB voltage, the second is a stacked plot to visualize and compare the pulse shapes.
+    Two options for heatmaps, when having multiple events per timepoint.
+    The first is creating a heatmap for each individual CALIB voltage, 
+    the second is a stacked plot to visualize and compare the pulse shapes.
+
+    Takes input csv from tasks.parameter_timescan
 """
 
-def plt_heatmap (
-    samples,
-    ax
-):
-        x_min = min(group_df['time'].min() for _, group_df in groups)
-        x_max = max(group_df['time'].max() for _, group_df in groups)
-        y_min = min(group_df['adc'].min() for _, group_df in groups) - 50
-        y_max = max(group_df['adc'].max() for _, group_df in groups) + 50
+if args.plot_type == 'HEATMAP':
+    x_min = min(group_df['time'].min() for _, group_df in groups)
+    x_max = max(group_df['time'].max() for _, group_df in groups)
+    y_min = min(group_df['adc'].min() for _, group_df in groups) - 50
+    y_max = max(group_df['adc'].max() for _, group_df in groups) + 50
 
-        x_bins = samples['time'].nunique() #amount of timepoints
-        y_bins = 128 #1024/8 = 128 for 1024 ADC bins
+    x_bins = samples['time'].nunique() #amount of timepoints
+    y_bins = 128 #1024/8 = 128 for 1024 ADC bins
 
-        x_edges = np.linspace(x_min, x_max, x_bins + 1)
-        y_edges = np.linspace(y_min, y_max, y_bins + 1)
+    x_edges = np.linspace(x_min, x_max, x_bins + 1)
+    y_edges = np.linspace(y_min, y_max, y_bins + 1)
 
-        def norm_heatmap(x, y, x_edges, y_edges):
-            H,_,_ = np.histogram2d(x,y, bins=[x_edges, y_edges])
+    def norm_heatmap(x, y, x_edges, y_edges):
+        H,_,_ = np.histogram2d(x,y, bins=[x_edges, y_edges])
 
-            #normalize each column (i.e. ADC axis for each time slice)
-            H_norm = np.zeros_like(H)
-            col_max = H.max(axis=0, keepdims=True) #max count per timeslice
-            nonzero_mask = col_max != 0 #to handle division by zero
-            H_norm[:, nonzero_mask[0]] = H[:, nonzero_mask[0]] / col_max[:, nonzero_mask[0]]
+        #normalize each column (i.e. ADC axis for each time slice)
+        H_norm = np.zeros_like(H)
+        col_max = H.max(axis=0, keepdims=True) #max count per timeslice
+        nonzero_mask = col_max != 0 #to handle division by zero
+        H_norm[:, nonzero_mask[0]] = H[:, nonzero_mask[0]] / col_max[:, nonzero_mask[0]]
 
-            return H_norm
+        return H_norm
 
-        #individual heatmap for each CALIB
-        for group_id, group_df in groups:
-            val = group_df[parameter_names[0]].iloc[0]
-            H_norm = norm_heatmap(group_df['time'], group_df['adc'], x_edges, y_edges)
-
-            plt.figure()
-            ax.imshow(
-                H_norm.T,  #transpose to have time on x-axis, ADC on y-axis
-                extent=[x_min, x_max, y_min, y_max],
-                aspect='auto',
-                cmap='Blues',
-                origin='lower',
-                vmin=0,
-                vmax=1
-            )
-            plt.xlabel('time / ns = (charge_to_l1a - 20 + 1)*25 - samples.phase_strobe*25/16')
-            plt.ylabel('ADC')
-            plt.colorbar(label='Normalized Counts')
-            plt.title(f'Normalized Heatmap for CALIB = {val}')
-            plt.savefig(f'{args.output}_NormalizedHeatmap_calib_{val}.png', dpi=200)
-            plt.close()
-
-
-        #stacked heatmap plot
-        H_combined = np.zeros((len(x_edges) - 1, len(y_edges) - 1))
-
-        for group_id, group_df in groups:
-            val = group_df[parameter_names[0]].iloc[0] 
-            H_norm = norm_heatmap(group_df['time'], group_df['adc'], x_edges, y_edges)
-            H_combined += H_norm
+    #individual heatmap for each CALIB
+    for group_id, group_df in groups:
+        val = group_df[parameter_names[0]].iloc[0]
+        H_norm = norm_heatmap(group_df['time'], group_df['adc'], x_edges, y_edges)
 
         plt.figure()
         plt.imshow(
-            H_combined.T,
+            H_norm.T,  #transpose to have time on x-axis, ADC on y-axis
             extent=[x_min, x_max, y_min, y_max],
-            origin='lower',
             aspect='auto',
             cmap='Blues',
+            origin='lower',
             vmin=0,
             vmax=1
         )
-
-        calib_vals = [group_df[parameter_names[0]].iloc[0] for _, group_df in groups]
-        calib_str = ", ".join(str(v) for v in calib_vals)
-
-        plt.colorbar(label='Normalized Counts')
         plt.xlabel('time / ns = (charge_to_l1a - 20 + 1)*25 - samples.phase_strobe*25/16')
         plt.ylabel('ADC')
-        plt.title(f'Combined Normalized Heatmap\nCALIB values: {calib_str}')
-        plt.tight_layout()
-        plt.savefig(f'{args.output}_CombinedHeatmap.png', dpi=200)
+        plt.colorbar(label='Normalized Counts')
+        plt.title(f'Normalized Heatmap for CALIB = {val}')
+        plt.savefig(f'{args.output}_NormalizedHeatmap_calib_{val}.png', dpi=200)
         plt.close()
 
 
-if args.plot_function = 'TIME' and args.plot_type == 'HEATMAP':
+    #stacked heatmap plot
+    H_combined = np.zeros((len(x_edges) - 1, len(y_edges) - 1))
+
+    for group_id, group_df in groups:
+        val = group_df[parameter_names[0]].iloc[0] 
+        H_norm = norm_heatmap(group_df['time'], group_df['adc'], x_edges, y_edges)
+        H_combined += H_norm
+
+    plt.figure()
+    plt.imshow(
+        H_combined.T,
+        extent=[x_min, x_max, y_min, y_max],
+        origin='lower',
+        aspect='auto',
+        cmap='Blues',
+        vmin=0,
+        vmax=1
+    )
+
+    calib_vals = [group_df[parameter_names[0]].iloc[0] for _, group_df in groups]
+    calib_str = ", ".join(str(v) for v in calib_vals)
+
+    plt.colorbar(label='Normalized Counts')
+    plt.xlabel('time / ns = (charge_to_l1a - 20 + 1)*25 - samples.phase_strobe*25/16')
+    plt.ylabel('ADC')
+    plt.title(f'Combined Normalized Heatmap\nCALIB values: {calib_str}')
+    plt.tight_layout()
+    plt.savefig(f'{args.output}_CombinedHeatmap.png', dpi=200)
+    plt.close()
+
+if args.plot_function == 'TIME' and args.plot_type == 'HEATMAP':
     plt_gen(plt_heatmap, samples)
