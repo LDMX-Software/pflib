0042187f1123b57d09e86bf6247fc73204446f5b
tomeichlersmith <eichl008@umn.edu>
2025-07-03 15:25:22 -0500

    seems to work, but a bit more boilerplate to share submenu pointer

---
+++ test/test_menu/sub/sub.cxx
@@ -0,0 +1,3 @@
+#include "sub.h"
+
+pflib::menu::Menu<test_menu::TargetHandle>* sub_menu = test_menu::menu("SB", "sub menu");
---
+++ test/test_menu/sub/sub.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "../test_menu.h"
+
+extern pflib::menu::Menu<test_menu::TargetHandle>* sub_menu;
