4d5d7231812c2a9560c2c2f8473976091988edd2
taylorjcolaizzi <zxk4gs@virginia.edu>
2025-07-09 15:44:20 -0400

    added trim_toa scan. scans through trim_toa for each channel, does preCC charge injection for each of them.

--- app/tool/tasks.cxx
+++ app/tool/tasks.cxx
@@ -697,6 +697,73 @@ static void toa_vref_scan(Target* tgt) {
   }
 }
 
+/**
+ * TASKS.TRIM_TOA_SCAN
+ * 
+ * Scan TRIM_TOA for each channel on chip
+ * Used to align TOA measurement after TOA_VREF scan
+ */
+
+static void trim_toa_scan(Target* tgt) {
+  int nevents = pftool::readline_int("Number of events per point: ", 100);
+  // Need to figure out which calib is best here.
+  int calib = pftool::readline_int("Setting for calib pulse amplitude? ", 500);
+
+  std::string output_filepath = pftool::readline_path("trim_toa_scan", ".csv");
+
+  auto roc = tgt->hcal().roc(pftool::state.iroc, pftool::state.type_version());
+
+  int trim_toa = 0;
+
+  pflib::DecodeAndWriteToCSV writer{
+    output_filepath,
+    [&](std::ofstream& f) {
+      boost::json::object header;
+      header["scan_type"] = "CH_#.TRIM_TOA sweep";
+      header["trigger"] = "CHARGE";
+      header["nevents_per_point"] = nevents;
+      f << "# " << boost::json::serialize(header) << "\n"
+        << "TRIM_TOA";
+      for (int ch{0}; ch < 72; ch++) {
+        f << "," << ch;
+      }
+      f << "\n";
+    },
+    [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket &ep) {
+      f << trim_toa;
+      for (int ch{0}; ch < 72; ch++) {
+        f << "," << ep.channel(ch).toa();
+      }
+      f << "\n";
+    }
+  };
+
+  tgt->setup_run(1 /* dummy */, DAQ_FORMAT_SIMPLEROC, 1 /* dummy */);
+
+  // Take a charge injection run at each trim_toa. Do preCC to test
+  // all the channels at the same time. Trim_toa has a range of 6 b.
+  // Need charge injection to get TOA values.
+  auto setup_builder = roc.testParameters()
+    .add("REFERENCEVOLTAGE_0", "CALIB_2V5", calib)
+    .add("REFERENCEVOLTAGE_1", "CALIB_2V5", calib)
+    .add("REFERENCEVOLTAGE_0", "INTCTEST", 1)
+    .add("REFERENCEVOLTAGE_1", "INTCTEST", 1);
+  for (int ch{0}; ch < 72; ch++) {
+    setup_builder.add("CH_"+std::to_string(ch), "LOWRANGE", 0);
+  }
+  auto setup_test = setup_builder.apply();
+
+  for (trim_toa = 0; trim_toa < 64; trim_toa++) {
+    pflib_log(info) << "Running TRIM_TOA = " << trim_toa;
+    auto trim_toa_test_builder = roc.testParameters();
+    for (int ch{0}; ch < 72; ch++) {
+      trim_toa_test_builder.add("CH_"+std::to_string(ch), "TRIM_TOA", trim_toa);
+    }
+    auto trim_toa_test = trim_toa_test_builder.apply();
+    tgt->daq_run("CHARGE", writer, nevents, pftool::state.daq_rate);
+  }
+}
+
 namespace {
 auto menu_tasks =
     pftool::menu("TASKS", "tasks for studying the chip and tuning its parameters")
@@ -708,5 +775,6 @@ auto menu_tasks =
         ->line("NOINV_VREF_SCAN", "scan over NOINV_VREF parameter", noinv_vref_scan)
         ->line("SAMPLING_PHASE_SCAN", "scan phase_ck, pedestal for clock phase alignment", sampling_phase_scan)
         ->line("TOA_VREF_SCAN", "scan over VREF parameters for TOA calibration", toa_vref_scan)
+        ->line("TRIM_TOA_SCAN", "scan over TRIM parameters for TOA calibration", trim_toa_scan)
 ;
 }
