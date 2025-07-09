c079a075551423238b589b3e33b2871737985ff4
taylorjcolaizzi <zxk4gs@virginia.edu>
2025-07-09 18:29:49 +0000

    fixed username, updated from main

--- app/tool/tasks.cxx
+++ app/tool/tasks.cxx
@@ -642,6 +642,61 @@ static void sampling_phase_scan(Target* tgt) {
   }
 }
 
+/**
+ * TASKS.TOA_VREF_SCAN
+ *
+ * Scan TOA_VREF for both halves of the chip
+ * Check ADC Pedestals for each point
+ * Used to align TOA trigger to fire just above pedestals
+ * Should be done after pedestal alignment
+ */
+static void toa_vref_scan(Target* tgt) {
+  int nevents = pftool::readline_int("Number of events per point: ", 100);
+
+  std::string output_filepath = pftool::readline_path("toa_vref_scan", ".csv");
+
+  auto roc = tgt->hcal().roc(pftool::state.iroc, pftool::state.type_version());
+
+  int toa_vref = 0;
+
+  pflib::DecodeAndWriteToCSV writer{
+    output_filepath,
+    [&](std::ofstream& f) {
+      boost::json::object header;
+      header["scan_type"] = "CH_#.TOA_VREF sweep";
+      header["trigger"] = "PEDESTAL";
+      header["nevents_per_point"] = nevents;
+      f << "# " << boost::json::serialize(header) << "\n"
+        << "TOA_VREF";
+      for (int ch{0}; ch < 72; ch++) {
+        f << "," << ch;
+      }
+      f << "\n";
+    },
+    [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket &ep) {
+      f << toa_vref;
+      for (int ch{0}; ch < 72; ch++) {
+        f << "," << ep.channel(ch).toa();
+      }
+      f << "\n";
+    }
+  };
+
+  tgt->setup_run(1 /* dummy */, DAQ_FORMAT_SIMPLEROC, 1 /* dummy */);
+
+  // Take a pedestal run on each parameter point
+  // Toa_vref has a 10 b range, but we're not going to need all of that
+  // since the pedestal variations are likely lower than ~300 adc.
+  for (toa_vref = 0; toa_vref < 256; toa_vref++) {
+    pflib_log(info) << "Running TOA_VREF = " << toa_vref;
+    auto toa_vref_test_builder = roc.testParameters();
+    toa_vref_test_builder.add("REFERENCEVOLTAGE_0", "TOA_VREF", toa_vref)
+    .add("REFERENCEVOLTAGE_1", "TOA_VREF", toa_vref);
+    auto toa_vref_test = toa_vref_test_builder.apply();
+    tgt->daq_run("PEDESTAL", writer, nevents, pftool::state.daq_rate);
+  }
+}
+
 namespace {
 auto menu_tasks =
     pftool::menu("TASKS", "tasks for studying the chip and tuning its parameters")
@@ -652,5 +707,6 @@ auto menu_tasks =
         ->line("INV_VREF_SCAN", "scan over INV_VREF parameter", inv_vref_scan)
         ->line("NOINV_VREF_SCAN", "scan over NOINV_VREF parameter", noinv_vref_scan)
         ->line("SAMPLING_PHASE_SCAN", "scan phase_ck, pedestal for clock phase alignment", sampling_phase_scan)
+        ->line("TOA_VREF_SCAN", "scan over VREF parameters for TOA calibration", toa_vref_scan)
 ;
 }
