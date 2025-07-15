a683431d409bea04b1a1c78a9d8c1d6dee60faaa
Andreas Pettersson <an6826pe-s@student.lu.se>
2025-07-15 19:02:44 +0200

    set_toa function for tasks (#177)
    * trying out a function that sets the toa over the pedestal. Wrote a new class that inherits from DecodeAndWrite that could be used when we don't want to write anything to a csv
    
    * finalized set_toa and fixed problems with DecodeAndWriteToBuffer.
    
    * fixed some names to make more sense
    
    * Fixed some minor bugs
    
    * moved DecodeAndBuffer to its own header and source files. Changed set_toa so that it applies the parameter to the roc instead of returning a test handle
    
    * Removed buffer functions from DecodeAndWrite
    
    * added a subclass of DecodeAndBuffer for reading out the buffer
    
    * bugfixing
    
    * Had to redo class functionality, because I don't understand it. Added DecodeAndBuffer to the CMake
    
    * removed get_toa call
    
    * implemented suggestions
    
    * changed DecodeAndBuffer to inherit from DecodeAndWrite directly
    
    * some cleanup of new DecodeAndBuffer
    
    - update documentation with advice on how to use buffer
    - return const refs to avoid copying data
    - drop unnecessary members now that we are inheriting from
      DecodeAndWrite
    
    * cosmetic cleanup and fix of how buffer is being used
    
    ---------
    
    Co-authored-by: Andreas Pettersson <andreas.pettersson.6826@student.lu.se>
    Co-authored-by: tomeichlersmith <eichl008@umn.edu>

--- app/tool/tasks.cxx
+++ app/tool/tasks.cxx
@@ -13,6 +13,7 @@ ENABLE_LOGGING();
 #include "pflib/utility/load_integer_csv.h"
 #include "pflib/utility/json.h"
 #include "pflib/DecodeAndWrite.h"
+#include "pflib/DecodeAndBuffer.h"
 
 /**
  * load a CSV of parameter points into member
@@ -73,6 +74,64 @@ load_parameter_points(const std::string& filepath) {
     }
   ); 
   return std::make_tuple(param_names, param_values);
+}
+
+/*
+ * TASKS.SET_TOA
+ *
+ * Do a charge injection run for a given channel, find the toa efficiency, and set
+ * the toa threshold to the first point where the toa efficiency is 1. 
+ * This corresponds to a point above the pedestal where the pedestal shouldn't 
+ * fluctuate and trigger the toa.
+ *
+ * Things that might be improved upon:
+ * the number of events nevents, which determines our toa efficiency.
+ * The calib value which defines a small pulse. 
+ *
+ */
+static void set_toa(Target* tgt, pflib::ROC& roc, int channel) {
+  int link = (channel / 36);
+  auto refvol_page = pflib::utility::string_format("REFERENCEVOLTAGE_%d", link);
+  auto channel_page = pflib::utility::string_format("CH_%d", channel);
+  int nevents = 30;
+  double toa_eff{2};
+  // in the calibration documentation, it is suggested to send a "small" charge injection.
+  // Here I used 200 but there is maybe a better value.
+  int calib = 200;
+  auto test_param_handle = roc.testParameters()
+      .add(refvol_page, "CALIB",  calib)
+      .add(refvol_page, "INTCTEST", 1)
+      .add(channel_page, "LOWRANGE", 1)
+      .apply();
+
+
+  pflib_log(info) << "finding the TOA threshold!";
+  // This class doesn't write to csv. When we just would like to
+  // use the data for setting params.
+  pflib::DecodeAndBuffer buffer(nevents);
+
+  tgt->setup_run(1 /* dummy - not stored */, DAQ_FORMAT_SIMPLEROC, 1 /* dummy */);
+  for (int toa_vref = 100; toa_vref < 250; toa_vref++) {
+    auto test_handle = roc.testParameters()
+      .add(refvol_page, "TOA_VREF", toa_vref)
+      .apply();
+    tgt->daq_run("CHARGE", buffer, nevents, pftool::state.daq_rate);
+    std::vector<double> toa_data;
+    for (const pflib::packing::SingleROCEventPacket &ep : buffer.get_buffer()) {
+      auto toa = ep.channel(channel).toa();
+      if (toa > 0) {
+        toa_data.push_back(toa);
+      }
+    }
+    toa_eff = static_cast<double>(toa_data.size())/nevents;
+    if (toa_eff == 1) {
+      roc.applyParameter(refvol_page, "TOA_VREF", toa_vref);
+      pflib_log(info) << "the TOA threshold is set to " << toa_vref;
+      return;
+    }
+    toa_vref += 1;
+  }
+  PFEXCEPTION_RAISE("NOTOA", "No TOA threshold was found for channel " + std::to_string(channel) + "!");
 }
 
 /**
