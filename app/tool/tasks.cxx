e379cdfc5607c2b15d519df82e064c48ac3755ce
tomeichlersmith <eichl008@umn.edu>
2025-07-16 11:31:38 -0500

    code cleanup and alignment with main
    - move level_pedestals task to its own file
    - move median implementation to pflib::utility
    - use DecodeAndBuffer already on main

--- app/tool/tasks.cxx
+++ app/tool/tasks.cxx
@@ -878,53 +878,7 @@ static void vt50_scan(Target* tgt) {
   }
 }
 
-#include <yaml-cpp/yaml.h>
-#include "pflib/algorithm/level_pedestals.h"
-
-/**
- * TASKS.LEVEL_PEDESTALS
- *
- * @see pflib::algorithm::level_pedestals for the core leveling algorithm
- */
-static void level_pedestals(Target* tgt) {
-  auto roc{tgt->hcal().roc(pftool::state.iroc, pftool::state.type_version())};
-  auto settings = pflib::algorithm::level_pedestals(tgt, roc);
-  YAML::Emitter out;
-  out << YAML::BeginMap;
-  for (const auto& page : settings) {
-    out << YAML::Key << page.first;
-    out << YAML::Value << YAML::BeginMap;
-    for (const auto& param : page.second) {
-      out << YAML::Key << param.first << YAML::Value << param.second;
-    }
-    out << YAML::EndMap;
-  }
-  out << YAML::EndMap;
-
-  if (pftool::readline_bool("View deduced settings? ", true)) {
-    std::cout << out.c_str() << std::endl;
-  }
-
-  if (pftool::readline_bool("Apply settings to the chip? ", false)) {
-    roc.applyParameters(settings);
-  }
-
-  if (pftool::readline_bool("Save settings to a file? ", false)) {
-    std::string fname = pftool::readline_path(
-      "level-pedestals-roc-"+std::to_string(pftool::state.iroc)+"-settings",
-      ".yaml"
-    );
-
-    std::ofstream f{fname};
-    if (not f.is_open()) {
-      PFEXCEPTION_RAISE(
-          "File",
-          "Unable to open file " + fname + "."
-      );
-    }
-    f << out.c_str() << std::endl;
-  }
-}
+#include "tasks/level_pedestals.h"
 
 namespace {
 auto menu_tasks =
--- src/pflib/algorithm/level_pedestals.cxx
+++ src/pflib/algorithm/level_pedestals.cxx
@@ -1,41 +1,23 @@
 #include "pflib/algorithm/level_pedestals.h"
+
 #include "pflib/utility/string_format.h"
+#include "pflib/utility/median.h"
 
-#include "pflib/DecodeAndWrite.h"
+#include "pflib/DecodeAndBuffer.h"
 
-/// probably partially replace by buffer that is in #177
-
-class DecodeAndGetMedians : public pflib::DecodeAndWrite {
-  std::vector<pflib::packing::SingleROCEventPacket> buffer_;
- public:
-  DecodeAndGetMedians(std::size_t n) : DecodeAndWrite() {
-    buffer_.reserve(n);
-  }
-  virtual ~DecodeAndGetMedians() = default;
-  virtual void start_run() final {
-    buffer_.clear();
-  }
-  virtual void write_event(const pflib::packing::SingleROCEventPacket &ep) final {
-    if (buffer_.size() == buffer_.capacity()) {
-      PFEXCEPTION_RAISE("BuffOverflow", "Attempting to fill buffer past its capacity.");
+static std::array<int, 72> get_adc_medians(const std::vector<pflib::packing::SingleROCEventPacket> &data) {
+  std::array<int, 72> medians;
+  std::vector<int> adcs;
+  adcs.reserve(data.size());
+  for (int ch{0}; ch < 72; ch++) {
+    adcs.clear();
+    for (std::size_t i{0}; i < adcs.size(); i++) {
+      adcs[i] = data[i].channel(ch).adc();
     }
-    buffer_.push_back(ep);
+    medians[ch] = pflib::utility::median(adcs);
   }
-  const auto& get_buffer() const { return buffer_; }
-  std::array<int, 72> get_medians() const {
-    std::size_t halfway{buffer_.size() / 2};
-    std::array<int, 72> medians;
-    for (int ch{0}; ch < 72; ch++) {
-      std::vector<int> adcs(buffer_.size());
-      for (std::size_t i{0}; i < adcs.size(); i++) {
-        adcs[i] = buffer_[i].channel(ch).adc();
-      }
-      std::nth_element(adcs.begin(), adcs.begin()+halfway, adcs.end());
-      medians[ch] = adcs[halfway];
-    }
-    return medians;
-  }
-};
+  return medians;
+}
 
 namespace pflib::algorithm {
 
@@ -50,23 +32,24 @@ level_pedestals(Target* tgt, ROC roc) {
 
   std::array<int, 2> target;
   std::array<int, 72> baseline, highend, lowend;
-  DecodeAndGetMedians buffer{n_events};
+  DecodeAndBuffer buffer{n_events};
   
   { // baseline run
-    pflib_log(info) << "100 event baseline run"
+    pflib_log(info) << "100 event baseline run";
     auto test_handle = roc.testParameters()
       .add_all_channels("SIGN_DAC", 0)
       .add_all_channels("DACB", 0)
       .add_all_channels("TRIM_INV", 0)
       .apply();
     tgt->daq_run("PEDESTAL", buffer, n_events, 100);
-    auto medians = buffer.get_medians();
+    auto medians = get_adc_medians(buffer.get_buffer());
     baseline = medians;
     for (int i_link{0}; i_link < 2; i_link++) {
       auto start{medians.begin()+36*i_link};
       auto end{start+36};
-      std::nth_element(start, start+18, end);
-      target[i_link] = medians[36*i_link+18];
+      auto halfway{start+18};
+      std::nth_element(start, halfway, end);
+      target[i_link] = *halfway;
     }
   }
 
@@ -78,7 +61,7 @@ level_pedestals(Target* tgt, ROC roc) {
       .add_all_channels("TRIM_INV", 63)
       .apply();
     tgt->daq_run("PEDESTAL", buffer, n_events, 100);
-    highend = buffer.get_medians();
+    highend = get_adc_medians(buffer.get_buffer());
   }
 
   { // lowend run
@@ -89,7 +72,7 @@ level_pedestals(Target* tgt, ROC roc) {
       .add_all_channels("TRIM_INV", 0)
       .apply();
     tgt->daq_run("PEDESTAL", buffer, n_events, 100);
-    lowend = buffer.get_medians();
+    lowend = get_adc_medians(buffer.get_buffer());
   }
 
   pflib_log(info) << "sample collections done, deducing settings";
