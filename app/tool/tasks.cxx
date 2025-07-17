1b805b5e8670626a107a0812095c348e478347b7
taylorjcolaizzi <zxk4gs@virginia.edu>
2025-07-17 14:18:20 -0400

    toa alignment exploration tools (#175)
    * fixed username, updated from main
    
    * added trim_toa scan.
    scans through trim_toa for each channel, does
    preCC charge injection for each of them.
    
    * adding a do_toa_vref_analysis.ipynb file; will be cleaning this file into a .py script instead
    
    * fixed the naming of the data file
    
    * Converted old do_toa_vref_analysis python notebook into toa_efficiency python script.
    
    * final draft of toa_efficiency.py uploaded. removed old .ipynb
    
    * remove unnecessary builder
    
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
    
    * added read_pflib_csv function, commented about example toa_efficiency plot
    
    * python script to output yaml file for toa_vref values
    
    * ana/toa_vref/toa_efficiency.py now outputs yaml file with recommended toa_vref values
    
    * removed unnecessary files
    
    * Fixed trim_toa_scan in tasks.cxx so it would be a 2D scan on calib AND trim_toa. Also made a temporary Python notebook to analyze data from these scans.
    
    * converted python notebook into a python script
    
    * remove unnecessary list
    
    yep! This wasn't being used.
    
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
    
    * move trim_toa_test before loop
    
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
    
    * final cleanup to get it compiling
    
    ---------
    
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
    Co-authored-by: Owen Cole <143980326+OwenCole04@users.noreply.github.com>
    Co-authored-by: tomeichlersmith <eichl008@umn.edu>

--- app/tool/tasks.cxx
+++ app/tool/tasks.cxx
@@ -878,6 +878,138 @@ static void vt50_scan(Target* tgt) {
   }
 }
 
+/**
+ * TASKS.TOA_VREF_SCAN
+ *
+ * Scan TOA_VREF for both halves of the chip
+ * Check ADC Pedestals for each point
+ * Used to align TOA trigger to fire just above pedestals
+ * Should be done after pedestal alignment
+ */
+static void toa_vref_scan(Target* tgt) {
+  int nevents = pftool::readline_int("Number of events per point: ", 100);
+
+  std::string output_filepath = pftool::readline_path("toa_vref_scan", ".csv");
+
+  auto roc = tgt->hcal().roc(pftool::state.iroc, pftool::state.type_version());
+
+  int toa_vref = 0;
+
+  pflib::DecodeAndWriteToCSV writer{
+    output_filepath,
+    [&](std::ofstream& f) {
+      boost::json::object header;
+      header["scan_type"] = "CH_#.TOA_VREF sweep";
+      header["trigger"] = "PEDESTAL";
+      header["nevents_per_point"] = nevents;
+      f << "# " << boost::json::serialize(header) << "\n"
+        << "TOA_VREF";
+      for (int ch{0}; ch < 72; ch++) {
+        f << "," << ch;
+      }
+      f << "\n";
+    },
+    [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket &ep) {
+      f << toa_vref;
+      for (int ch{0}; ch < 72; ch++) {
+        f << "," << ep.channel(ch).toa();
+      }
+      f << "\n";
+    }
+  };
+
+  tgt->setup_run(1 /* dummy */, DAQ_FORMAT_SIMPLEROC, 1 /* dummy */);
+
+  // Take a pedestal run on each parameter point
+  // Toa_vref has a 10 b range, but we're not going to need all of that
+  // since the pedestal variations are likely lower than ~300 adc.
+  for (toa_vref = 0; toa_vref < 256; toa_vref++) {
+    pflib_log(info) << "Running TOA_VREF = " << toa_vref;
+    auto toa_vref_test = roc.testParameters()
+      .add("REFERENCEVOLTAGE_0", "TOA_VREF", toa_vref)
+      .add("REFERENCEVOLTAGE_1", "TOA_VREF", toa_vref)
+      .apply();
+    tgt->daq_run("PEDESTAL", writer, nevents, pftool::state.daq_rate);
+  }
+}
+
+/**
+ * TASKS.TRIM_TOA_SCAN
+ * 
+ * Scan TRIM_TOA  and CALIB for each channel on chip
+ * Used to align TOA measurement after TOA_VREF scan
+ */
+
+static void trim_toa_scan(Target* tgt) {
+  int nevents = pftool::readline_int("Number of events per point: ", 100);
+
+  std::string output_filepath = pftool::readline_path("trim_toa_scan", ".csv");
+
+  auto roc = tgt->hcal().roc(pftool::state.iroc, pftool::state.type_version());
+
+  int trim_toa = 0;
+  int calib = 0;
+
+  pflib::DecodeAndWriteToCSV writer{
+    output_filepath,
+    [&](std::ofstream& f) {
+      boost::json::object header;
+      header["scan_type"] = "CH_#.TRIM_TOA sweep";
+      header["trigger"] = "CHARGE";
+      header["nevents_per_point"] = nevents;
+      f << "# " << boost::json::serialize(header) << "\n"
+        << "TRIM_TOA" << "," << "CALIB_2V5";
+      for (int ch{0}; ch < 72; ch++) {
+        f << "," << ch;
+      }
+      f << "\n";
+    },
+    [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket &ep) {
+      f << trim_toa << "," << calib;
+      // Write the TOA values for each channel
+      for (int ch{0}; ch < 72; ch++) {
+        f << "," << ep.channel(ch).toa();
+      }
+      f << "\n";
+    }
+  };
+
+  tgt->setup_run(1 /* dummy */, DAQ_FORMAT_SIMPLEROC, 1 /* dummy */);
+
+  // Take a charge injection run at each trim_toa. Do preCC to test
+  // all the channels at the same time. Trim_toa has a range of 6 b.
+  // And vary calib, from 0 to 256. Want to see how toa_efficiency
+  // changes with trim_toa and calib. More details in the ana script.
+  auto setup_builder = roc.testParameters()
+    .add("REFERENCEVOLTAGE_0", "CALIB_2V5", calib)
+    .add("REFERENCEVOLTAGE_1", "CALIB_2V5", calib)
+    .add("REFERENCEVOLTAGE_0", "INTCTEST", 1)
+    .add("REFERENCEVOLTAGE_1", "INTCTEST", 1);
+  for (int ch{0}; ch < 72; ch++) {
+    setup_builder.add("CH_"+std::to_string(ch), "HIGHRANGE", 1);
+  }
+  auto setup_test = setup_builder.apply();
+
+  for (trim_toa = 0; trim_toa < 64; trim_toa += 16) {
+    pflib_log(info) << "Running TRIM_TOA = " << trim_toa;
+    auto trim_toa_test_builder = roc.testParameters();
+    for (int ch{0}; ch < 72; ch++) {
+      trim_toa_test_builder.add("CH_"+std::to_string(ch), "TRIM_TOA", trim_toa);
+    }
+    // set TRIM_TOA for each channel
+    auto trim_toa_test = trim_toa_test_builder.apply();
+    for (calib = 0; calib < 4096; calib += 16) {
+      pflib_log(info) << "Running CALIB = " << calib;
+      // Set the CALIB parameters for both halves
+      auto calib_test = roc.testParameters()
+        .add("REFERENCEVOLTAGE_0", "CALIB_2V5", calib)
+        .add("REFERENCEVOLTAGE_1", "CALIB_2V5", calib)
+        .apply();
+      tgt->daq_run("CHARGE", writer, nevents, pftool::state.daq_rate);
+    }
+  }
+}
+
 namespace {
 auto menu_tasks =
     pftool::menu("TASKS", "tasks for studying the chip and tuning its parameters")
@@ -889,5 +1021,7 @@ auto menu_tasks =
         ->line("NOINV_VREF_SCAN", "scan over NOINV_VREF parameter", noinv_vref_scan)
         ->line("SAMPLING_PHASE_SCAN", "scan phase_ck, pedestal for clock phase alignment", sampling_phase_scan)
         ->line("VT50_SCAN", "Hones in on the vt50 with a binary or bisectional scan", vt50_scan)
+        ->line("TOA_VREF_SCAN", "scan over VREF parameters for TOA calibration", toa_vref_scan)
+        ->line("TRIM_TOA_SCAN", "scan over TRIM parameters for TOA calibration", trim_toa_scan)
 ;
 }
