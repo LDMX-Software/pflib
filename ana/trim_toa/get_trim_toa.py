e5a443611165d66b0cd4958ae3f72e49165e31b3
taylorjcolaizzi <zxk4gs@virginia.edu>
2025-07-25 14:52:35 -0400

    cleanup

--- ana/trim_toa/get_trim_toa.py
+++ ana/trim_toa/get_trim_toa.py
@@ -67,13 +67,11 @@ data, head = read_pflib_csv(args.f)
 
 unique_trim_toas = data['TRIM_TOA'].unique()
 unique_calibs = data['CALIB'].unique()
-# trim = unique_trim_toas[0]
 print('working on plotting toa_efficiency vs calib')
 array = np.array([], dtype = 'int').reshape(0, 3)
 for trim in unique_trim_toas:
     print(f'on trim_toa = {trim}')
     current_trim_toa = data[data['TRIM_TOA'] == trim]
-    # for chan in range(36, 72):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5))
     for chan in range(36):
         toa_efficiency = []
@@ -82,12 +80,10 @@ for trim in unique_trim_toas:
             toa_triggers = len(current_calib[current_calib[str(chan)] != 0])
             chan_toa_efficiency = toa_triggers / len(current_calib)
             toa_efficiency.append(chan_toa_efficiency)
-        # plt.plot(unique_calibs, toa_efficiency, label=f'CH_{chan}', linestyle = '-', marker = 'none')
         ax1.plot(unique_calibs, toa_efficiency, label = f'CH_{chan}', linestyle = '-', marker = 'none')
         ax1.set_title('link 0')
         ax1.set_xlabel('calib')
         ax1.set_ylabel('toa_efficiency')
-        # plt.plot(current_trim_toa['CALIB'], current_trim_toa['0'], linestyle = 'none', marker = 'o', alpha = .1)
         for i in range(len(toa_efficiency)):
             if toa_efficiency[i] != 0.0:
                 val = i
@@ -102,7 +98,6 @@ for trim in unique_trim_toas:
             toa_triggers = len(current_calib[current_calib[str(chan)] != 0])
             chan_toa_efficiency = toa_triggers / len(current_calib)
             toa_efficiency.append(chan_toa_efficiency)
-        # plt.plot(unique_calibs, toa_efficiency, label=f'CH_{chan}', linestyle = '-', marker = 'none')
         ax2.plot(unique_calibs, toa_efficiency, label = f'CH_{chan}', linestyle = '-', marker = 'none')
         ax2.set_title('link 1')
         ax2.set_xlabel('calib')
@@ -119,7 +114,6 @@ for trim in unique_trim_toas:
     plt.grid()
     plt.tight_layout()
     plt.savefig(f'toa_efficiency_TRIM_TOA_{trim}.png')
-    # plt.show()
     plt.close()  # close the plot to avoid displaying it immediately
 print('plots saved')
 
@@ -147,7 +141,6 @@ for chan in range(36):
     trim_toa = (chan_data[:,0])
     slope, offset, r_value, p_value, slope_err = lr(trim_toa, calib_2v5)
     stats.loc[len(stats)] = [chan, slope, offset]
-    # plt.plot(trim_toa, slope * trim_toa + offset, color = colors[chan])
     ax1.plot(trim_toa, slope * trim_toa + offset, color = colors[chan])
 for chan in range(36, 72):
     chan_data = array[array[:, 2] == chan] # picks out the third column
@@ -166,10 +159,7 @@ ax2.set_xlabel('trim_toa')
 ax1.grid(True)
 plt.grid()
 plt.suptitle("TRIM_TOA vs CALIB for each channel's toa turnaround points\nand linear fits")
-# plt.legend(loc = 'upper left', bbox_to_anchor = (1, 1.15))
 plt.savefig('threshold_points.png')
-# plt.show()
 plt.close()
-# Goal is to see a descending line for each channel. But I'm kinda not seeing that yet.
 
 print('saved figs')
