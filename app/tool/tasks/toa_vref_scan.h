d4dacfc89d32fc295ecafd5517b7a6fd2c2861f5
taylorjcolaizzi <zxk4gs@virginia.edu>
2025-08-06 19:09:57 +0000

    made a couple changes to toa_vref_scan so it's more like c++

--- app/tool/tasks/toa_vref_scan.h
+++ app/tool/tasks/toa_vref_scan.h
@@ -8,6 +8,9 @@
  * Scan TOA_VREF for both halves of the chip
  * Check ADC Pedestals for each point
  * Used to align TOA trigger to fire just above pedestals
- * Should be done after pedestal alignment
+ * Should be done after global pedestal alignment
+ *
+ * TODO: this scan will also output the ideal TOA_VREF 
+ * values using the pedestals method.
  */
 void toa_vref_scan(Target* tgt);
