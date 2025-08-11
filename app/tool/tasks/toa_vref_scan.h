66c197f32e7d85b0a953086f40165b1afc4a24b5
taylorjcolaizzi <zxk4gs@virginia.edu>
2025-08-11 14:14:05 -0400

    193 convert toa alignment (#198)
    * made a couple changes to toa_vref_scan so it's more like c++
    
    * adding new files to organize code better
    
    * first draft of toa_vref_scan in c++
    
    * fixed a couple typos, but it still won't compile
    
    * tried renaming things to fix issue, but it didn't seem to work
    
    * forgot to add files to cmade dependencies
    
    * finally got it to compile and toa_vref_scan works now
    
    * added trim_toa_scan back in
    
    * empty data check; keep comment
    
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
    
    * keep 10 µs sleep after setting parameters
    
    Ha! I forgot that 256 * 10^(-6) is teeny tiny!
    
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
    
    * added NaN to efficiency if sample is empty
    
    * removed 'chan_effs' variable
    
    * remove intermediate variables
    
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
    
    * removed old error message
    
    ---------
    
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>

--- app/tool/tasks/toa_vref_scan.h
+++ app/tool/tasks/toa_vref_scan.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "../pftool.h"
 
 /**
@@ -8,6 +7,9 @@
  * Scan TOA_VREF for both halves of the chip
  * Check ADC Pedestals for each point
  * Used to align TOA trigger to fire just above pedestals
- * Should be done after pedestal alignment
+ * Should be done after global pedestal alignment
+ *
+ * TODO: this scan will also output the ideal TOA_VREF 
+ * values using the pedestals method.
  */
 void toa_vref_scan(Target* tgt);
---
+++ src/pflib/utility/efficiency.cxx
@@ -0,0 +1,14 @@
+#include "pflib/utility/efficiency.h"
+#include <limits> // need for "numeric_limits" to work in conditional below.
+
+namespace pflib::utility {
+
+double efficiency(std::vector<int> samples) {
+  /// find number of non-zero samples divided by total number of samples in vector
+  if (samples.size() == 0) {
+    return std::numeric_limits<double>::quiet_NaN();
+  }
+  double eff = std::count_if(samples.begin(), samples.end(), [](int sample) { return sample > 0; }) / static_cast<double>(samples.size());
+  return eff;
+  }
+}
