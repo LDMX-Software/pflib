6fd892185b00aa7622ecf9a2eb5ae8e972e6a1ce
tomeichlersmith <eichl008@umn.edu>
2025-09-16 13:37:27 -0500

    more docu cleanup and get compiling

--- src/pflib/algorithm/trim_toa_scan.cxx
+++ src/pflib/algorithm/trim_toa_scan.cxx
@@ -1,54 +1,69 @@
 #include "pflib/algorithm/trim_toa_scan.h"
+
 #include "pflib/algorithm/get_toa_efficiencies.h"
 
 #include "pflib/utility/string_format.h"
+#include "pflib/utility/median.h"
 
 #include "pflib/DecodeAndBuffer.h"
 
-// probably don't need these, but just double checking
-#include <algorithm>
-#include <cmath>
 
-
-// Here's the regression. Main idea is that it finds the median slope from all the pairs of points in the dataset.
-// Similarly, does the same for the intercept using the median slope. Supposedly less sensitive to outliers than the standard linear regression.
-// Got this function straight from Copilot. Maybe we should put it in algorithm or utility? Not sure if we'll use linear regression for 
-// TOT and stuff like that...
-void siegelRegression(const std::vector<double>& xVals, const std::vector<double>& yVals, double& slope, double& intercept) {
-  if (xVals.size() != yVals.size()) {
-    throw std::invalid_argument("xVals and yVals must be the same size.");
+/**
+ * Perform a linear regression using the
+ * [Theil-Sen estimator](https://en.wikipedia.org/wiki/Theil%E2%80%93Sen_estimator)
+ * which is more robust against outliers.
+ *
+ * We do the naive algorithm, calculating the slope of all pairs of sample points
+ * and then finding the median of that slope. The final intercept is the median
+ * of the intercepts from all points using the median slope.
+ *
+ * @param[in] x_vals list of x-coordinate samples
+ * @param[in] y_vals list of y-coordinate samples
+ * @return 2-tuple of the form (slope, intercept)
+ */
+std::tuple<double,double> siegel_regression(
+    const std::vector<double>& x_vals,
+    const std::vector<double>& y_vals
+) {
+  if (x_vals.size() != y_vals.size()) {
+    throw std::invalid_argument("x_vals and y_vals must be the same size.");
   }
-  if (xVals.empty()) {
+  if (x_vals.empty()) {
     throw std::invalid_argument("Input vectors must not be empty.");
   }
   
-  size_t n = xVals.size();
-  std::vector<double> slopes;
+  size_t n = x_vals.size();
 
+  std::vector<double> slopes;
+  slopes.reserve(n*n);
   for (size_t i = 0; i < n; ++i) {
     for (size_t j = i + 1; j < n; ++j) {
-      if (xVals[j] != xVals[i]) {
-        double m = (yVals[j] - yVals[i]) / (xVals[j] - xVals[i]);
-        slopes.push_back(m);
+      /**
+       * @note We are ignoring any pairs which would lead to division by zero.
+       */
+      if (x_vals[j] != x_vals[i]) {
+        slopes.push_back((y_vals[j] - y_vals[i]) / (x_vals[j] - x_vals[i]));
       }
     }
   }
 
-  slope = median(slopes);
+  double slope = pflib::utility::median(slopes);
 
   std::vector<double> intercepts;
+  intercepts.reserve(n);
   for (size_t i = 0; i < n; i++) {
-    intercepts.push_back(yVals[i] - slope * xVals[i]);
+    intercepts.push_back(y_vals[i] - slope * x_vals[i]);
   }
 
-  intercept = median(intercepts);
+  double intercept = pflib::utility::median(intercepts);
+  return std::make_tuple(slope, intercept);
 }
-
-// end vibe coding. back to what we had before
 
 /**
  * Calculate the TRIM_TOA for each channel that best aligns all of them
  * to a common threshold voltage, charge injection pulse (calib).
+ *
+ * @note Not currently operational, but a good place to pickup from.
  */
 namespace pflib::algorithm {
 
@@ -56,21 +71,27 @@ std::map<std::string, std::map<std::string, int>>
 trim_toa_scan(Target* tgt, ROC roc) {
   static auto the_log_{::pflib::logging::get("trim_toa_scan")};
 
-  /// Charge injection scan (100 samples) while varying TRIM_TOA.
-  /// Purpose is to align TRIM_TOA for each channel.
-  /// Calculates TOA efficiency while looking at charge injection data. 
-  /// Then uses Siegel Linear Regression to calculate the aligned
-  /// TRIM_TOA value for each channel to match a common "calib" value.
-  /// Note: Reduce the sample size (ex: 100 to 10) to decrease the scan time.
+  /**
+   * Charge injection scan (100 samples) while varying TRIM_TOA.
+   * Purpose is to align TRIM_TOA for each channel.
+   * Calculates TOA efficiency while looking at charge injection data.
+   * Then uses Siegel Linear Regression to calculate the aligned
+   * TRIM_TOA value for each channel to match a common "calib" value.
+   *
+   * @note Reduce the sample size (ex: 100 to 10) to decrease the scan time.
+   */
 
   static const std::size_t n_events = 100;
 
   tgt->setup_run(1, Target::DaqFormat::SIMPLEROC, 1);
 
-  std::array<int, 72> target; //trim_toa is a channel-wise parameter (1 value per channel)
-  std::array<std::array<std::array<double, 72>, 8>, 200> final_data; // 72 channels, 200 calib values, 8 trim_toa values. Only store toa_efficiency here.
+  //trim_toa is a channel-wise parameter (1 value per channel)
+  std::array<int, 72> target; 
+  // 72 channels, 200 calib values, 8 trim_toa values. Only store toa_efficiency here.
+  std::array<std::array<std::array<double, 72>, 8>, 200> final_data;
 
-  DecodeAndBuffer buffer{n_events}; // working in buffer, not in writer
+  // working in buffer, not in writer
+  DecodeAndBuffer buffer{n_events};
 
   // loop over trim_toa, from trim_toa = 0 to 32 by skipping 4
   // loop over calib, from calib = 0 to 800 by skipping over 4
@@ -95,38 +116,42 @@ trim_toa_scan(Target* tgt, ROC roc) {
       tgt->daq_run("CHARGE", buffer, n_events, 100);
 
       pflib_log(trace) << "finished trim_toa = " << trim_toa << ", and calib = " << calib << ", getting efficiencies";
-      // std::cout << "buffer size: " << buffer.get_buffer().size() << std::endl; // debug
       auto efficiencies = get_toa_efficiencies(buffer.get_buffer());
       pflib_log(trace) << "got channel efficiencies, storing now";
       for (int ch{0}; ch < 72; ch++) {
-        final_data[calib/4][trim_toa/4][ch] = efficiencies[ch]; // need to divide by 4 because index is value/4 from final_data initialization
-        // std::cout << "calib=" << calib << ", trim_toa=" << trim_toa << ", ch=" << ch<< ", eff=" << efficiencies[ch] << std::endl; //debug
+        // need to divide by 4 because index is value/4 from final_data initialization and for loop
+        final_data[calib/4][trim_toa/4][ch] = efficiencies[ch];
       }
     }
   }
 
   pflib_log(info) << "sample collections done, deducing settings";
 
-  // Now that we have the data, we need to analyze it.
-  // We'll be looking for the turn-on (threshold) points for each channel 
-  // at each trim_toa value. The turn-on (threshold) point is the first
-  // point where toa_efficiency goes from 0 to non-zero. The toa_efficiency
-  // is simply the number of times TOA triggers divided by the sample size, for
-  // a given trim_toa/calib/channel combination.
+  /**
+   * Now that we have the data, we need to analyze it.
+   *
+   * We'll be looking for the turn-on (threshold) points for each channel 
+   * at each trim_toa value. The turn-on (threshold) point is the first 
+   * point where toa_efficiency goes from 0 to non-zero. The toa_efficiency 
+   * is simply the number of times TOA triggers divided by the sample size, for 
+   * a given trim_toa/calib/channel combination.
+   * 
+   * We'll be using the Siegel Linear Regression because it's less sensitive to 
+   * outliers, since sometimes changing the trim_toa causes the threshold (turn-on) 
+   * points to "wrap around".
+   */
 
-  // We'll be using the Siegel Linear Regression because it's less sensitive to
-  // outliers, since sometimes changing the trim_toa causes the threshold (turn-on)
-  // points to "wrap around".
-
+  // "threshold_points" is a 2D vector.
+  // Column 1 is channel index, Column 2 is calib, Column 3 is trim_toa.
   std::vector<std::vector<int>> threshold_points;
 
-  // "threshold_points" is a 2D vector. Column 1 is channel index, Column 2 is calib, Column 3 is trim_toa.
   // Each channel has multiple threshold points, but we don't know how many at the start.
 
   for (int trim_toa{0}; trim_toa < 32; trim_toa += 4) {
     for (int ch{0}; ch < 72; ch++) {
       for (int calib{0}; calib < 800; calib += 4) {
-        if (final_data[calib/4][trim_toa/4][ch] > 0.0) { // again, needing to divide by 4.
+        // divide by 4 to convert value to index
+        if (final_data[calib/4][trim_toa/4][ch] > 0.0) {
           threshold_points.push_back({ch, calib, trim_toa});
           break;
         }
@@ -153,10 +178,9 @@ trim_toa_scan(Target* tgt, ROC roc) {
       continue;
     }
 
-    double slope, intercept;
     std::cout << "about to do linear regression" << std::endl;
-    std::cout << "xVals size: " << calib.size() << ", yVals size: " << trim_toa.size() << std::endl;
-    siegelRegression(calib, trim_toa, slope, intercept);
+    std::cout << "x_vals size: " << calib.size() << ", y_vals size: " << trim_toa.size() << std::endl;
+    auto [slope, intercept] = siegel_regression(calib, trim_toa);
     std::cout << "did regression" << std::endl;
   }
 
