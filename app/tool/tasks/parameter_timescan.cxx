8f6b4eff8da8c2d7385cf6e963c910412f72dedf
Andreas Pettersson <appettersson@hotmail.com>
2025-10-17 16:20:06 +0200

    fixed bug that sets tot and toa to 0 if we don't do a totscan (#223)
    * fixed bug that sets tot and toa to 0 if we dont do a totscan
    
    * syntax fix
    
    * syntax fix
    
    ---------
    
    Co-authored-by: Andreas Pettersson <andreas.pettersson.6826@student.lu.se>

--- app/tool/tasks/parameter_timescan.cxx
+++ app/tool/tasks/parameter_timescan.cxx
@@ -48,10 +48,13 @@ void parameter_timescan(Target* tgt) {
     test_param_handle.add(refvol_page, "CALIB", preCC ? 0 : calib)
         .add(refvol_page, "CALIB_2V5", preCC ? calib : 0)
         .add(refvol_page, "INTCTEST", 1)
-        .add(refvol_page, "CHOICE_CINJ", (highrange && !preCC) ? 1 : 0)
-        .add(refvol_page, "TOA_VREF", toa_threshold)
-        .add(refvol_page, "TOT_VREF", tot_threshold);
+        .add(refvol_page, "CHOICE_CINJ", (highrange && !preCC) ? 1 : 0);
+    if (totscan) {
+    test_param_handle.add(refvol_page, "TOA_VREF", toa_threshold)
+      .add(refvol_page, "TOT_VREF", tot_threshold);
+    }
   };
+
   if (link[0]) add_rv(0);
   if (link[1]) add_rv(1);
 
