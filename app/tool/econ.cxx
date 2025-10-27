c61582d3adb2e4caabf26a1bafa45fe3ebca4c6e
cmantill <criss.ms7@gmail.com>
2025-10-27 18:00:24 +0000

    fix str

--- app/tool/econ.cxx
+++ app/tool/econ.cxx
@@ -20,7 +20,7 @@ void print_econs(Target* tgt) {
 static void econ_render(Target* tgt) {
   try {
     auto econ{tgt->hcal().econ(pftool::state.iecon)};
-    printf(" Active ECON: %d (%s)\n", pftool::state.iecon, econ.type());
+    printf(" Active ECON: %d (%s)\n", pftool::state.iecon, econ.type().c_str());
   } catch (const std::exception&) {
     printf(" Active ECON: %d (Not Configured)\n", pftool::state.iecon);
     print_econs(tgt);
