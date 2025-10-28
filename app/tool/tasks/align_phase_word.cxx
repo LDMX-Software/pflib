188bff6a36a8f662f206eac30e2ce78c57596289
joshgreaves332 <vve7nj@virginia.edu>
2025-10-28 17:06:55 -0400

    compile

--- app/tool/tasks/align_phase_word.cxx
+++ app/tool/tasks/align_phase_word.cxx
@@ -277,9 +277,9 @@ void align_phase_word(Target* tgt) {
     // value e.g. ./uhal_backend_v3.py -b
     // Housekeeping-FastCommands-fastcontrol-axi-0 --node
     // bx_link_reset_roc${ECON} --val 3516 auto cmdcounters =
-    // tgt->fc().getCmdCounters(); for(uint32_t i : cmdcounters){
-    //   std::cout << i << ", ";
-    // }std::cout << std::endl;
+    tgt->fc().getCmdCounters(); for(uint32_t i : cmdcounters){
+      std::cout << i << ", ";
+    }std::cout << std::endl;
 
     // // econ version exists,
     // //
