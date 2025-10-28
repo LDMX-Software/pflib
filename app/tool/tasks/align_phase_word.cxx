eaf3d1eeda80d741008e01097a53290afc719b58
joshgreaves332 <vve7nj@virginia.edu>
2025-10-28 14:59:51 -0400

    compile

--- app/tool/tasks/align_phase_word.cxx
+++ app/tool/tasks/align_phase_word.cxx
@@ -219,7 +219,7 @@ void align_phase_word(Target* tgt) {
   // // see linkreset_rocs()
 
   //LINK_RESET
-    tgt->fc().linkreset_rocs();     // is this sufficient? Do I need to set a value e.g. ./uhal_backend_v3.py -b Housekeeping-FastCommands-fastcontrol-axi-0 --node bx_link_reset_roc${ECON} --val 3516
+    // tgt->fc().linkreset_rocs();     // is this sufficient? Do I need to set a value e.g. ./uhal_backend_v3.py -b Housekeeping-FastCommands-fastcontrol-axi-0 --node bx_link_reset_roc${ECON} --val 3516
 
     // // econ version exists, 
   // // ------------------------------------------------------------------------------------------------------------ //
