aec6d2e0e3dfb1f3c6700080e5a18921e5c09e4a
joshgreaves332 <vve7nj@virginia.edu>
2025-10-28 10:46:44 -0400

    removing printouts in dump

--- app/tool/tasks/align_phase_word.cxx
+++ app/tool/tasks/align_phase_word.cxx
@@ -6,49 +6,15 @@
 ENABLE_LOGGING();
 
 void align_phase_word(Target* tgt) {
-  auto roc = tgt->hcal().roc(pftool::state.iroc); //, pftool::state.type_version());
-  auto econ = tgt->hcal().econ(pftool::state.iecon); //, pftool::state.type_version());
-
-
-// Do I need something like this implemented?
-
-  // pflib::DecodeAndWriteToCSV writer{
-  //     output_filepath,
-  //     [&](std::ofstream& f) {
-  //       boost::json::object header;
-  //       header["scan_type"] = "CH_#.TOA sweep";
-  //       header["trigger"] = "CHARGE";
-  //       header["nevents_per_point"] = nevents;
-  //       f << "# " << boost::json::serialize(header) << "\n"
-  //         << "CALIB";
-  //       for (int ch{0}; ch < 72; ch++) {
-  //         f << "," << ch;
-  //       }
-  //       f << "\n";
-  //     },
-  //     [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket& ep) {
-  //       f << calib;
-  //       // Write the TOA values for each channel
-  //       for (int ch{0}; ch < 72; ch++) {
-  //         f << "," << ep.channel(ch).toa();
-  //       }
-  //       f << "\n";
-  //     }};
+  auto roc = tgt->hcal().roc(pftool::state.iroc);
+  auto econ = tgt->hcal().econ(pftool::state.iecon);
 
 
 // -------------------------------------------------------------------------------------------------------------------------------- //
 // ----------------------------------------------------- PHASE ALIGNMENT ----------------------------------------------------- //
   int IDLE = 89478485;  // == 5555555. hardcode based on phase alignment script
 
-  // Do I need one of these for the ECON? What does this do?
-  tgt->setup_run(1 /* dummy */, Target::DaqFormat::ECOND_NO_ZS, 2 /* dummy */);
-  tgt->setup_run(1 /* dummy */, Target::DaqFormat::SIMPLEROC, 1 /* dummy */);
-
-
-
   // ---------------------------------- SETTING ROC REGISTERS ---------------------------------- //
-  // Is this how I set ROC parameters appropriately?
-  // --ROC. do these test parameters correspond to e.g. before_state_roc.yaml?
   { // scope this
     auto roc_setup_builder = roc.testParameters()
                             .add("DIGITALHALF_0", "IDLEFRAME", IDLE)
@@ -63,22 +29,17 @@ void align_phase_word(Target* tgt) {
 
 
   // ---------------------------------- SETTING ECON REGISTERS ---------------------------------- //
-  // -- ECON do these test parameters correspond to e.g. before_state.yaml?
-  { // scope this
-    std::cout << "1 test = " << std::endl ;
-    auto econ_setup_builder = econ.testParameters().add("CLOCKSANDRESETS", "GLOBAL_PUSM_RUN", 0); // set run bit 0 while configuring
-    auto econ_setup_test = econ_setup_builder.apply();
-    auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");   // dump Parameter I added to force this functionality to be able to assign to an output variable
-    auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE"); 
-    std::cout << "First PUSM_STATE = " << pusm_state << std::endl ;
-    std::cout << "First PUSM_RUN = " << pusm_run << std::endl ;
-  }
-
-
-  // I can manually apply them, eg:
-  // set run bit 0 while configuring
-  // econ.testParameters().add("CLOCKSANDRESETS_GLOBAL", "PUSM_RUN", 0).apply();
-
+  
+  // NOT setting run bit - commenting out
+  // { // scope this
+  //   std::cout << "1 test = " << std::endl ;
+  //   auto econ_setup_builder = econ.testParameters().add("CLOCKSANDRESETS", "GLOBAL_PUSM_RUN", 0); // set run bit 0 while configuring
+  //   auto econ_setup_test = econ_setup_builder.apply();
+  //   auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");   // dump Parameter I added to force this functionality to be able to assign to an output variable
+  //   auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE"); 
+  //   std::cout << "First PUSM_STATE = " << pusm_state << std::endl ;
+  //   std::cout << "First PUSM_RUN = " << pusm_run << std::endl ;
+  // }
 
   //Phase ON
   { // scope this
@@ -100,31 +61,32 @@ void align_phase_word(Target* tgt) {
     auto eprxgrptop = econ.dumpParameter("EPRXGRPTOP","GLOBAL_TRACK_MODE");
     auto cheprxgrp5 = econ.dumpParameter("CHEPRXGRP","5_TRAIN_CHANNEL");
     std::cout << "EPRXGRPTOP = " << eprxgrptop << std::endl ; 
-    std::cout << "CHEPRXGRP5 = " << cheprxgrp5 << std::endl ;   // arbitrarily using 5
+    std::cout << "CHEPRXGRP5 (Example channel 5) = " << cheprxgrp5 << std::endl ;   // arbitrarily using 5 as example
   }
 
 
-  { // scope this
-    // Set run bit ON
-    auto econ_setup_builder = econ.testParameters()
-                            .add("CLOCKSANDRESETS", "GLOBAL_PUSM_RUN", 1);
-    auto econ_setup_test = econ_setup_builder.apply();
-    auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");
-    auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE");
-    std::cout << "PUSM_STATE = " << pusm_state << std::endl ;
-    std::cout << "PUSM_RUN = " << pusm_run << std::endl ;
-  }
+  // { // scope this
+  // NOT setting run bit - commenting out
+  //   // Set run bit ON
+  //   auto econ_setup_builder = econ.testParameters()
+  //                           .add("CLOCKSANDRESETS", "GLOBAL_PUSM_RUN", 1);
+  //   auto econ_setup_test = econ_setup_builder.apply();
+  //   auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");
+  //   auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE");
+  //   std::cout << "PUSM_STATE = " << pusm_state << std::endl ;
+  //   std::cout << "PUSM_RUN = " << pusm_run << std::endl ;
+  // }
 
-  { // scope this
-    // Set run bit OFF
-    auto econ_setup_builder = econ.testParameters()
-                            .add("CLOCKSANDRESETS", "GLOBAL_PUSM_RUN", 0);
-    auto econ_setup_test = econ_setup_builder.apply();
-    auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");
-    auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE");
-    std::cout << "PUSM_STATE = " << pusm_state << std::endl ;
-    std::cout << "PUSM_RUN = " << pusm_run << std::endl ;
-  }
+  // { // scope this
+  //   // Set run bit OFF
+  //   auto econ_setup_builder = econ.testParameters()
+  //                           .add("CLOCKSANDRESETS", "GLOBAL_PUSM_RUN", 0);
+  //   auto econ_setup_test = econ_setup_builder.apply();
+  //   auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");
+  //   auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE");
+  //   std::cout << "PUSM_STATE = " << pusm_state << std::endl ;
+  //   std::cout << "PUSM_RUN = " << pusm_run << std::endl ;
+  // }
 
 
   { // scope this
@@ -144,20 +106,21 @@ void align_phase_word(Target* tgt) {
                             .add("CHEPRXGRP", "11_TRAIN_CHANNEL", 0); 
     auto econ_setup_test = econ_setup_builder.apply();  
     auto cheprxgrp5 = econ.dumpParameter("CHEPRXGRP","5_TRAIN_CHANNEL");
-    std::cout << "CHEPRXGRP5 = " << cheprxgrp5 << std::endl ;
+    std::cout << "CHEPRXGRP5 (Example channel 5) = " << cheprxgrp5 << std::endl ;
   }
 
 
-  { // scope this
-    // Set run bit ON
-    auto econ_setup_builder = econ.testParameters()
-                            .add("CLOCKSANDRESETS", "GLOBAL_PUSM_RUN", 1);
-    auto econ_setup_test = econ_setup_builder.apply();
-    auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");
-    auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE");
-    std::cout << "PUSM_STATE = " << pusm_state << std::endl ;
-    std::cout << "PUSM_RUN = " << pusm_run << std::endl ;
-  }
+  // { // scope this
+  // // NOT setting run bit - commenting out
+  //   // Set run bit ON
+  //   auto econ_setup_builder = econ.testParameters()
+  //                           .add("CLOCKSANDRESETS", "GLOBAL_PUSM_RUN", 1);
+  //   auto econ_setup_test = econ_setup_builder.apply();
+  //   auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");
+  //   auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE");
+  //   std::cout << "PUSM_STATE = " << pusm_state << std::endl ;
+  //   std::cout << "PUSM_RUN = " << pusm_run << std::endl ;
+  // }
 
   { // scope this
     // Set Training OFF
@@ -176,50 +139,46 @@ void align_phase_word(Target* tgt) {
                             .add("CHEPRXGRP", "11_CHANNEL_LOCKED", 1);
     auto econ_setup_test = econ_setup_builder.apply();  
     auto cheprxgrp5 = econ.dumpParameter("CHEPRXGRP","5_CHANNEL_LOCKED");
-    std::cout << "CHEPRXGRP5 = " << cheprxgrp5 << std::endl ;  
-  }
-                          ;
-  // auto econ_setup_test = econ_setup_builder.apply();
+    std::cout << "CHEPRXGRP5 (Example channel 5) = " << cheprxgrp5 << std::endl ;  
+  };
   // -------------------------------------------------------------------------------------------- //
   
 
   // ---------------------------------- READING ECON REGISTERS ---------------------------------- //
 
   // Read PUSH
-  // .add("CLOCKSANDRESETS_GLOBAL", "PUSM_STATE", 0)
   auto pusm_run = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_RUN");   // dump Parameter I added to force this functionality to be able to assign to an output variable
   auto pusm_state = econ.dumpParameter("CLOCKSANDRESETS","GLOBAL_PUSM_STATE");
     
-  // auto eprxgrptop = econ.dumpParameter("EPRXGRPTOP","GLOBAL_TRACK_MODE");  
   std::cout << "PUSM_STATE = " << pusm_state << std::endl ;
   std::cout << "PUSM_RUN = " << pusm_run << std::endl ;
   //" (0x" << std::hex << pusm_state << std::dec << ")\n";
 
-  // Check phase_select
-  auto phase_sel_0 = econ.dumpParameter("CHEPRXGRP", "0_CHANNEL_LOCKED");     // can also readParameters()
-  auto phase_sel_1 = econ.dumpParameter("CHEPRXGRP", "1_CHANNEL_LOCKED"); 
-  auto phase_sel_2 = econ.dumpParameter("CHEPRXGRP", "2_CHANNEL_LOCKED"); 
-  auto phase_sel_3 = econ.dumpParameter("CHEPRXGRP", "3_CHANNEL_LOCKED"); 
-  auto phase_sel_4 = econ.dumpParameter("CHEPRXGRP", "4_CHANNEL_LOCKED"); 
-  auto phase_sel_5 = econ.dumpParameter("CHEPRXGRP", "5_CHANNEL_LOCKED"); 
-  auto phase_sel_6 = econ.dumpParameter("CHEPRXGRP", "6_CHANNEL_LOCKED"); 
-  auto phase_sel_7 = econ.dumpParameter("CHEPRXGRP", "7_CHANNEL_LOCKED"); 
-  auto phase_sel_8 = econ.dumpParameter("CHEPRXGRP", "8_CHANNEL_LOCKED"); 
-  auto phase_sel_9 = econ.dumpParameter("CHEPRXGRP", "9_CHANNEL_LOCKED"); 
-  auto phase_sel_10 = econ.dumpParameter("CHEPRXGRP", "10_CHANNEL_LOCKED"); 
-  auto phase_sel_11 = econ.dumpParameter("CHEPRXGRP", "11_CHANNEL_LOCKED"); 
+  // Check channel lock
+  auto ch_lock_0 = econ.dumpParameter("CHEPRXGRP", "0_CHANNEL_LOCKED");     // can also readParameters()
+  auto ch_lock_1 = econ.dumpParameter("CHEPRXGRP", "1_CHANNEL_LOCKED"); 
+  auto ch_lock_2 = econ.dumpParameter("CHEPRXGRP", "2_CHANNEL_LOCKED"); 
+  auto ch_lock_3 = econ.dumpParameter("CHEPRXGRP", "3_CHANNEL_LOCKED"); 
+  auto ch_lock_4 = econ.dumpParameter("CHEPRXGRP", "4_CHANNEL_LOCKED"); 
+  auto ch_lock_5 = econ.dumpParameter("CHEPRXGRP", "5_CHANNEL_LOCKED"); 
+  auto ch_lock_6 = econ.dumpParameter("CHEPRXGRP", "6_CHANNEL_LOCKED"); 
+  auto ch_lock_7 = econ.dumpParameter("CHEPRXGRP", "7_CHANNEL_LOCKED"); 
+  auto ch_lock_8 = econ.dumpParameter("CHEPRXGRP", "8_CHANNEL_LOCKED"); 
+  auto ch_lock_9 = econ.dumpParameter("CHEPRXGRP", "9_CHANNEL_LOCKED"); 
+  auto ch_lock_10 = econ.dumpParameter("CHEPRXGRP", "10_CHANNEL_LOCKED"); 
+  auto ch_lock_11 = econ.dumpParameter("CHEPRXGRP", "11_CHANNEL_LOCKED"); 
 
-  std::cout << "phase select 0 = " << phase_sel_0 << std::endl ;
-  std::cout << "phase select 1 = " << phase_sel_1 << std::endl ;
-  std::cout << "phase select 2 = " << phase_sel_2 << std::endl ;
-  std::cout << "phase select 3 = " << phase_sel_3 << std::endl ;
-  std::cout << "phase select 4 = " << phase_sel_4 << std::endl ;
-  std::cout << "phase select 6 = " << phase_sel_6 << std::endl ;
-  std::cout << "phase select 7 = " << phase_sel_7 << std::endl ;
-  std::cout << "phase select 8 = " << phase_sel_8 << std::endl ;
-  std::cout << "phase select 9 = " << phase_sel_9 << std::endl ;
-  std::cout << "phase select 10 = " << phase_sel_10 << std::endl ;
-  std::cout << "phase select 11 = " << phase_sel_11 << std::endl ;
+  std::cout << "channel_locked 0 = " << ch_lock_0 << std::endl ;
+  std::cout << "channel_locked 1 = " << ch_lock_1 << std::endl ;
+  std::cout << "channel_locked 2 = " << ch_lock_2 << std::endl ;
+  std::cout << "channel_locked 3 = " << ch_lock_3 << std::endl ;
+  std::cout << "channel_locked 4 = " << ch_lock_4 << std::endl ;
+  std::cout << "channel_locked 6 = " << ch_lock_6 << std::endl ;
+  std::cout << "channel_locked 7 = " << ch_lock_7 << std::endl ;
+  std::cout << "channel_locked 8 = " << ch_lock_8 << std::endl ;
+  std::cout << "channel_locked 9 = " << ch_lock_9 << std::endl ;
+  std::cout << "channel_locked 10 = " << ch_lock_10 << std::endl ;
+  std::cout << "channel_locked 11 = " << ch_lock_11 << std::endl ;
 
 // --------------------------------------------------------------------- //
 
@@ -347,7 +306,7 @@ int INVERT_FCMD = 1; // Should this be 1 or 0? (See Econ align.sh)
   // sets when snapshot is going to be taken
   { // scope this
     auto econ_setup_builder = econ.testParameters()
-                            .add("ALIGNER", "GLOBAL_ORBSYN_CNT_SNAPSHOT", 0);           // UNSURE what this parameter should be. "snapshot location BX" or "ORBSYNCSNAPSHOT"
+                            .add("ALIGNER", "GLOBAL_ORBSYN_CNT_SNAPSHOT", 0);
     auto econ_setup_test = econ_setup_builder.apply();
   }
 //   // ---------------------------------- --------------------------------------------------------- //
@@ -363,16 +322,13 @@ std::cout << "Orbsyn_cnt_load_val = " << cnt_load_val << std::endl ;
 //       // check_econd_roc_alignment.yaml
 //   // ---------------------------------- --------------------------------------------------------- //
 
-  
-          // DO I need a second round of alignment settings?     //Configure ECOND for Alignment (from econd_init_cpp.yaml)
-
 
 
 //   // ---------------------------------------------- FAST CONTROL -------------------------------------------- //
 // // see linkreset_rocs()
 
 //LINK_RESET
-  tgt->fc().linkreset_rocs();     // is this sufficient? Do I need to set a value e.g. ./uhal_backend_v3.py -b Housekeeping-FastCommands-fastcontrol-axi-0 --node bx_link_reset_roc${ECON} --val 3516
+  // tgt->fc().linkreset_rocs();     // is this sufficient? Do I need to set a value e.g. ./uhal_backend_v3.py -b Housekeeping-FastCommands-fastcontrol-axi-0 --node bx_link_reset_roc${ECON} --val 3516
 
   // // econ version exists, 
 // // ------------------------------------------------------------------------------------------------------------ //
@@ -435,9 +391,6 @@ std::cout << "chAligner select 4 = " << ch_select_4 << std::endl ;
 std::cout << "chAligner pattern_match 5 = " << ch_pm_5 << std::endl ;
 std::cout << "chAligner snapshot_dv 5 = " << ch_snap_dv_5 << std::endl ;
 std::cout << "chAligner select 5 = " << ch_select_5 << std::endl ;
-
-//       // read back econ_align.sh registers.
-//       // check_econd_roc_alignment.yaml
 //   // ---------------------------------- --------------------------------------------------------- //
 
 
@@ -446,34 +399,6 @@ std::cout << "chAligner select 5 = " << ch_select_5 << std::endl ;
 // // -------------------------------------------------------------------------------------------------------------------------------- //
 
 
-
-
-
-
-
-
-
-
-
-  // // Do I need one of these for the ECON?
-  // tgt->daq_run("CHARGE", writer, nevents, pftool::state.daq_rate);
-
-
-
-  // for (int ch{0}; ch < 72; ch++) {
-  //   setup_builder.add("CH_" + std::to_string(ch), "LOWRANGE", 1);
-  // }
-  // auto setup_test = setup_builder.apply();
-
-  // for (calib = 0; calib < 800; calib += 4) {
-  //   pflib_log(info) << "Running CALIB = " << calib;
-  //   // Set the CALIB parameters for both halves
-  //   auto calib_test = roc.testParameters()
-  //                         .add("REFERENCEVOLTAGE_0", "CALIB", calib)
-  //                         .add("REFERENCEVOLTAGE_1", "CALIB", calib)
-  //                         .apply();
-  //   tgt->daq_run("CHARGE", writer, nevents, pftool::state.daq_rate);
-  // }
 
 
 }
