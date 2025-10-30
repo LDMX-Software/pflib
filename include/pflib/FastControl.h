5b5c66698a3683afbd39ce096c40c743d4daa4fc
joshgreaves332 <vve7nj@virginia.edu>
2025-10-30 16:29:34 -0400

    compile

--- include/pflib/FastControl.h
+++ include/pflib/FastControl.h
@@ -33,6 +33,9 @@ class FastControl {
   /** send a link reset */
   virtual void linkreset_rocs() = 0;
 
+  /** set custom bunch crossing */
+  virtual void bx_custom(int bx_addr, int bx_mask, int bx_new) = 0;
+
   /** send a buffer clear */
   virtual void bufferclear() = 0;
 
