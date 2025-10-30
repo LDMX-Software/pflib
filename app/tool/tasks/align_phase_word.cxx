2d93a1afa83b82bc73752cbfc7d1ca6a6aff8e74
joshgreaves332 <vve7nj@virginia.edu>
2025-10-30 19:59:03 -0400

    compile

--- app/tool/tasks/align_phase_word.cxx
+++ app/tool/tasks/align_phase_word.cxx
@@ -19,7 +19,8 @@ void align_phase_word(Target* tgt) {
   // SET and Read PUSH runbit; also ensure GLOBAL_INVERT_COMMAND_RX is set to
   // get ECON to lock to FCMDs; otherwise run bit will = 3.
   // Scope 2 builders to make this happen in sequence
-  {  // set inversion value
+  
+  // {  // set inversion value
   //   auto econ_setup_builder = econ.testParameters().add(
   //       "FCTRL", "GLOBAL_INVERT_COMMAND_RX",
   //       1);  // set fctrl inversion so that ECON can Lock.
