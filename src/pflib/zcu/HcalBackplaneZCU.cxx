677e9366b6e37959eb02a04d57faff8ae97276ed
Jeremiah Mans <jmmans@umn.edu>
2025-11-02 11:12:23 -0600

    Add implementation of DAQ functions

--- src/pflib/zcu/HcalBackplaneZCU.cxx
+++ src/pflib/zcu/HcalBackplaneZCU.cxx
@@ -79,10 +79,52 @@ class OptoElinksZCU : public Elinks {
 };
 
 class HcalBackplaneZCU_Capture : public DAQ {
+  static constexpr uint32_t ADDR_IDLE_PATTERN  = 0x604;
+  static constexpr uint32_t ADDR_HEADER_MARKER = 0x600;
+  static constexpr uint32_t MASK_HEADER_MARKER = 0x0001FF00;
+  static constexpr uint32_t ADDR_ENABLE        = 0x600;
+  static constexpr uint32_t MASK_ENABLE        = 0x00000001;
+  static constexpr uint32_t ADDR_EVB_CLEAR     = 0x100;
+  static constexpr uint32_t MASK_EVB_CLEAR     = 0x00000001;
+  static constexpr uint32_t ADDR_ADV_IO        = 0x080;
+  static constexpr uint32_t MASK_ADV_IO        = 0x00000001;
+  static constexpr uint32_t ADDR_ADV_AXIS      = 0x080;
+  static constexpr uint32_t MASK_ADV_AXIS      = 0x00000002;
+
+  static constexpr uint32_t ADDR_PACKET_SETUP  = 0x400;
+  static constexpr uint32_t MASK_ECON_ID       = 0x000003FF;
+  static constexpr uint32_t MASK_L1A_PER_PACKET= 0x00007C00;
+  static constexpr uint32_t MASK_SOI           = 0x000F8000;
+  static constexpr uint32_t AXIS_ENABLE        = 0x80000000;
+  
+  static constexpr uint32_t ADDR_UPPER_ADDR    = 0x404;
+  static constexpr uint32_t MASK_UPPER_ADDR    = 0x0000003F;
+  
+  static constexpr uint32_t ADDR_INFO          = 0x800;
+  static constexpr uint32_t MASK_IO_NEVENTS    = 0x0000007F;
+  static constexpr uint32_t MASK_IO_SIZE_NEXT  = 0x0000FF80;
+  static constexpr uint32_t MASK_AXIS_NWORDS   = 0x1FFF0000;
+  static constexpr uint32_t MASK_TVALID_DAQ    = 0x20000000;
+  static constexpr uint32_t MASK_TREADY_DAQ    = 0x40000000;
+  
  public:
-  HcalBackplaneZCU_Capture() : DAQ(1), capture_("econd-buffer-0") {}
-  virtual void reset() {}
-  virtual int getEventOccupancy() {}
+  HcalBackplaneZCU_Capture() : DAQ(1), capture_("econd-buffer-0") {
+    // setting up with expected capture parameters
+    capture_.write(ADDR_IDLE_PATTERN, 0x12);
+    capture_.writeMasked(ADDR_HEADER_MARKER,MASK_HEADER_MARKER,0x0);
+    per_econ_=true; // reading from the per-econ buffer, not the AXIS buffer
+  }
+  virtual void reset() {
+    capture_.write(ADDR_EVB_CLEAR,MASK_EVB_CLEAR); // auto-clear
+  }
+  virtual int getEventOccupancy() {
+    capture_.writeMasked(ADDR_UPPER_ADDR,MASK_UPPER_ADDR,0); // get on page 0
+    if (per_econ) 
+      return capture_.readMasked(ADDR_INFO,MASK_IO_NEVENTS);
+    else
+      if (capture_.readMasked(ADDR_INFO,MASK_AXIS_NWORDS)!=0) return 1;
+      else return 0;
+  }
   virtual void setupLink(int ilink, int l1a_delay, int l1a_capture_width) {
     // none of these parameters are relevant for the econd capture, which is
     // data-pattern based
@@ -91,18 +133,57 @@ class HcalBackplaneZCU_Capture : public DAQ {
     l1a_delay = -1;
     l1a_capture_width = -1;
   }
-  virtual void bufferStatus(int ilink, bool& empty, bool& full) {}
+  virtual void bufferStatus(int ilink, bool& empty, bool& full) {
+    int nevt=getEventOccupancy();
+    empty=(nevt==0);
+    full=(nevt==0x7f);
+  }
   virtual void setup(int econid, int samples_per_ror, int soi) {
     ::DAQ(econid, samples_per_ror, soi);
+    capture_.writeMasked(ADDR_PACKET_SETUP,MASK_ECON_ID,econid);
+    capture_.writeMasked(ADDR_PACKET_SETUP,MASK_L1A_PER_PACKET,samples_per_ror);
+    capture_.writeMasked(ADDR_PACKET_SETUP,MASK_SOI,soi);
   }
-  virtual void enable(bool doenable) {}
-  virtual std::vector<uint32_t> getLinkData(int ilink) {}
-  virtual void advanceLinkReadPtr() {}
+  virtual void enable(bool doenable) {
+    if (doenable) capture_.rmw(ADDR_ENABLE,MASK_ENABLE,MASK_ENABLE);
+    else capture_.rmw(ADDR_ENABLE,MASK_ENABLE,0);
+    if (!per_econ_ && doenable) capture_.rmw(ADDR_PACKET_SETUP,AXIS_ENABLE,AXIS_ENABLE);
+    else capture_.rmw(ADDR_PACKET_SETUP,AXIS_ENABLE,0);
+  }
+  virtual std::vector<uint32_t> getLinkData(int ilink) {
+    uint32_t words=0;
+    std::vector<uint32_t> retval;
+    static const uint32_t UBITS = 0x3F00;
+    static const uint32_t LBITS = 0x00FF;
+    
+    if (per_econ_) 
+      words=capture_.readMasked(ADDR_INFO,MASK_IO_SIZE_NEXT);
+    else
+      words=capture_.readMasked(ADDR_INFO,MASK_AXIS_NWORDS);
+
+    uint32_t iold=0xFFFFFF;
+    for (uint32_t i=0; i<words; i++) {
+      if ((iold&UBITS)!=(i&UBITS))  // new upper address block
+        if (per_econ_)
+          capture_.writeMasked(ADDR_UPPER_ADDR,MASK_UPPER_ADDR,(i>>8)|0x04);
+        else
+          capture_.writeMasked(ADDR_UPPER_ADDR,MASK_UPPER_ADDR,(i>>8)|0x20);
+      retval.push_back(capture_.read(ADDR_PAGED_READ+(i&LBITS)));
+    }
+    return retval;
+  }
+  virtual void advanceLinkReadPtr() {
+    if (per_econ_)
+      capture_.write(ADDR_ADV_IO,MASK_ADV_IO); // auto-clear
+    else
+      capture_.write(ADDR_ADV_AXIS,MASK_ADV_AXIS); // auto-clear
+  }
 
   virtual std::map<std::string, uint32_t> get_debug(uint32_t ask) {}
 
  private:
   UIO capture_;
+  bool per_econ_;
 };
 
 class HcalBackplaneZCU : public Hcal {
