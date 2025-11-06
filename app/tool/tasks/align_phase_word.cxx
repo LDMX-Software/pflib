c12704a949bc72cbdc5fed3728157be04634ee43
joshgreaves332 <vve7nj@virginia.edu>
2025-11-06 15:25:28 -0500

    TESTING

--- app/tool/tasks/align_phase_word.cxx
+++ app/tool/tasks/align_phase_word.cxx
@@ -299,8 +299,8 @@ void align_phase_word(Target* tgt) {
                 ["GLOBAL_ORBSYN_CNT_SNAPSHOT"] =  //   BX value econ takes its
                                                   //   snapshot
           3533;  // 0xdcd //    3532;  // 0xdcc  // 3080;  // 0xc08
-      parameters["ALIGNER"]["GLOBAL_MATCH_PATTERN_VAL"] =
-          1789569706;  // 0x6aaaaaaa;
+      parameters["ALIGNER"]["GLOBAL_MATCH_PATTERN_VAL"] = 10760600711006082389ULL;  // 0x95555555a5555555
+          // 1789569706;  // 0x6aaaaaaa;
                        // 2505397589;  // 0x95555555 // =
                        // 10760600711006082389ULL;  // 0x95555555a5555555
       // = 2505397589;  // 0x95555555  // this will be inverted by inversion bit
