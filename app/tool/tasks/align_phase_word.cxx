489b241f0e459cbf17a387ef19dc7d2f155aecd8
joshgreaves332 <vve7nj@virginia.edu>
2025-11-07 16:28:45 -0500

    fixing git issues

--- app/tool/tasks/align_phase_word.cxx
+++ app/tool/tasks/align_phase_word.cxx
@@ -291,7 +291,7 @@ void align_phase_word(Target* tgt) {
 
       // ------- SCAN BUNCH CROSSINGS ------- //
       int snapshot_6bx;
-      int start_val = 3530; //0;   // near your orbit region of interest
+      int start_val = 3503; //0;   // near your orbit region of interest
       int end_val = 3563;  // up to orbit rollover
       int testval = 3532; 
 
