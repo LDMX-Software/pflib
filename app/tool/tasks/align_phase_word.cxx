125bd6b5fd80a0a87b4e811930a24019d571a0e5
joshgreaves332 <vve7nj@virginia.edu>
2025-11-10 15:01:11 -0500

    implementing shift and mask on found header

--- app/tool/tasks/align_phase_word.cxx
+++ app/tool/tasks/align_phase_word.cxx
@@ -260,8 +260,7 @@ void align_phase_word(Target* tgt) {
               << ", 0x" << std::hex << ch_select << std::dec
               << std::endl;
             
-          std::cout << "256bit snapshot: " << std::hex << std::uppercase << snapshot 
-          << std::dec << std::endl;
+          // shift and mask the snapshot to confirm special header
           boost::multiprecision::uint256_t shifted = (snapshot >> (ch_select - 32)) & 0xffffffffffffffffULL;
           std::cout << "Shifted and masked: 0x" << std::hex << std::uppercase << shifted 
           << std::dec << std::endl;
