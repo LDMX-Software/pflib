06c98dfa96d0663b3cf2e26f6b2cbbaf08baac84
tomeichlersmith <eichl008@umn.edu>
2025-11-11 09:40:39 -0600

    outline what I'm thinking for a PyTarget
    std::unique_ptr prevents the copy constructor from being deduced but the
    python bindings need a copy constructor, maybe implement my own, maybe
    bind `pflib::Target` with `no_init` and then bind the factory fucntions
    for creation

--- src/pflib/python/bindings.cxx
+++ src/pflib/python/bindings.cxx
@@ -1,10 +1,33 @@
 #include <boost/python.hpp>
 
-char const* greet() {
-  return "hello, world";
+#include "pflib/Target.h"
+
+class PyTarget {
+  std::unique_ptr<pflib::Target> tgt_;
+ public:
+  PyTarget(/* determine which type of pflib::Target is made */);
+  void configure(/* how to pass configuration */);
+  void start_run();
+  void end_run();
+};
+
+PyTarget::PyTarget() {
+}
+
+void PyTarget::configure() {
+}
+
+void PyTarget::start_run() {
+}
+
+void PyTarget::end_run() {
 }
 
 BOOST_PYTHON_MODULE(pypflib) {
   using namespace boost::python;
-  def("greet", greet);
+  class_<PyTarget>("PyTarget")
+    .def("configure", &PyTarget::configure)
+    .def("start_run", &PyTarget::start_run)
+    .def("end_run", &PyTarget::end_run)
+  ;
 }
