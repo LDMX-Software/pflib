a61366799c7e6df53110f28b146214a3eff8ee2d
tomeichlersmith <eichl008@umn.edu>
2025-11-12 12:36:42 -0600

    working from a file of ECOND pedestals

---
+++ include/pflib/packing/SoftWrappedECONDEventPacket.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "pflib/packing/ECONDEventPacket.h"
+#include "pflib/packing/Reader.h"
+
+namespace pflib::packing {
+
+/**
+ * Unpack an event packet that includes an extra header inserted by
+ * the software
+ *
+ * @see ECONDEventPacket for how a single sample from a single ECOND
+ * is unpacked.
+ */
+class SoftWrappedECONDEventPacket {
+  /// handle to logging source
+  mutable ::pflib::logging::logger the_log_{::pflib::logging::get("decoding")};
+
+ public:
+  /// provide number of links (eRx) on the ECOND
+  SoftWrappedECONDEventPacket(std::size_t n_links);
+  /**
+   * Corruption bits
+   *
+   * Index | Description
+   * ------|------------
+   *  0    | sw header version mismatch
+   */
+  std::array<bool, 1> corruption;
+  /// L1A index for this packet
+  int il1a;
+  /// contributor ID specifying ECOND
+  int contrib_id;
+  /// whether this packet is the sample-of-interest
+  bool is_soi;
+  /// actual data packet from ECOND
+  ECONDEventPacket data;
+  /// unpack the given data into this structure
+  void from(std::span<uint32_t> frame);
+  /// read into this structure from the input Reader
+  Reader& read(Reader& r);
+};
+
+}  // namespace pflib::packing
