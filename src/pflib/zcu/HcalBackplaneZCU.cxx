d60cbf450f4be6191082ff45b65920aa452eb52d
tomeichlersmith <eichl008@umn.edu>
2025-11-15 11:42:20 -0600

    cleanup zcu target, remove incorrect comments

--- src/pflib/zcu/HcalBackplaneZCU.cxx
+++ src/pflib/zcu/HcalBackplaneZCU.cxx
@@ -365,7 +365,6 @@ class HcalBackplaneZCU : public HcalBackplane {
   }
 
  private:
-  /// let the target that holds this Hcal see our members
   std::unique_ptr<pflib::zcu::lpGBT_ICEC_Simple> daq_tport_, trig_tport_;
   std::unique_ptr<lpGBT> daq_lpgbt_, trig_lpgbt_;
   std::shared_ptr<pflib::I2C> roc_i2c_, econ_i2c_;
@@ -374,7 +373,6 @@ class HcalBackplaneZCU : public HcalBackplane {
   std::shared_ptr<pflib::FastControl> fc_;
   Target::DaqFormat format_;
   int contrib_id_;
-  std::shared_ptr<HcalBackplaneZCU> hcal_;
 };
 
 Target* makeTargetHcalBackplaneZCU(int ilink, uint8_t board_mask) {
