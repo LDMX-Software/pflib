3c0bc23680c54694c2dd79b47f8083007615e077
tomeichlersmith <eichl008@umn.edu>
2025-11-21 08:45:54 -0800

    use differnet build dir for conda to avoid clash

--- env/conda/build.sh
+++ env/conda/build.sh
@@ -1,8 +1,8 @@
 set -o nounset
 set -o errexit
 
-cmake -B build -S . -DCMAKE_INSTALL_PREFIX=${PREFIX}
-cmake --build build --target install -- -j ${CPU_COUNT}
+cmake -B conda-build -S . -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake --build conda-build --target install -- -j ${CPU_COUNT}
 
 # filthy hack to get pypflib into the PYTHONPATH without having to use pip
 pyrogue_dir=$(pip show rogue | grep Location | cut -f 2 -d ':' | sed "s|${BUILD_PREFIX}|${PREFIX}|")
