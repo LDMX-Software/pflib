b5b0fa85200b253fc888424222af4c428fa89baf
Jeremiah Mans <jmmans@umn.edu>
2025-11-20 22:47:46 -0600

    daq and elinks initial drafts, not fully ready

---
+++ include/pflib/bittware/bittware_daq.h
@@ -0,0 +1,33 @@
+#include "pflib/DAQ.h"
+#include "pflib/bittware/bittware_axilite.h"
+
+namespace pflib {
+namespace bittware {
+
+class HcalBackplaneBW_Capture : public DAQ {
+ public:
+  HcalBackplaneBW_Capture(const char* dev = "/dev/datadev_0");
+  virtual void reset();
+  virtual int getEventOccupancy();
+  virtual void setupLink(int ilink, int l1a_delay, int l1a_capture_width) {
+    // none of these parameters are relevant for the econd capture, which is
+    // data-pattern based
+  }
+  virtual void getLinkSetup(int ilink, int& l1a_delay, int& l1a_capture_width) {
+    l1a_delay = -1;
+    l1a_capture_width = -1;
+  }
+  virtual void bufferStatus(int ilink, bool& empty, bool& full);
+  virtual void setup(int econid, int samples_per_ror, int soi);
+  virtual void enable(bool doenable);
+  virtual bool enabled();
+  virtual std::vector<uint32_t> getLinkData(int ilink);
+  virtual void advanceLinkReadPtr();
+  virtual std::map<std::string, uint32_t> get_debug(uint32_t ask);
+ private:
+  AxiLite capture_;
+  bool per_econ_;
+};
+
+}
+}  // namespace pflib
---
+++ src/pflib/bittware/bittware_elinks.cxx
@@ -0,0 +1,35 @@
+#include "pflib/bittware/bittware_elinks.h"
+#include <unistd.h>
+
+namespace pflib {
+namespace bittware {
+
+static constexpr uint32_t QUAD_CODER0_BASE_ADDRESS =
+      0x3000;  // compiled into the firmware
+
+OptoElinksBW::OptoElinksBW(int ilink, const char* dev)
+      : Elinks(1),
+        ilinkOpto_(ilink),
+        axil_(QUAD_CODER0_BASE_ADDRESS,dev) {
+  }
+std::vector<uint32_t> OptoElinksBW::spy(int ilink) {
+  std::vector<uint32_t> retval;
+  // spy now...
+  static constexpr int REG_PULSE                = 0x100;
+  static constexpr int BIT_SPY_START            = 16;
+  static constexpr int REG_READ_WHICH           = 0x600;
+  static constexpr int MASK_READ_WHICH          = 0xFF00;
+  static constexpr int REG_SPY_BASE             = 0x880;
+  
+  axil_.write(REG_PULSE,(1<<BIT_SPY_START));
+  axil_.writeMasked(REG_READ_WHICH,MASK_READ_WHICH,ilink);
+  usleep(1000);
+  for (int i = 0; i < 32; i++) {
+    usleep(1);
+    retval.push_back(axil_.read(REG_SPY_BASE+i*4));
+  }
+  return retval;
+}
+
+}
+}  // namespace pflib
