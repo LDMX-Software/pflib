ca5e032d9d44fe0b436dc745f4d61297b3c1c250
Jeremiah Mans <jmmans@physics.umn.edu>
2025-11-21 11:44:45 -0600

    Adjust fastcontrol enables, econ align task (#278)
    * add as option to pftool main
    
    * cleanup zcu target, remove incorrect comments
    
    * able to start constructing a BW target, failing when trying to do standard setup
    
    * remove /4 since bittware_axilite is byte-based addressing
    
    * fixup olink names, fixup base addr for rocs relative to backplane
    
    * throw exception if failed to read one reg
    
    * leave in example of how to check fw vers
    
    * store what we know in comments
    
    * parse link for --bw arg
    
    * Apply clang-format --style=Google
    
    * daq and elinks initial drafts, not fully ready
    
    * Adjust the enables for FC
    
    * Implement alignment task
    
    * Apply clang-format --style=Google
    
    ---------
    
    Co-authored-by: tomeichlersmith <eichl008@umn.edu>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Jeremiah Mans <jmmans@umn.edu>

--- app/tool/tasks/align_econ_lpgbt.cxx
+++ app/tool/tasks/align_econ_lpgbt.cxx
@@ -3,19 +3,26 @@
 void align_econ_lpgbt(Target* tgt) {
   auto econ = tgt->econ(pftool::state.iecon);
 
-  int chipaddr = 0x78;
-  chipaddr |= 0x4;
+  if (pftool::state.iecon != 0) {
+    printf(" I only know how to align ECON-D to link 0\n");
+    return;
+  }
+  uint32_t idle = pftool::readline_int("Idle pattern", 0x1277CC, true);
 
-  pflib::zcu::lpGBT_ICEC_Simple ic("standardLpGBTpair-0", false, chipaddr);
-  pflib::lpGBT lpgbt_daq(ic);
-  pflib::zcu::lpGBT_ICEC_Simple ec("standardLpGBTpair-0", true, 0x78);
-  pflib::lpGBT lpgbt_trg(ec);
-
-  int pusm_daq = lpgbt_daq.status();
-  printf(" lpGBT-DAQ PUSM %s (%d)\n", lpgbt_daq.status_name(pusm_daq).c_str(),
-         pusm_daq);
-
-  int pusm_trg = lpgbt_trg.status();
-  printf(" lpGBT-TRG PUSM %s (%d)\n", lpgbt_trg.status_name(pusm_trg).c_str(),
-         pusm_trg);
+  std::vector<uint32_t> got;
+  for (int phase = 0; phase < 32; phase++) {
+    econ.applyParameter("FormatterBuffer", "Global_align_serializer_0", phase);
+    usleep(1000);
+    std::vector<uint32_t> spy = tgt->elinks().spy(0);
+    got.push_back(spy[0]);
+    uint32_t obs = spy[0] >> 8;
+    if (obs == idle) {
+      printf("Found alignment at %d\n", phase);
+      return;
+    }
+  }
+  for (int phase = 0; phase < 32; phase++) {
+    printf(" %2d 0x%08x\n", phase, got[phase]);
+  }
+  printf("Did not find alignment\n");
 }
