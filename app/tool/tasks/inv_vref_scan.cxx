32d23b0e102b7d14d16bf1f62565f26ddea42e0a
joshgreaves332 <vve7nj@virginia.edu>
2025-11-23 20:25:27 -0500

    implementing helper function for inv_vref_scan, to allow for daq format dependent behaviour

--- app/tool/tasks/inv_vref_scan.cxx
+++ app/tool/tasks/inv_vref_scan.cxx
@@ -6,19 +6,15 @@
 
 ENABLE_LOGGING();
 
-void inv_vref_scan(Target* tgt) {
-  int nevents = pftool::readline_int("Number of events per point: ", 1);
-
-  std::string output_filepath = pftool::readline_path("inv_vref_scan", ".csv");
-  int ch_link0 = pftool::readline_int("Channel to scan on link 0", 17);
-  int ch_link1 = pftool::readline_int("Channel to scan on link 1", 51);
-  int channels[2] = {ch_link0, ch_link1};
-
-  auto roc = tgt->roc(pftool::state.iroc);
-
+// helper function to facilitate EventPacket dependent behaviour
+template <class EventPacket>
+static void inv_vref_scan_writer(Target* tgt, pflib::ROC& roc, size_t nevents,
+                            std::string& output_filepath, int channels[2]) {
+  int link = 0;
   int inv_vref = 0;
+  int i_ch = 0;  // 0–35
 
-  DecodeAndWriteToCSV writer{
+  DecodeAndWriteToCSV<EventPacket> writer{
       output_filepath,
       [&](std::ofstream& f) {
         nlohmann::json header;
@@ -32,15 +28,22 @@ void inv_vref_scan(Target* tgt) {
         }
         f << '\n';
       },
-      [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket& ep) {
+      [&](std::ofstream& f, const EventPacket& ep) {
         f << inv_vref;
         for (int ch : channels) {
-          f << ',' << ep.channel(ch).adc();
+          link = (ch / 36);
+          i_ch = ch % 36;
+          if constexpr (std::is_same_v<EventPacket,pflib::packing::SingleROCEventPacket>) {
+            f << ',' << ep.channel(ch).adc();
+          } else if constexpr (std::is_same_v<EventPacket,pflib::packing::MultiSampleECONDEventPacket>) {
+            f << ',' << ep.samples[ep.i_soi].channel(link, i_ch).adc();
+          }
         }
         f << '\n';
       }};
 
-  tgt->setup_run(1 /* dummy */, Target::DaqFormat::SIMPLEROC, 1 /* dummy */);
+  tgt->setup_run(1 /* dummy - not stored */, pftool::state.daq_format_mode,
+                 1 /* dummy */);
 
   // increment inv_vref in increments of 20. 10 bit value but only scanning to
   // 600
@@ -54,4 +57,64 @@ void inv_vref_scan(Target* tgt) {
     // store current scan state in header for writer access
     daq_run(tgt, "PEDESTAL", writer, nevents, pftool::state.daq_rate);
   }
+}
+
+
+void inv_vref_scan(Target* tgt) {
+  int nevents = pftool::readline_int("Number of events per point: ", 1);
+
+  std::string output_filepath = pftool::readline_path("inv_vref_scan", ".csv");
+  int ch_link0 = pftool::readline_int("Channel to scan on link 0", 17);
+  int ch_link1 = pftool::readline_int("Channel to scan on link 1", 51);
+  int channels[2] = {ch_link0, ch_link1};
+
+  auto roc = tgt->roc(pftool::state.iroc);
+
+
+
+  if (pftool::state.daq_format_mode == Target::DaqFormat::SIMPLEROC) {
+    inv_vref_scan_writer<pflib::packing::SingleROCEventPacket>(
+        tgt, roc, nevents, output_filepath, channels);
+  } else if (pftool::state.daq_format_mode ==
+             Target::DaqFormat::ECOND_SW_HEADERS) {
+    inv_vref_scan_writer<pflib::packing::MultiSampleECONDEventPacket>(
+        tgt, roc, nevents, output_filepath, channels);
+  }
+
+  // DecodeAndWriteToCSV writer{
+  //     output_filepath,
+  //     [&](std::ofstream& f) {
+  //       nlohmann::json header;
+  //       header["scan_type"] = "CH_#.INV_VREF sweep";
+  //       header["trigger"] = "PEDESTAL";
+  //       header["nevents_per_point"] = nevents;
+  //       f << "# " << header << "\n"
+  //         << "INV_VREF";
+  //       for (int ch : channels) {
+  //         f << ',' << ch;
+  //       }
+  //       f << '\n';
+  //     },
+  //     [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket& ep) {
+  //       f << inv_vref;
+  //       for (int ch : channels) {
+  //         f << ',' << ep.channel(ch).adc();
+  //       }
+  //       f << '\n';
+  //     }};
+
+  // tgt->setup_run(1 /* dummy */, Target::DaqFormat::SIMPLEROC, 1 /* dummy */);
+
+  // // increment inv_vref in increments of 20. 10 bit value but only scanning to
+  // // 600
+  // for (inv_vref = 0; inv_vref <= 600; inv_vref += 20) {
+  //   pflib_log(info) << "Running INV_VREF = " << inv_vref;
+  //   // set inv_vref simultaneously for both links
+  //   auto test_param = roc.testParameters()
+  //                         .add("REFERENCEVOLTAGE_0", "INV_VREF", inv_vref)
+  //                         .add("REFERENCEVOLTAGE_1", "INV_VREF", inv_vref)
+  //                         .apply();
+  //   // store current scan state in header for writer access
+  //   daq_run(tgt, "PEDESTAL", writer, nevents, pftool::state.daq_rate);
+  // }
 }
