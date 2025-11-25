066c82a136f7e3bd7e2a3b3758a5e8351eb31600
joshgreaves332 <vve7nj@virginia.edu>
2025-11-25 14:27:33 -0500

    adding template to helper function definitions on those cxxs I just modified

--- app/tool/tasks/set_toa.cxx
+++ app/tool/tasks/set_toa.cxx
@@ -6,25 +6,9 @@
 
 ENABLE_LOGGING();
 
-void set_toa(Target* tgt, pflib::ROC& roc, int channel) {
-  int link = (channel / 36);
-  auto refvol_page = pflib::utility::string_format("REFERENCEVOLTAGE_%d", link);
-  auto channel_page = pflib::utility::string_format("CH_%d", channel);
-  int nevents = 30;
-  double toa_eff{2};
-  // in the calibration documentation, it is suggested to send a "small" charge
-  // injection. Here I used 200 but there is maybe a better value.
-  int calib = 200;
-  auto test_param_handle = roc.testParameters()
-                               .add(refvol_page, "CALIB", calib)
-                               .add(refvol_page, "INTCTEST", 1)
-                               .add(channel_page, "LOWRANGE", 1)
-                               .apply();
-
-  pflib_log(info) << "finding the TOA threshold!";
-  // This class doesn't write to csv. When we just would like to
-  // use the data for setting params.
-  DecodeAndBuffer buffer(nevents);
+template <class EventPacket> 
+void set_toa_runs(Target* tgt, ROC& roc, size_t n_events){
+  DecodeAndBuffer<EventPacket> buffer{n_events};
 
   tgt->setup_run(1 /* dummy - not stored */, Target::DaqFormat::SIMPLEROC,
                  1 /* dummy */);
@@ -49,4 +33,50 @@ void set_toa(Target* tgt, pflib::ROC& roc, int channel) {
   }
   PFEXCEPTION_RAISE("NOTOA", "No TOA threshold was found for channel " +
                                  std::to_string(channel) + "!");
+
+}
+
+void set_toa(Target* tgt, pflib::ROC& roc, int channel) {
+  int link = (channel / 36);
+  auto refvol_page = pflib::utility::string_format("REFERENCEVOLTAGE_%d", link);
+  auto channel_page = pflib::utility::string_format("CH_%d", channel);
+  int nevents = 30;
+  double toa_eff{2};
+  // in the calibration documentation, it is suggested to send a "small" charge
+  // injection. Here I used 200 but there is maybe a better value.
+  int calib = 200;
+  auto test_param_handle = roc.testParameters()
+                               .add(refvol_page, "CALIB", calib)
+                               .add(refvol_page, "INTCTEST", 1)
+                               .add(channel_page, "LOWRANGE", 1)
+                               .apply();
+
+  pflib_log(info) << "finding the TOA threshold!";
+  // This class doesn't write to csv. When we just would like to
+  // use the data for setting params.
+  // DecodeAndBuffer buffer(nevents);
+
+  // tgt->setup_run(1 /* dummy - not stored */, Target::DaqFormat::SIMPLEROC,
+  //                1 /* dummy */);
+  // for (int toa_vref = 100; toa_vref < 250; toa_vref++) {
+  //   auto test_handle =
+  //       roc.testParameters().add(refvol_page, "TOA_VREF", toa_vref).apply();
+  //   daq_run(tgt, "CHARGE", buffer, nevents, pftool::state.daq_rate);
+  //   std::vector<double> toa_data;
+  //   for (const pflib::packing::SingleROCEventPacket& ep : buffer.get_buffer()) {
+  //     auto toa = ep.channel(channel).toa();
+  //     if (toa > 0) {
+  //       toa_data.push_back(toa);
+  //     }
+  //   }
+  //   toa_eff = static_cast<double>(toa_data.size()) / nevents;
+  //   if (toa_eff == 1) {
+  //     roc.applyParameter(refvol_page, "TOA_VREF", toa_vref);
+  //     pflib_log(info) << "the TOA threshold is set to " << toa_vref;
+  //     return;
+  //   }
+  //   toa_vref += 1;
+  // }
+  // PFEXCEPTION_RAISE("NOTOA", "No TOA threshold was found for channel " +
+  //                                std::to_string(channel) + "!");
 }
