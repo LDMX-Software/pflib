828bebdac27f1e6ca11878c32f2ea9e17231ddc1
joshgreaves332 <vve7nj@virginia.edu>
2025-11-26 17:49:50 -0500

    adding packet check to appropriate channel indexing in get_toa_Efficiencies.cxx, same as for other tasks

--- app/tool/algorithm/get_toa_efficiencies.cxx
+++ app/tool/algorithm/get_toa_efficiencies.cxx
@@ -11,9 +11,29 @@ std::array<double, 72> get_toa_efficiencies(
   /// reserve a vector of the appropriate size to avoid repeating allocation
   /// time for all 72 channels
   std::vector<int> toas(data.size());
+  int i_ch = 0;  // 0–35
+  int i_link = 0;
+
   for (int ch{0}; ch < 72; ch++) {
+    i_link = (ch / 36);
+    i_ch = ch % 36;
     for (std::size_t i{0}; i < toas.size(); i++) {
-      toas[i] = data[i].channel(ch).toa();
+
+      if constexpr (std::is_same_v<
+                            EventPacket,
+                            pflib::packing::MultiSampleECONDEventPacket>) {
+        toas[i] = data[i].samples[ep.i_soi].channel(i_link, i_ch).toa();
+      } else if constexpr (std::is_same_v<
+                                EventPacket,
+                                pflib::packing::SingleROCEventPacket>) {
+        toas[i] = data[i].channel(ch).toa();
+      } else {
+        PFEXCEPTION_RAISE("BadConf",
+                          "Unable to do all_channels_to_csv for the "
+                          "currently configured format.");
+      }
+
+      
     }
     /// we assume that the data provided is not empty otherwise the efficiency
     /// calculation is meaningless
