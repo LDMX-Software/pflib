94242a37c40081d2ff1d1c54323ef438c3be2267
joshgreaves332 <vve7nj@virginia.edu>
2025-11-25 20:47:25 -0500

    updating trim_inv_dacb_Scan for dynamic eventpacket type behaviour

--- app/tool/tasks/trim_inv_dacb_scan.cxx
+++ app/tool/tasks/trim_inv_dacb_scan.cxx
@@ -6,14 +6,9 @@
 
 ENABLE_LOGGING();
 
-void trim_inv_dacb_scan(Target* tgt) {
-  int nevents = pftool::readline_int("Number of events per point: ", 1);
-
-  std::string output_filepath =
-      pftool::readline_path("trim_inv_dacb_scan", ".csv");
-
-  auto roc = tgt->roc(pftool::state.iroc);
-
+// helper function to facilitate EventPacket dependent behaviour
+template <class EventPacket>
+static void trim_inv_dacb_scan(Target* tgt, pflib::ROC& roc, size_t nevents){
   int trim_inv = 0;
   int dacb = 0;
 
@@ -31,7 +26,7 @@ void trim_inv_dacb_scan(Target* tgt) {
         }
         f << '\n';
       },
-      [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket& ep) {
+      [&](std::ofstream& f, const EventPacket& ep) {
         f << trim_inv << ',' << dacb;
         for (int ch{0}; ch < 72; ch++) {
           f << ',' << ep.channel(ch).adc();
@@ -39,7 +34,7 @@ void trim_inv_dacb_scan(Target* tgt) {
         f << '\n';
       }};
 
-  tgt->setup_run(1 /* dummy */, Target::DaqFormat::SIMPLEROC, 1 /* dummy */);
+  tgt->setup_run(1 /* dummy - not stored */, pftool::state.daq_format_mode, 1 /* dummy */);
 
   // take pedestal run for dacb parameter points (trim_inv = 0)
   auto sign_dac_builder = roc.testParameters();
@@ -79,4 +74,84 @@ void trim_inv_dacb_scan(Target* tgt) {
     auto trim_inv_test = trim_inv_test_builder.apply();
     daq_run(tgt, "PEDESTAL", writer, nevents, pftool::state.daq_rate);
   }
+
+}
+
+void trim_inv_dacb_scan(Target* tgt) {
+  int nevents = pftool::readline_int("Number of events per point: ", 1);
+  std::string output_filepath =
+      pftool::readline_path("trim_inv_dacb_scan", ".csv");
+  auto roc = tgt->roc(pftool::state.iroc);
+
+  if (pftool::state.daq_format_mode == Target::DaqFormat::SIMPLEROC) {
+    noinv_vref_scan_writer<pflib::packing::SingleROCEventPacket>(
+        tgt, roc, nevents, output_filepath);
+  } else if (pftool::state.daq_format_mode ==
+             Target::DaqFormat::ECOND_SW_HEADERS) {
+    noinv_vref_scan_writer<pflib::packing::MultiSampleECONDEventPacket>(
+        tgt, roc, nevents, output_filepath);
+  }
+
+  // DecodeAndWriteToCSV writer{
+  //     output_filepath,
+  //     [&](std::ofstream& f) {
+  //       nlohmann::json header;
+  //       header["scan_type"] = "CH_#.TRIM_INV & CH_#.DACB sweep";
+  //       header["trigger"] = "PEDESTAL";
+  //       header["nevents_per_point"] = nevents;
+  //       f << "# " << header << "\n"
+  //         << "TRIM_INV,DACB";
+  //       for (int ch{0}; ch < 72; ch++) {
+  //         f << ',' << ch;
+  //       }
+  //       f << '\n';
+  //     },
+  //     [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket& ep) {
+  //       f << trim_inv << ',' << dacb;
+  //       for (int ch{0}; ch < 72; ch++) {
+  //         f << ',' << ep.channel(ch).adc();
+  //       }
+  //       f << '\n';
+  //     }};
+
+  // tgt->setup_run(1 /* dummy */, Target::DaqFormat::SIMPLEROC, 1 /* dummy */);
+
+  // // take pedestal run for dacb parameter points (trim_inv = 0)
+  // auto sign_dac_builder = roc.testParameters();
+  // for (int ch{0}; ch < 72; ch++) {
+  //   sign_dac_builder.add("CH_" + std::to_string(ch), "SIGN_DAC", 1);
+  // }
+  // auto sign_dac_test = sign_dac_builder.apply();
+
+  // for (dacb = 0; dacb < 64; dacb += 4) {
+  //   auto dacb_test_builder = roc.testParameters();
+  //   for (int ch{0}; ch < 72; ch++) {
+  //     dacb_test_builder.add("CH_" + std::to_string(ch), "DACB", dacb);
+  //   }
+  //   auto dacb_test = dacb_test_builder.apply();
+
+  //   pflib_log(info) << "Running DACB = " << dacb;
+  //   daq_run(tgt, "PEDESTAL", writer, nevents, pftool::state.daq_rate);
+  // }
+
+  // // reset dacb to 0
+  // auto dacb_reset_builder = roc.testParameters();
+  // dacb = 0;
+  // for (int ch{0}; ch < 72; ch++) {
+  //   dacb_reset_builder.add("CH_" + std::to_string(ch), "DACB", dacb)
+  //       .add("CH_" + std::to_string(ch), "SIGN_DAC", 0);
+  // }
+  // auto dacb_reset = dacb_reset_builder.apply();
+
+  // // take pedestal run for trim_inv parameter points
+  // for (trim_inv = 4; trim_inv < 64; trim_inv += 4) {
+  //   pflib_log(info) << "Running TRIM_INV = " << trim_inv;
+  //   auto trim_inv_test_builder = roc.testParameters();
+  //   for (int ch{0}; ch < 72; ch++) {
+  //     trim_inv_test_builder.add("CH_" + std::to_string(ch), "TRIM_INV",
+  //                               trim_inv);
+  //   }
+  //   auto trim_inv_test = trim_inv_test_builder.apply();
+  //   daq_run(tgt, "PEDESTAL", writer, nevents, pftool::state.daq_rate);
+  // }
 }
