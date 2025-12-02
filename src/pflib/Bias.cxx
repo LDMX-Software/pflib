40999e34bbe74d0b99f364ee9f5c2a2d6c2192b9
Erik Wallin <skelpdar@gmail.com>
2025-12-02 06:22:03 -0800

    Board I2C initialization and temperature readout (#291)
    * Board I2C initialization and temperature readout
    
    * Apply clang-format --style=Google
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/pflib/Bias.cxx
+++ src/pflib/Bias.cxx
@@ -40,13 +40,6 @@ void MAX5825::set(uint8_t channel, uint16_t code) {
                                0);
 }
 
-/*
-void MAX5825::setRefVoltage(int level) {
-  uint8_t cmd = REFn | 0x4 | (level & 0x3);
-  set(cmd, 0);
-}
-*/
-
 /// DAC chip addresses taken from HGCROC specs
 const uint8_t Bias::ADDR_LED_0 = 0x18;
 const uint8_t Bias::ADDR_LED_1 = 0x1A;
@@ -54,22 +47,59 @@ const uint8_t Bias::ADDR_SIPM_0 = 0x10;
 const uint8_t Bias::ADDR_SIPM_1 = 0x12;
 
 Bias::Bias(std::shared_ptr<I2C> i2c) {
+  i2c_ = i2c;
+
   led_.emplace_back(i2c, Bias::ADDR_LED_0);
   led_.emplace_back(i2c, Bias::ADDR_LED_1);
   sipm_.emplace_back(i2c, Bias::ADDR_SIPM_0);
   sipm_.emplace_back(i2c, Bias::ADDR_SIPM_1);
 }
 
-/*
 void Bias::initialize() {
-  led_[0].setRefVoltage(3);
-  led_[1].setRefVoltage(3);
-  sipm_[0].setRefVoltage(3);
-  sipm_[1].setRefVoltage(3);
+  // Reset all DAC:s
+  i2c_->general_write_read(0x10, {0x35, 0x96, 0x30}, 0);
+  i2c_->general_write_read(0x12, {0x35, 0x96, 0x30}, 0);
+  i2c_->general_write_read(0x14, {0x35, 0x96, 0x30}, 0);
+  i2c_->general_write_read(0x18, {0x35, 0x96, 0x30}, 0);
 
-  led_[0].set(MAX5825::POWERn, 0xFF00);
+  // Set internal ref on DAC to 4.096 V
+  i2c_->general_write_read(0x10, {0x27, 0x00, 0x00}, 0);
+  i2c_->general_write_read(0x12, {0x27, 0x00, 0x00}, 0);
+  i2c_->general_write_read(0x14, {0x27, 0x00, 0x00}, 0);
+  i2c_->general_write_read(0x18, {0x27, 0x00, 0x00}, 0);
+
+  // Set up the GPIO device MCP23008
+  i2c_->general_write_read(0x20, {0x00, 0x70}, 0);
+
+  // Turn on the status LED
+  i2c_->general_write_read(0x20, {0x09, 0x80}, 0);
+
+  // Set up the board temperature sensor TMP101
+  i2c_->general_write_read(0x4A, {0x01, 0x60}, 0);
+
+  // Set up the two onewire-to-I2C devices DS2482
+  // Reset
+  i2c_->general_write_read(0x1C, {0xF0}, 0);
+  // Standard speed, weak pull-up, no active pull-up
+  i2c_->general_write_read(0x1C, {0xC3, 0xF0}, 0);
+  i2c_->general_write_read(0x1D, {0xF0}, 0);
+  i2c_->general_write_read(0x1D, {0xC3, 0xF0}, 0);
 }
-*/
+
+double Bias::readTemp() {
+  i2c_->general_write_read(0x4A, {0x00}, 0);
+  usleep(250);  // Response is a bit slow
+  std::vector<uint8_t> ret = i2c_->general_write_read(0x4A, {}, 2);
+
+  int dec = 625 * ((ret.at(0) * 256 + ret.at(1)) >> 4);
+  int integer = dec / 10000;
+  int decimal = dec - integer * 10000;
+  char cs[10];
+  snprintf(cs, 10, "%d.%d", integer, decimal);
+  std::string str = cs;
+  return std::stod(str);
+  ;
+}
 
 int Bias::readSiPM(uint8_t channel) {
   int i_chip = (channel > 7);
