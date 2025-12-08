59e2301fe7fabfffba37b8568f65009df439cd83
tomeichlersmith <eichl008@umn.edu>
2025-12-08 12:39:30 -0600

    fixup rogue-read and it appears to be working
    event-wide CRC is matching, seeing number of events I triggered

--- ana/rogue-read.py
+++ ana/rogue-read.py
@@ -4,20 +4,34 @@ import numpy as np
 import pypflib
 print(pypflib.version.git_describe())
 pypflib.logging.open(True)
-#pypflib.logging.set(pypflib.logging.level.info)
+#pypflib.logging.set(pypflib.logging.level.trace)
 
-ep = pypflib.packing.MultiSampleECONDEventPacket(1)
+# number of links/channels enabled in the ECON-D
+#   for HcalBackplane -> 2 channels for 1 ROC
+#   for the EcalSMM -> complicated, depends on which layer
+#                      because some of the ROCs are not accessible
+ep = pypflib.packing.MultiSampleECONDEventPacket(2)
 
-with pyrogue.utilities.fileio.FileReader(files=sys.argv[1], configChan = 255) as fd:
+# do NOT provide configChan = 255 because it does not provide a Loader to yaml.load
+# which fails in newer versions of Python :tada:
+with pyrogue.utilities.fileio.FileReader(files=sys.argv[1]) as fd:
     for header, data in fd.records():
-        if data[-1] == 0x0a:
+        if data[-1] == 0x0a or header.channel != 0:
             # fucking magic byte signaling config packet
             continue
 
+        # need to convert the data into a std::vector<uint32_t>
+        # data here is a np.ndarray('int8') so we re-view
+        # it in our words and skip the first four words which
+        # contain the Rogue-inserted EventHeader
         v = pypflib.packing.WordVector()
         words = data.view('uint32')
         v.extend(words[4:].tolist())
+
+        # this is the call that attempts to decode the word vector
         ep.from_word_vector(v)
+
+        # access the decoded data
         print(ep.n_samples)
         print(ep.i_soi)
         print(ep.soi().channel(0,10).adc)
