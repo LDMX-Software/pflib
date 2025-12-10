5f6812df0bd9728a717a95cb314f32cd518ab77f
joshgreaves332 <vve7nj@virginia.edu>
2025-12-09 19:34:22 -0500

    moving include target.h up to ecal.h instead of ecalSMM.cxx

--- src/pflib/bittware/EcalSMMTarget.cxx
+++ src/pflib/bittware/EcalSMMTarget.cxx
@@ -1,5 +1,5 @@
 #include "pflib/Ecal.h"
-#include "pflib/Target.h"
+// #include "pflib/Target.h" // commenting this out because its now newly included in ecal.h
 #include "pflib/bittware/bittware_FastControl.h"
 #include "pflib/bittware/bittware_daq.h"
 #include "pflib/bittware/bittware_elinks.h"
