9bc817066d78001e3bf686cb9abfa6f3e37f73c0
Andreas Pettersson <appettersson@hotmail.com>
2025-12-11 15:54:29 +0100

    added functionality for external charge injections with LED pulses

--- app/tool/tasks/multi_channel_scan.cxx
+++ app/tool/tasks/multi_channel_scan.cxx
@@ -12,6 +12,7 @@ ENABLE_LOGGING();
 void multi_channel_scan(Target* tgt) {
   int nevents = pftool::readline_int("How many events per time point? ", 1);
   int calib = pftool::readline_int("Setting for calib pulse amplitude? ", 256);
+  bool isLED = pftool::readline_bool("Use external LED flashes?", false);
   bool highrange = pftool::readline_bool("Use highrange (Y) or preCC (N)? ", false);
   int start_bx = pftool::readline_int("Starting BX? ", -1);
   int n_bx = pftool::readline_int("Number of BX? ", 3);
@@ -23,99 +24,56 @@ void multi_channel_scan(Target* tgt) {
   link == 0 ? ch0 = 18 : ch0 = 54;
 
   auto roc{tgt->roc(pftool::state.iroc)};
-  auto test_param_handle = roc.testParameters();
-  auto refvol_page = pflib::utility::string_format("REFERENCEVOLTAGE_%d", link);
-  test_param_handle
-        .add(refvol_page, "CALIB", highrange ? calib : 0)
-        .add(refvol_page, "CALIB_2V5", highrange ? 0 : calib)
-        .add(refvol_page, "INTCTEST", 1)
-        .add(refvol_page, "CHOICE_CINJ", highrange ? 1 : 0);
-  auto test_param = test_param_handle.apply();
-
-  std::filesystem::path parameter_points_file =
-      pftool::readline("File of parameter points: ");
-
-  pflib_log(info) << "loading parameter points file...";
-  auto [param_names, param_values] =
-      load_parameter_points(parameter_points_file);
-  pflib_log(info) << "successfully loaded parameter points";
-
-  int phase_strobe{0};
-  int charge_to_l1a{0};
-  double time{0};
-  double clock_cycle{25.0};
-  int n_phase_strobe{16};
-  int offset{1};
-  std::size_t i_param_point{0};
-  int nr_channels{-1};
-  DecodeAndWriteToCSV writer{
-      fname,
-      [&](std::ofstream& f) {
-        nlohmann::json header;
-        header["preCC"] = "True";
-        f << std::boolalpha << "# " << header << '\n' << "nr channels," << "time,";
-        for (const auto& [page, parameter] : param_names) {
-          f << page << '.' << parameter << ',';
-        }
-        f << "channel,";
-        f << pflib::packing::Sample::to_csv_header << '\n';
-      },
-      [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket& ep) {
-        for (int ch{0}; ch < 72; ch++) {
-          f << nr_channels+1 << ',';
-          f << time << ',';
-
-          for (const auto& val : param_values[i_param_point]) {
-            f << val << ',';
-          }
-          f << ch << ',';
-
-          ep.channel(ch).to_csv(f);
-          f << '\n';
-        }
-      }};
-  tgt->setup_run(1 /* dummy - not stored */, Target::DaqFormat::SIMPLEROC,
-                 1 /* dummy */);
-  // Do the scan for increasing amount of channels
-  for (nr_channels; nr_channels < 18; nr_channels++) {
-    pflib_log(info) << "running scan " << nr_channels + 1 << " out of 19";
+  if (isLED) {
+    auto refvol_page = pflib::utility::string_format("REFERENCEVOLTAGE_%d", link);
     auto test_param_handle = roc.testParameters();
-    if (nr_channels == -1) { // In case of -1, just take pedestal data
-      daq_run(tgt, "PEDESTAL", writer, nevents, pftool::state.daq_rate);
-      continue;
-    }
-    for (int ch = ch0-nr_channels; ch <= ch0+nr_channels; ch++) {
-      auto channel_page = pflib::utility::string_format("CH_%d", ch);
-      test_param_handle
-          .add(channel_page, "HIGHRANGE", 1);
-    }
-    auto test_param_two = test_param_handle.apply();
-    i_param_point = 0;
-    for (; i_param_point < param_values.size(); i_param_point++) {
-      // set parameters
-      auto test_param_builder = roc.testParameters();
-      for (std::size_t i_param{0}; i_param < param_names.size(); i_param++) {
-        const auto& full_page = param_names[i_param].first;
-        std::string str_page = full_page.substr(0, full_page.find("_"));
-        const auto& param = param_names[i_param].second;
-        int value = param_values[i_param_point][i_param];
+    test_param_handle
+          .add(refvol_page, "INTCTEST", 1);
+    auto test_param = test_param_handle.apply();
 
-        if (str_page == "CH" || str_page == "TOP") {
-          test_param_builder.add(full_page, param, value);
-        } else {
-          test_param_builder.add(str_page + "_" + std::to_string(link), param,
-                                     value);
-        }
-        pflib_log(info) << param << " = " << value;
+    int phase_strobe{0};
+    int charge_to_l1a{0};
+    double time{0};
+    double clock_cycle{25.0};
+    int n_phase_strobe{16};
+    int offset{1};
+    int nr_channels{-1};
+    DecodeAndWriteToCSV writer{
+        fname,
+        [&](std::ofstream& f) {
+          nlohmann::json header;
+          header["highrange"] = highrange;
+          header["preCC"] = !highrange;
+          header["ledflash"] = isLED;
+          f << std::boolalpha << "# " << header << '\n' << "nr channels," << "time,";
+          f << "channel,";
+          f << pflib::packing::Sample::to_csv_header << '\n';
+        },
+        [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket& ep) {
+          for (int ch{0}; ch < 72; ch++) {
+            f << nr_channels+1 << ',';
+            f << time << ',';
+            f << ch << ',';
+
+            ep.channel(ch).to_csv(f);
+            f << '\n';
+          }
+        }};
+    tgt->setup_run(1 /* dummy - not stored */, Target::DaqFormat::SIMPLEROC,
+                   1 /* dummy */);
+    // Do the scan for increasing amount of channels
+    for (nr_channels; nr_channels < 1; nr_channels++) {
+      auto test_param_handle = roc.testParameters();
+      if (nr_channels == -1) { // In case of -1, just take pedestal data
+        daq_run(tgt, "PEDESTAL", writer, 1, pftool::state.daq_rate);
+        continue;
       }
-      auto test_param_three = test_param_builder.apply();
-      // timescan
-      auto central_charge_to_l1a = tgt->fc().fc_get_setup_calib();
+      int central_charge_to_l1a = tgt->fc().fc_get_setup_led();
       for (charge_to_l1a = central_charge_to_l1a + start_bx;
            charge_to_l1a < central_charge_to_l1a + start_bx + n_bx;
            charge_to_l1a++) {
-        tgt->fc().fc_setup_calib(charge_to_l1a);
-        pflib_log(info) << "charge_to_l1a = " << tgt->fc().fc_get_setup_calib();
+        tgt->fc().fc_setup_led(charge_to_l1a);
+        pflib_log(info) << "led_to_l1a = " << tgt->fc().fc_get_setup_led();
         for (phase_strobe = 0; phase_strobe < n_phase_strobe; phase_strobe++) {
           auto phase_strobe_test_handle =
               roc.testParameters()
@@ -126,11 +84,125 @@ void multi_channel_scan(Target* tgt) {
           time =
               (charge_to_l1a - central_charge_to_l1a + offset) * clock_cycle -
               phase_strobe * clock_cycle / n_phase_strobe;
-          daq_run(tgt, "CHARGE", writer, nevents, pftool::state.daq_rate);
+          daq_run(tgt, "LED", writer, 1, pftool::state.daq_rate);
         }
       }
       // reset charge_to_l1a to central value
-      tgt->fc().fc_setup_calib(central_charge_to_l1a);
+      tgt->fc().fc_setup_led(central_charge_to_l1a);
+    }
+  } else {
+    auto refvol_page = pflib::utility::string_format("REFERENCEVOLTAGE_%d", link);
+    auto test_param_handle = roc.testParameters();
+    test_param_handle
+          .add(refvol_page, "CALIB", highrange ? calib : 0)
+          .add(refvol_page, "CALIB_2V5", highrange ? 0 : calib)
+          .add(refvol_page, "INTCTEST", 1)
+          .add(refvol_page, "CHOICE_CINJ", highrange ? 1 : 0);
+    auto test_param = test_param_handle.apply();
+
+    std::filesystem::path parameter_points_file =
+        pftool::readline("File of parameter points: ");
+
+    pflib_log(info) << "loading parameter points file...";
+    auto [param_names, param_values] =
+        load_parameter_points(parameter_points_file);
+    pflib_log(info) << "successfully loaded parameter points";
+
+    int phase_strobe{0};
+    int charge_to_l1a{0};
+    double time{0};
+    double clock_cycle{25.0};
+    int n_phase_strobe{16};
+    int offset{1};
+    std::size_t i_param_point{0};
+    int nr_channels{-1};
+    DecodeAndWriteToCSV writer{
+        fname,
+        [&](std::ofstream& f) {
+          nlohmann::json header;
+          header["highrange"] = highrange;
+          header["precc"] = !highrange;
+          header["ledflash"] = isLED;
+          f << std::boolalpha << "# " << header << '\n' << "nr channels," << "time,";
+          for (const auto& [page, parameter] : param_names) {
+            f << page << '.' << parameter << ',';
+          }
+          f << "channel,";
+          f << pflib::packing::Sample::to_csv_header << '\n';
+        },
+        [&](std::ofstream& f, const pflib::packing::SingleROCEventPacket& ep) {
+          for (int ch{0}; ch < 72; ch++) {
+            f << nr_channels+1 << ',';
+            f << time << ',';
+
+            for (const auto& val : param_values[i_param_point]) {
+              f << val << ',';
+            }
+            f << ch << ',';
+
+            ep.channel(ch).to_csv(f);
+            f << '\n';
+          }
+        }};
+    tgt->setup_run(1 /* dummy - not stored */, Target::DaqFormat::SIMPLEROC,
+                   1 /* dummy */);
+    // Do the scan for increasing amount of channels
+    for (nr_channels; nr_channels < 18; nr_channels++) {
+      pflib_log(info) << "running scan " << nr_channels + 1 << " out of 19";
+      auto test_param_handle = roc.testParameters();
+      if (nr_channels == -1) { // In case of -1, just take pedestal data
+        daq_run(tgt, "PEDESTAL", writer, nevents, pftool::state.daq_rate);
+        continue;
+      }
+      for (int ch = ch0-nr_channels; ch <= ch0+nr_channels; ch++) {
+        auto channel_page = pflib::utility::string_format("CH_%d", ch);
+        test_param_handle
+            .add(channel_page, "HIGHRANGE", 1);
+      }
+      auto test_param_two = test_param_handle.apply();
+      i_param_point = 0;
+      for (; i_param_point < param_values.size(); i_param_point++) {
+        // set parameters
+        auto test_param_builder = roc.testParameters();
+        for (std::size_t i_param{0}; i_param < param_names.size(); i_param++) {
+          const auto& full_page = param_names[i_param].first;
+          std::string str_page = full_page.substr(0, full_page.find("_"));
+          const auto& param = param_names[i_param].second;
+          int value = param_values[i_param_point][i_param];
+
+          if (str_page == "CH" || str_page == "TOP") {
+            test_param_builder.add(full_page, param, value);
+          } else {
+            test_param_builder.add(str_page + "_" + std::to_string(link), param,
+                                       value);
+          }
+          pflib_log(info) << param << " = " << value;
+        }
+        auto test_param_three = test_param_builder.apply();
+        // timescan
+        int central_charge_to_l1a;
+        central_charge_to_l1a = tgt->fc().fc_get_setup_calib();
+        for (charge_to_l1a = central_charge_to_l1a + start_bx;
+             charge_to_l1a < central_charge_to_l1a + start_bx + n_bx;
+             charge_to_l1a++) {
+          tgt->fc().fc_setup_calib(charge_to_l1a);
+          pflib_log(info) << "charge_to_l1a = " << tgt->fc().fc_get_setup_calib();
+          for (phase_strobe = 0; phase_strobe < n_phase_strobe; phase_strobe++) {
+            auto phase_strobe_test_handle =
+                roc.testParameters()
+                    .add("TOP", "PHASE_STROBE", phase_strobe)
+                    .apply();
+            pflib_log(info) << "TOP.PHASE_STROBE = " << phase_strobe;
+            usleep(10);  // make sure parameters are applied
+            time =
+                (charge_to_l1a - central_charge_to_l1a + offset) * clock_cycle -
+                phase_strobe * clock_cycle / n_phase_strobe;
+            daq_run(tgt, "CHARGE", writer, nevents, pftool::state.daq_rate);
+          }
+        }
+        // reset charge_to_l1a to central value
+        tgt->fc().fc_setup_calib(central_charge_to_l1a);
+      }
     }
   }
 }
