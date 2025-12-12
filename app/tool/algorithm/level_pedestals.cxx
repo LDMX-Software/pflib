fab15ba92d491bee3ffca0cc0c0e9d580fa64062
joshgreaves332 <vve7nj@virginia.edu>
2025-12-11 21:53:39 -0500

    introducing mapping file for channels

--- app/tool/algorithm/level_pedestals.cxx
+++ app/tool/algorithm/level_pedestals.cxx
@@ -126,6 +126,10 @@ static void pedestal_runs(Target* tgt, ROC& roc, std::array<int, 72>& baseline,
 std::map<std::string, std::map<std::string, uint64_t>> level_pedestals(
     Target* tgt, ROC roc) {
   static auto the_log_{::pflib::logging::get("level_pedestals")};
+  
+  std::string mask_file = pftool::readline_int("Path to maskfile: ", "");
+  std::vector<int> masked_channels;
+  std::ifstream ch_masks("");
 
   /// do three runs of 100 samples each to have well defined pedestals
   static const std::size_t n_events = 100;
