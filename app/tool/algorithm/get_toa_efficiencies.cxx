650ef99cc6b9432b6fc8981ac760844472130d05
joshgreaves332 <vve7nj@virginia.edu>
2025-12-13 21:19:03 -0500

    completing masking code addition to trim_toa_scan and get_toa_Efficiencies. also needed in toa_vref_scan

--- app/tool/algorithm/get_toa_efficiencies.cxx
+++ app/tool/algorithm/get_toa_efficiencies.cxx
@@ -5,27 +5,35 @@
 namespace pflib::algorithm {
 
 // helper function
-//  bool is_masked(int ch, ){
-//    return count(masked_channels.begin(), masked_channels.end(), ch) > 0)
-//  }
+ bool is_masked(int ch, const std::vector<int>& masked_channels){
+  return (count(masked_channels.begin(), masked_channels.end(), ch) > 0) ;
+ }
 
 template <class EventPacket>
 std::array<double, 72> get_toa_efficiencies(
-    const std::vector<EventPacket>& data, std::vector<int>& masked_channels) {
+    const std::vector<EventPacket>& data, const std::vector<int>& masked_channels) {
   std::array<double, 72> efficiencies;
   /// reserve a vector of the appropriate size to avoid repeating allocation
   /// time for all 72 channels
+  // fill with NANs so that channels can be masked out
+  efficiencies.fill(std::numeric_limits<double>::quiet_NaN());
   std::vector<int> toas(data.size());
   int i_ch = 0;  // 0–35
   int i_link = 0;
 
   for (int ch{0}; ch < 72; ch++) {
+
+    if (is_masked(ch, masked_channels)) {
+      continue;   // leave NaN
+    } 
+
     i_link = (ch / 36);
     i_ch = ch % 36;
     for (std::size_t i{0}; i < toas.size(); i++) {
       if constexpr (std::is_same_v<
                         EventPacket,
                         pflib::packing::MultiSampleECONDEventPacket>) {
+
         toas[i] = data[i].samples[data[i].i_soi].channel(i_link, i_ch).toa();
       } else if constexpr (std::is_same_v<
                                EventPacket,
@@ -52,10 +60,10 @@ std::array<double, 72> get_toa_efficiencies(
 template std::array<double, 72>
 pflib::algorithm::get_toa_efficiencies<pflib::packing::SingleROCEventPacket>(
     const std::vector<pflib::packing::SingleROCEventPacket>& data,
-    std::vector<int>& masked_channels);
+    const std::vector<int>& masked_channels);
 template std::array<double, 72> pflib::algorithm::get_toa_efficiencies<
     pflib::packing::MultiSampleECONDEventPacket>(
     const std::vector<pflib::packing::MultiSampleECONDEventPacket>& data,
-    std::vector<int>& masked_channels);
+    const std::vector<int>& masked_channels);
 
 }  // namespace pflib::algorithm
