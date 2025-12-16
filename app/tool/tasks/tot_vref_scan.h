0e622caac65d9603b30fe12c9d579632d1cedece
Andreas Pettersson <andreas.pettersson.6826@student.lu.se>
2025-12-16 11:24:31 +0100

    moved files from branch tot_vref_scan to fit new code structure

---
+++ app/tool/tasks/tot_vref_scan.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../pftool.h"
+
+/**
+ * TASKS.TOT_VREF_SCAN
+ *
+ * Scan TOT_VREF for both halves of the chip
+ * Scan TRIM_TOT for all channels
+ *
+ *
+ */
+void tot_vref_scan(Target* tgt);
