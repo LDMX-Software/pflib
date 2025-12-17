585fac0510ce8ba19b6a3f1927933ea0a5d32eb8
tomeichlersmith <eichl008@umn.edu>
2025-12-17 22:36:32 +0000

    playing around with econ config, update zcu FC

--- src/pflib/zcu/EcalSMM.cxx
+++ src/pflib/zcu/EcalSMM.cxx
@@ -79,6 +79,9 @@ class EcalSMMTargetZCU : public Target {
   virtual void setup_run(int irun, Target::DaqFormat format, int contrib_id) {
     format_ = format;
     contrib_id_ = contrib_id;
+
+    daq().reset();
+    fc().clear_run();
   }
 
   virtual std::vector<uint32_t> read_event() override {
