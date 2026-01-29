6946833a402daf622bcb2983b6618cdeec9da057
Andreas Pettersson <appettersson@hotmail.com>
2026-01-29 14:14:01 +0100

    Update src/pflib/utility/mean.cxx
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>

--- src/pflib/utility/mean.cxx
+++ src/pflib/utility/mean.cxx
@@ -4,20 +4,20 @@
 
 namespace pflib::utility {
 
-double mean(std::vector<int> samples) {
-  // The mean is the sum over the size of the samples
+template<typename T>
+double mean(const std::vector<T>& samples) {
   if (samples.size() == 0) {
-    throw "Trying to get the mean of an empty vector";
+    return std::numeric_limits<double>::nan();
   }
   return std::accumulate(samples.begin(), samples.end(), 0.0) / samples.size();
 }
 
-double mean(std::vector<double> samples) {
-  // The mean is the sum over the size of the samples
-  if (samples.size() == 0) {
-    throw "Trying to get the mean of an empty vector";
-  }
-  return std::accumulate(samples.begin(), samples.end(), 0.0) / samples.size();
+double mean(const std::vector<double>& samples) {
+  return mean(samples);
+}
+
+double mean(const std::vector<int>& samples) {
+  return mean(samples);
 }
 
 }  // namespace pflib::utility
