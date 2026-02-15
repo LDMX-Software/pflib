f1394e010b265b49ea5c6174988a92a466bf8577
Andreas Pettersson <andreas.pettersson.6826@student.lu.se>
2026-02-15 20:23:46 +0100

    added analysis

--- ana/tot_vref/s-curves.py
+++ ana/tot_vref/s-curves.py
@@ -24,7 +24,17 @@ plt.rcParams['xtick.labelsize'] = 16; plt.rcParams['ytick.labelsize'] = 16
 fig_toa, ax_toa = plt.subplots(1, 1)
 fig_tot, ax_tot = plt.subplots(1, 1)
 
+# For some reason, TOA and TOT is always 0 on link 1
+# My theory is that TOA_vref isn't set correctly
 samples = samples[samples['channel'] < 36]
+
+ana = samples[samples['channel'] == 13]
+
+gp = ana.groupby(['calib'])
+
+for i, df in gp:
+    plt.scatter(df['time'], df['tot'])
+    plt.show()
 
 # Efficiency calculation
 def efficiency(vals):
@@ -34,21 +44,26 @@ def efficiency(vals):
             counts += 1
     return counts/len(vals)
 
+def condition_max(vals):
+    if (min(vals) == -1):
+        return 1023
+    else:
+        return max(vals)
+
 # Make the median out of samples per timepoint, and get the efficiencies
 samples = (samples
-            .groupby(['CALIB', 'channel'], as_index=False) # implement channels
+            .groupby(['calib', 'channel'], as_index=False) # implement channels
             .agg(toa_eff=('toa', efficiency), 
                  tot_eff=('tot', efficiency),
-                 max_adc=('adc', 'max')))
+                 max_adc=('adc', condition_max)))
 
 #ax_toa.plot(samples['CALIB'], samples['toa_eff'])
 #ax_tot.plot(samples['CALIB'], samples['tot_eff'])
 # Plot data
 ch_group = samples.groupby('channel')
 for ch_id, ch_df in ch_group:
-    print(ch_df)
-    ax_toa.scatter(ch_df['max_adc'], ch_df['toa_eff'], label=f'ch {ch_id}')
-    ax_tot.scatter(ch_df['max_adc'], ch_df['tot_eff'], label=f'ch {ch_id}')
+    ax_toa.scatter(ch_df['calib'], ch_df['toa_eff'], label=f'ch {ch_id}')
+    ax_tot.scatter(ch_df['calib'], ch_df['tot_eff'], label=f'ch {ch_id}')
 
 ax_toa.legend(fontsize=12, ncols=6)
 ax_tot.legend(fontsize=12, ncols=6)
