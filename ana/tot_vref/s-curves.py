fc14f79e6c39d8747d6b29cef2656b679a350e9d
Andreas Pettersson <andreas.pettersson.6826@student.lu.se>
2026-03-18 17:38:52 +0100

    s-curve analyzer

--- ana/tot_vref/s-curves.py
+++ ana/tot_vref/s-curves.py
@@ -14,53 +14,42 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('data', type=Path, help='decoded CSV file to analyze')
+parser.add_argument("-pa", "--plot-adc", 
+                    help='Plot the adc, with toa and tot, all channels for each link')
+parser.add_argument("-st", "--style", type=Path,
+                    help='Adress of matplotlib style sheet')
 args = parser.parse_args()
 
 samples = pd.read_csv(args.data)
 
-plt.rcParams['figure.figsize'] = [15, 12]
-plt.rcParams['xtick.labelsize'] = 16; plt.rcParams['ytick.labelsize'] = 16
+if (args.style != None):
+    plt.style.use(args.style)
 
-# For some reason, TOA and TOT is always 0 on link 1
-# My theory is that TOA_vref isn't set correctly
-#samples = samples[samples['channel'] >= 36]
+if (args.plot_adc):
+    fig, ax = plt.subplots(1,1)
+    ana = samples[samples['channel'] < 36]
+    ax.scatter(ana['time'], ana['adc'], label='adc')
+    ax.scatter(ana['time'], ana['toa'], label='toa')
+    ax.scatter(ana['time'], ana['tot'], label='tot')
+    ax.legend()
+    ax.set_xlabel('Time [ns]', fontsize=24)
+    ax.set_ylabel('Output data', fontsize=24)
+    fig.savefig('adc_link0.png', dpi=400, bbox_inches='tight')
+    plt.close()
 
-#ana = samples[samples['channel'] == 37]
-ana = samples[samples['channel'] < 36]
-plt.scatter(ana['time'], ana['adc'], label='adc')
-plt.scatter(ana['time'], ana['toa'], label='toa')
-plt.scatter(ana['time'], ana['tot'], label='tot')
-plt.scatter(ana['time'], 100*ana['Tp'], label=r'100$\times$Tp')
-plt.scatter(ana['time'], 100*ana['Tc'], label=r'100$\times$Tc')
-plt.legend()
-plt.xlabel('Time [ns]', fontsize=24)
-plt.ylabel('Output data', fontsize=24)
-plt.savefig('data_link0.png', dpi=400, bbox_inches='tight')
-plt.show()
-plt.clf()
+    fig, ax = plt.subplots(1,1)
+    ana = samples[samples['channel'] >= 36]
+    ax.scatter(ana['time'], ana['adc'], label='adc')
+    ax.scatter(ana['time'], ana['toa'], label='toa')
+    ax.scatter(ana['time'], ana['tot'], label='tot')
+    ax.legend()
+    ax.set_xlabel('Time [ns]', fontsize=24)
+    ax.set_ylabel('Output data', fontsize=24)
+    fig.savefig('adc_link1.png', dpi=400, bbox_inches='tight')
+    plt.close()
 
-ana = samples[samples['channel'] >= 36]
-plt.scatter(ana['time'], ana['adc'], label='adc')
-plt.scatter(ana['time'], ana['toa'], label='toa')
-plt.scatter(ana['time'], ana['tot'], label='tot')
-plt.scatter(ana['time'], 100*ana['Tp'], label=r'100$\times$Tp')
-plt.scatter(ana['time'], 100*ana['Tc'], label=r'100$\times$Tc')
-plt.legend()
-plt.xlabel('Time [ns]', fontsize=24)
-plt.ylabel('Output data', fontsize=24)
-plt.savefig('data_link1.png', dpi=400, bbox_inches='tight')
-plt.show()
-
-gp = ana.groupby(['calib'])
-
-#for i, df in gp:
-#    plt.scatter(df['time'], df['adc'], label='adc')
-#    plt.scatter(df['time'], df['toa'], label='toa')
-#    plt.legend()
-#    plt.show()
-
-fig_toa, ax_toa = plt.subplots(1, 1)
-fig_tot, ax_tot = plt.subplots(1, 1)
+fig_toa, ax_toa = plt.subplots(1, 2, sharey=True)
+fig_tot, ax_tot = plt.subplots(1, 2, sharey=True)
 fig_adc, ax_adc = plt.subplots(1, 1)
 
 # Efficiency calculation
@@ -78,28 +67,38 @@ def condition_max(vals):
         return 1023
     else:
         return max(vals)
-
 # Make the median out of samples per timepoint, and get the efficiencies
+
+# First chose a timepoint where we know that the toa and tot will trigger
+
+time_trig = np.median(samples[samples['calib'] == samples['calib'].max()]
+            .groupby(['channel', 'time'])
+            .filter(lambda g: (g['tot'] > -1).all())
+            ['time'].unique()
+            )
+samples = samples[samples['time'] == time_trig]
 samples = (samples
             .groupby(['calib', 'channel'], as_index=False) # implement channels
             .agg(toa_eff=('toa', efficiency), 
                  tot_eff=('tot', efficiency),
                  max_adc=('adc', condition_max)))
 
-#ax_toa.plot(samples['CALIB'], samples['toa_eff'])
-#ax_tot.plot(samples['CALIB'], samples['tot_eff'])
 # Plot data
 ch_group = samples.groupby('channel')
 max_non_saturated = 0
 for ch_id, ch_df in ch_group:
-    if ch_id >= 0:
-        ax_toa.plot(ch_df['max_adc'], ch_df['toa_eff'], label=f'ch {ch_id}')
-        ax_tot.plot(ch_df['max_adc'], ch_df['tot_eff'], label=f'ch {ch_id}')
-        ax_adc.scatter(ch_df['calib'], ch_df['max_adc'], label=f'ch {ch_id}')
-    #else:
-    #    ax_toa.plot(ch_df['calib'], ch_df['toa_eff'])
-    #    ax_tot.plot(ch_df['calib'], ch_df['tot_eff'])
-    #    ax_adc.scatter(ch_df['calib'], ch_df['max_adc'])
+    if ch_id == 0:
+        ax_toa[0].plot(ch_df['calib'], ch_df['toa_eff'], label=f'channels')
+        ax_tot[0].plot(ch_df['calib'], ch_df['tot_eff'], label=f'channels')
+        ax_toa[1].plot(ch_df['max_adc'], ch_df['toa_eff'], label=f'channels')
+        ax_tot[1].plot(ch_df['max_adc'], ch_df['tot_eff'], label=f'channels')
+        ax_adc.scatter(ch_df['calib'], ch_df['max_adc'], label=f'channels')
+    else:
+        ax_toa[0].plot(ch_df['calib'], ch_df['toa_eff'])
+        ax_tot[0].plot(ch_df['calib'], ch_df['tot_eff'])
+        ax_toa[1].plot(ch_df['max_adc'], ch_df['toa_eff'])
+        ax_tot[1].plot(ch_df['max_adc'], ch_df['tot_eff'])
+        ax_adc.scatter(ch_df['calib'], ch_df['max_adc'])
     temp_df = ch_df[ch_df['max_adc'] != 1023]
     max_val = temp_df['max_adc'].max()
     if (max_val > max_non_saturated):
@@ -107,9 +106,22 @@ for ch_id, ch_df in ch_group:
 
 ax_adc.axhline(y = max_non_saturated, linestyle='--', color='b', label=f'max non-saturated at {max_non_saturated}')
 
-ax_toa.legend(fontsize=12, ncols=6)
-ax_tot.legend(fontsize=12, ncols=6)
+ax_toa[0].legend(fontsize=12, ncols=6)
+ax_tot[0].legend(fontsize=12, ncols=6)
+ax_toa[1].legend(fontsize=12, ncols=6)
+ax_tot[1].legend(fontsize=12, ncols=6)
+ax_toa[0].set_xlabel('Calib [a.u.]')
+ax_tot[0].set_xlabel('Calib [a.u.]')
+ax_toa[1].set_xlabel('Max ADC [a.u.]')
+ax_tot[1].set_xlabel('Max ADC [a.u.]')
+ax_toa[0].set_ylabel('TOA efficiency')
+ax_tot[0].set_ylabel('TOT efficiency')
 ax_adc.legend(fontsize=12, ncols=6)
+ax_adc.set_xlabel('Calib [a.u.]')
+ax_adc.set_ylabel('Max ADC [a.u.]')
+fig_toa.savefig("toa_s_curve.png", dpi=400)
+fig_tot.savefig("tot_s_curve.png", dpi=400)
+fig_adc.savefig("adc_linearity.png", dpi=400)
 plt.show()
 #fig_toa.savefig('toa_efficiency.png', dpi=400, bbox_inches='tight')
 #fig_tot.savefig('tot_efficiency.png', dpi=400, bbox_inches='tight')
