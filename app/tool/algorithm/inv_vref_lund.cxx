483ad14cfad0d86f20d00b45ad5ec30dc7038dd6
Andreas Pettersson <andreas.pettersson.6826@student.lu.se>
2026-03-18 11:26:39 +0100

    moved scan to an algorithm based configuration

---
+++ app/tool/algorithm/inv_vref_lund.cxx
@@ -0,0 +1,184 @@
+#include "inv_vref_lund.h"
+
+#include "../daq_run.h"
+#include "../econ_links.h"
+#include "../tasks/inv_vref_lund.h"
+#include "pflib/utility/string_format.h"
+#include "pflib/utility/median.h"
+#include "pflib/utility/mean.h"
+#include "pflib/utility/stdev.h"
+
+namespace pflib::algorithm {
+
+DataFitter::DataFitter() {};
+
+void DataFitter::sort_and_append(std::vector<int>& inv_vrefs, 
+                                 std::vector<int>& pedestals,
+                                 std::vector<double>& stdevs,
+                                 int& step) {
+  static auto the_log_{::pflib::logging::get("inv_vref_scan:sort")};
+  pflib_log(info) << "Sorting Data";
+  // We ignore first and last elements since they miss derivs
+  struct DerivPoint {
+    int i;
+    double LH;
+    double LH_stdev;
+    double RH;
+    double RH_stdev;
+  };
+  std::vector<DerivPoint> slope_points;
+
+  double flat_threshold = 0.1;
+  std::vector<double> LH_derivs;
+  std::vector<double> LH_stdevs;
+  std::vector<double> RH_derivs;
+  for (int i = 1; i < inv_vrefs.size()-1; i++) {
+    double LH = static_cast<double>(pedestals[i] - pedestals[i-1]) / (inv_vrefs[i] - inv_vrefs[i-1]);
+    double RH = static_cast<double>(pedestals[i] - pedestals[i+1]) / (inv_vrefs[i] - inv_vrefs[i+1]);
+
+    // Threshold check. CMS uses 0.05. This value fits with my analysis as well.
+    if (std::abs(LH) < flat_threshold || std::abs(RH) < flat_threshold) { // flat regime
+      nonlinear_.push_back({inv_vrefs[i], pedestals[i], LH, RH});
+    } else { // we're in a linear regime or there's outliers
+      double LH_err = (stdevs[i] - stdevs[i+1]) / (inv_vrefs[i] - inv_vrefs[i-1]);
+      double RH_err = (stdevs[i] - stdevs[i+1]) / (inv_vrefs[i] - inv_vrefs[i+1]);
+      slope_points.push_back({i, LH, LH_err, RH, RH_err});
+      LH_derivs.push_back(LH);
+      LH_stdevs.push_back(LH_err);
+      RH_derivs.push_back(RH);
+    }
+  }
+  // Now we get the linear region. CMS removes outliers by using the ADC median.
+  // From my analysis I chose to also consider the stdev of each point, which is 0 in 
+  // the low inv_vref region (inv_vref approx less than 250).
+  // NOTE! This seems to vary between boards, and more analysis between boards should be 
+  // done to find the optimal selections.
+  //
+  // We could use both LH and RH derivs to improve selections (?).
+
+  
+  LH_std_median_ = pflib::utility::median(LH_stdevs);
+  LH_median_ = pflib::utility::median(LH_derivs);
+
+  pflib_log(info) << "Median stdev = " << LH_std_median_;
+  pflib_log(info) << "Median LH = " << LH_median_;
+  
+  for (const auto& p : slope_points) {
+    if ((std::abs(p.LH - LH_median_) < 0.7*std::abs(LH_median_)) &&
+         std::abs(p.LH_stdev) > 0.0001) { // Linear regime. 
+      pflib_log(info) << "inv_vref is : " << inv_vrefs[p.i];
+      linear_.push_back({inv_vrefs[p.i], pedestals[p.i], p.LH, p.RH});
+    }
+  }
+}
+
+// helper function to facilitate EventPacket dependent behaviour
+template <class EventPacket>
+static void inv_vref_scan_getter(Target* tgt, pflib::ROC& roc, size_t nevents,
+                                 std::array<int, 2>& channels,
+                                 std::array<int, 2>& inv_vref_tgt,
+                                 std::array<int, 2>& noinv_vref_tgt) {
+  static auto the_log_{::pflib::logging::get("inv_vref_scan:getter")};
+  int n_links = 2;
+  if constexpr (std::is_same_v<EventPacket,
+                               pflib::packing::MultiSampleECONDEventPacket>) {
+    n_links = determine_n_links(tgt);
+  }
+
+  int noinv_vref = 612;
+  int target_adc = 200;
+  
+  DecodeAndBuffer<EventPacket> buffer{1, 2};
+
+  tgt->setup_run(1 /* dummy - not stored */, pftool::state.daq_format_mode,
+                 1 /* dummy */);
+
+  std::vector<int> pedestals_l0;
+  std::vector<double> stds_l0;
+  std::vector<int> pedestals_l1;
+  std::vector<double> stds_l1;
+  std::vector<int> inv_vrefs;
+
+  int step = 20;
+
+  for (int inv_vref = 0; inv_vref < 1024; inv_vref += step) {
+    pflib_log(info) << "Running INV_VREF = " << inv_vref;
+    // set inv_vref simultaneously for both links
+    auto test_param = roc.testParameters()
+                          .add("REFERENCEVOLTAGE_0", "INV_VREF", inv_vref)
+                          .add("REFERENCEVOLTAGE_1", "INV_VREF", inv_vref)
+                          .add("REFERENCEVOLTAGE_0", "NOINV_VREF", noinv_vref)
+                          .add("REFERENCEVOLTAGE_1", "NOINV_VREF", noinv_vref)
+                          .apply();
+    // store current scan state in header for writer access
+    daq_run(tgt, "PEDESTAL", buffer, nevents, pftool::state.daq_rate);
+
+    auto data = buffer.get_buffer();
+
+    std::vector<int> adcs_l0;
+    std::vector<int> adcs_l1;
+
+    for (std::size_t i{0}; i < data.size(); i++) {
+      if constexpr (std::is_same_v<
+                        EventPacket,
+                        pflib::packing::MultiSampleECONDEventPacket>) {
+        adcs_l0.push_back(
+            data[i].samples[data[i].i_soi].channel(0, channels[0]).adc());
+        adcs_l1.push_back(
+            data[i].samples[data[i].i_soi].channel(1, channels[1]).adc());
+      } else if constexpr (std::is_same_v<
+                               EventPacket,
+                               pflib::packing::SingleROCEventPacket>) {
+        adcs_l0.push_back(data[i].channel(channels[0]).adc());
+        adcs_l1.push_back(data[i].channel(channels[1]).adc());
+      } else {
+        PFEXCEPTION_RAISE("BadConf",
+                          "Unable to get adc for the cofigured format");
+        }
+      }
+    pedestals_l0.push_back(pflib::utility::median(adcs_l0));
+    stds_l0.push_back(pflib::utility::stdev(adcs_l0));
+    pedestals_l1.push_back(pflib::utility::median(adcs_l1));
+    stds_l1.push_back(pflib::utility::stdev(adcs_l1));
+    inv_vrefs.push_back(inv_vref);
+    }
+  // sort data and fit
+  DataFitter fitter_l0;
+  DataFitter fitter_l1;
+  fitter_l0.sort_and_append(inv_vrefs, pedestals_l0, stds_l0, step);
+  fitter_l1.sort_and_append(inv_vrefs, pedestals_l1, stds_l1, step);
+  inv_vref_tgt[0] = fitter_l0.fit(target_adc);
+  inv_vref_tgt[1] = fitter_l1.fit(target_adc);
+  noinv_vref_tgt[0] = noinv_vref;
+  noinv_vref_tgt[1] = noinv_vref;
+}
+
+std::map<std::string, std::map<std::string, uint64_t>> inv_vref_lund(
+    Target* tgt, ROC& roc) {
+
+  std::array<int, 2> inv_vref;
+  std::array<int, 2> noinv_vref;
+
+  if (pftool::state.daq_format_mode == Target::DaqFormat::SIMPLEROC) {
+    inv_vref_scan_getter<pflib::packing::SingleROCEventPacket>(
+        tgt, roc, nevents, channels, inv_vref, noinv_vref);
+  } else if (pftool::state.daq_format_mode ==
+             Target::DaqFormat::ECOND_SW_HEADERS) {
+    inv_vref_scan_getter<pflib::packing::MultiSampleECONDEventPacket>(
+        tgt, roc, nevents, channels, inv_vref, noinv_vref);
+  } else {
+    pflib_log(warn) << "Unsupported DAQ format ("
+                    << static_cast<int>(pftool::state.daq_format_mode)
+                    << ") in level_pedestals. Skipping pedestal leveling...";
+  }
+  std::map<std::string, std::map<std::string, uint64_t>> settings;
+  for (int i_link{0}; i_link < 2; i_link++) {
+    auto refvol_page =
+        pflib::utility::string_format("REFERENCEVOLTAGE_%d", i_link);
+    settings[refvol_page]["INV_VREF"] = inv_vref[i_link];
+    settings[refvol_page]["NOINV_VREF"] = noinv_vref[i_link];
+  }
+  return settings;
+}
+
+}  // namespace pflib::algorithm
---
+++ app/tool/algorithm/inv_vref_lund.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "pflib/Target.h"
+
+/**
+ * @namespace pflib::algorithm
+ * housing of higher-level methods for repeatable tasks
+ */
+namespace pflib::algorithm {
+
+/**
+ *
+ * Data class that stores information about each point in a 1d space.
+ * sort_and_append() sorts the data according to inv_vref selections that locate
+ * a linear region in the INV_VREF parameter space, and then appends to each Point
+ * fit() fits the linear region according to a standard linear fit.
+ *
+ */
+class DataFitter
+{
+  public:
+    DataFitter();
+
+    // Member functions
+
+    void sort_and_append(std::vector<int>& inv_vrefs,
+                         std::vector<int>& pedestals,
+                         std::vector<double>& stds,
+                         int& step);
+    int fit(int target);
+
+    // Member variables
+
+    struct Point {
+      int x_;
+      int y_;
+      double LH_;
+      double RH_;
+    };
+    std::vector<Point> linear_;
+    std::vector<Point> nonlinear_;
+    int LH_median_;
+    double LH_std_median_;
+    int RH_median_;
+
+};
+
+/**
+ * Find the inv_vref parameters. The noinv_vref parameters are 
+ * hardcoded to 612 for each link.
+ *
+ * @param[in] tgt pointer to Target to interact with
+ *
+ * @note Only functional for single-ROC targets
+ */
+std::map<std::string, std::map<std::string, uint64_t>> inv_vref_scan(
+    Target* tgt, ROC& roc);
+
+}  // namespace pflib::algorithm
