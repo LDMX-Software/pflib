5396adc6d7da76510c41c6589ab1ececa286c533
blaise <blazeiro@gmail.com>
2026-04-18 15:46:45 +0200

    Split the links in the figures and added linear-regression p-value and gradient evaluation

--- ana/charge/time-scan-evaluation.py
+++ ana/charge/time-scan-evaluation.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import json
 import os
+from scipy import stats
 
 from pathlib import Path
 import argparse
 
 # ------- PARSER -------
 
-plot_types = ['HEATMAP', 'SCATTER']
-
 parser = argparse.ArgumentParser()
 parser.add_argument('datasets', type=Path, nargs="+", help='Parameter timescan of one channel, one CALIB, n samples per time-point')
-parser.add_argument('-p', '--plot', choices=plot_types, type=str, help=f'Plot results. Available types: {", ".join(plot_types)}')
+parser.add_argument('-p', '--plot', action='store_true', help=f'Plot results')
+parser.add_argument('-g', '--goodness_of_fit', action='store_true', help='Perform a goodness-of-fit evaluation for a linear fit')
 parser.add_argument('-pd', '--plot_directory', type=Path, help='Figures directory path. If not provided, figures are not saved automatically')
-parser.add_argument('-csv', '--csv', type=Path, help='Save the scan results to a csv with the given path')
+parser.add_argument('-csv', '--csv', type=Path, help='Save the fit results to a csv with the given path')
 args = parser.parse_args()
 
 # ------- LOADING & SORTING DATA -------
@@ -48,28 +48,40 @@ def read_data(path: Path):
 
     time_points = np.unique(data["time"].to_numpy())
     avg_adc = []
+    std_dev = []
 
     for time_point in time_points:
         avg_adc.append(np.mean(data[data.time == time_point].adc))
-    avg_peak = round(max(avg_adc))
+    avg_peak = (max(avg_adc))
 
-    return np.array((avg_peak, run_params["calib"], run_params["channel"], datatype))
+    return np.array((avg_peak, run_params["calib"], run_params["channel"], link, datatype))
 
 
 def scatter_plot_evaluation(data, multi_type):
     
     calibs = np.unique(data.calib.to_numpy(dtype=np.int64))
-    print(calibs)
-
     if not multi_type:
+        fig, (ax1,ax2) = plt.subplots(2,1,figsize=(12,8))
+
         for calib in calibs:
-            plt.scatter(data[data.calib==str(calib)].channel.to_numpy(dtype=np.int64), data[data.calib==str(calib)].peak.to_numpy(dtype=np.int64), alpha = 0.5, label=f"calib{calib}, {data[data.calib==str(calib)].scan_type.to_numpy()[0]}")
-        plt.axvline(35, label="Link border", linestyle='--', c='r')
-        plt.xticks(np.arange(72))
-        plt.xlabel("Channels")
-        plt.ylabel("Average peak ADC [a.u.]")
-        plt.grid()
-        plt.legend()
+
+            link0_peaks = data[(data.calib==str(calib)) & (data.link == str(0))].peak.to_numpy(dtype=np.float64)
+            link1_peaks = data[(data.calib==str(calib)) & (data.link == str(1))].peak.to_numpy(dtype=np.float64)
+            ax1.scatter(np.arange(36), link0_peaks, alpha = 0.5, label=f"calib{calib}, {data[data.calib==str(calib)].scan_type.to_numpy()[0]}")
+            ax2.scatter(np.arange(36,72), link1_peaks, alpha = 0.5, label=f"calib{calib}, {data[data.calib==str(calib)].scan_type.to_numpy()[0]}")
+        ax1.set_xticks(np.arange(36))
+        ax1.set_title("Link 0")
+        ax1.set_xlabel("Channels")
+        ax1.set_ylabel("Average peak ADC [a.u.]")
+        ax1.grid()
+        ax1.legend()
+
+        ax2.set_xticks(np.arange(36,72))
+        ax2.set_title("Link 1")
+        ax2.set_xlabel("Channels")
+        ax2.set_ylabel("Average peak ADC [a.u.]")
+        ax2.grid()
+        ax2.legend()
         plt.show()
     
     else:
@@ -80,8 +92,8 @@ def scatter_plot_evaluation(data, multi_type):
             fig, (ax1,ax2) = plt.subplots(2,1,figsize=(12,8))
 
             for calib in calibs:
-                ax1.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[0])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[0])].peak.to_numpy(dtype=np.int64), label=f"calib{calib}, {types[0]}")
-                ax2.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[1])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[1])].peak.to_numpy(dtype=np.int64), label=f"calib{calib}, {types[1]}")
+                ax1.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[0])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[0])].peak.to_numpy(dtype=np.float64), label=f"calib{calib}, {types[0]}")
+                ax2.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[1])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[1])].peak.to_numpy(dtype=np.float64), label=f"calib{calib}, {types[1]}")
             
             ax1.grid()
             ax2.grid()
@@ -94,9 +106,9 @@ def scatter_plot_evaluation(data, multi_type):
             fig, (ax1,ax2,ax3) = plt.subplots(3,1,figsize=(12,8))
 
             for calib in calibs:
-                ax1.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[0])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[0])].peak.to_numpy(dtype=np.int64), label=f"calib{calib}, {types[0]}")
-                ax2.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[1])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[1])].peak.to_numpy(dtype=np.int64), label=f"calib{calib}, {types[1]}")
-                ax3.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[2])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[2])].peak.to_numpy(dtype=np.int64), label=f"calib{calib}, {types[2]}")
+                ax1.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[0])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[0])].peak.to_numpy(dtype=np.float64), label=f"calib{calib}, {types[0]}")
+                ax2.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[1])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[1])].peak.to_numpy(dtype=np.float64), label=f"calib{calib}, {types[1]}")
+                ax3.scatter(data[(data.calib==str(calib)) & (data.scan_type==types[2])].channel.to_numpy(dtype=np.int64), data[(data.calib==str(calib)) & (data.scan_type==types[2])].peak.to_numpy(dtype=np.float64), label=f"calib{calib}, {types[2]}")
 
             ax1.grid()
             ax2.grid()
@@ -106,18 +118,37 @@ def scatter_plot_evaluation(data, multi_type):
 
         else: print("Too many scan types! Maximum: 3")
 
+def gradient_evaluation(data):
+    
+    results = []
+    calibs = np.unique(data.calib.to_numpy(dtype=np.int64))
+    types = np.unique(data.scan_type.to_numpy())
+
+    for Type in types:
+        for calib in calibs:
+            link0_peaks = data[(data.calib==str(calib)) & (data.link == str(0)) & (data.scan_type == Type)].peak.to_numpy(dtype=np.float64)
+            link1_peaks = data[(data.calib==str(calib)) & (data.link == str(1)) & (data.scan_type == Type)].peak.to_numpy(dtype=np.float64)
+            
+            gradient0, intercept0, r_value0, p_value0, std_err0 = stats.linregress(np.arange(36), link0_peaks)
+            gradient1, intercept1, r_value1, p_value1, std_err1 = stats.linregress(np.arange(36,72), link1_peaks)
+            
+            goodness_of_fit = np.array(([[gradient0, p_value0], [gradient1, p_value1]]))
+            results.append([goodness_of_fit, calib, Type])
+    
+    return results
+
 data = []
 for dataset in args.datasets:
     data.append(read_data(dataset))
 data = np.array(data)
 
-data_sorted = {"peak" : data[:,0], "calib" : data[:,1], "channel" : data[:,2], "scan_type" : data[:,3]}
+data_sorted = {"peak" : data[:,0], "calib" : data[:,1], "channel" : data[:,2], "link" : data[:,3], "scan_type" : data[:,4]}
 if len(np.unique(data_sorted["scan_type"])) > 1: multi_type = True
 else: multi_type = False
 df = pd.DataFrame(data_sorted)
 
-
-scatter_plot_evaluation(df, multi_type)
-
-
-
+if args.plot : scatter_plot_evaluation(df, multi_type)
+if args.goodness_of_fit : 
+    fit = gradient_evaluation(df)
+    if args.csv : print("Save to csv - WIP")
+    else: print(fit)
