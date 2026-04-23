6fd9c2cc9a7edb44e2fe843a54a774b9087e3633
Andreas Pettersson <appettersson@hotmail.com>
2026-04-23 12:00:38 +0200

    added calib input parameters

--- app/tool/tasks/channel_wise_calib_scan.cxx
+++ app/tool/tasks/channel_wise_calib_scan.cxx
@@ -16,7 +16,7 @@ void channel_wise_calib_scan(Target* tgt) {
   int min_calib = pftool::readline_int("Minimum calib value = ", 0);
   int max_calib = pftool::readline_int("Maximum calib value = ", 550);
   if ((min_calib < 0) || (min_calib > 4095) || 
-      (max_calib < 0) || (max_calib > 4095) {
+      (max_calib < 0) || (max_calib > 4095)) {
       throw std::invalid_argument("Min and Max calib values have to be within the range: 0 <= calib <= 4095");
   }
   pflib::ROC roc{tgt->roc(pftool::state.iroc)};
