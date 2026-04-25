cc431f9d948d817701056ac89c6544fe504e0369
blaise <blazeiro@gmail.com>
2026-04-25 18:30:10 +0200

    Polished up the figures, added RMS, added saving options for figures and csv results

--- ana/charge/time-scan-evaluation.py
+++ ana/charge/time-scan-evaluation.py
@@ -20,6 +20,13 @@ args = parser.parse_args()
 
 # ------- LOADING & SORTING DATA -------
 
+def root_mean_square(adc):
+        mean_adc = np.mean(adc)
+        sum = 0
+        for sample in adc:
+            sum += (sample-mean_adc)**2
+        return np.sqrt(sum/len(adc))
+
 def read_data(path: Path):
     
     '''To allow for sorting later in the script, the scan modes (or datatypes), that is preCC, lowrange and highrange, are initially assigned integers 0, 1, 2, respectively.'''
@@ -50,28 +57,30 @@ def read_data(path: Path):
 
     time_points = np.unique(data["time"].to_numpy())
     avg_adc = []
-    std_dev = []
+    rms = []
 
     for time_point in time_points:
         avg_adc.append(np.mean(data[data.time == time_point].adc))
+        rms.append(root_mean_square(data[data.time == time_point].adc.to_numpy()))
     avg_peak = (max(avg_adc))
+    avg_peak_index = avg_adc.index(avg_peak)
+    rms_peak = rms[avg_peak_index]
 
-    return np.array((avg_peak, run_params["calib"], run_params["channel"], link, datatype))
-
+    return np.array((avg_peak, run_params["calib"], run_params["channel"], link, datatype, rms_peak))
 
 def scatter_plot_evaluation(data, data_type):
     
     calibs = np.unique(data.calib.to_numpy(dtype=np.int64))
 
     
-    fig, (ax1,ax2) = plt.subplots(2,1,figsize=(12,8))
+    fig, (ax1,ax2) = plt.subplots(2,1,figsize=(12,10))
 
     for calib in calibs:
 
         link0_peaks = data[(data.calib==calib) & (data.link == 0)].peak.to_numpy(dtype=np.float64)
         link1_peaks = data[(data.calib==calib) & (data.link == 1)].peak.to_numpy(dtype=np.float64)
-        ax1.scatter(np.arange(36), link0_peaks, alpha = 0.5, label=f"calib{calib}, {data_type}")
-        ax2.scatter(np.arange(36,72), link1_peaks, alpha = 0.5, label=f"calib{calib}, {data_type}")
+        ax1.scatter(np.arange(36), link0_peaks, alpha = 0.5, label=f"CALIB{calib}")
+        ax2.scatter(np.arange(36,72), link1_peaks, alpha = 0.5, label=f"CALIB{calib}")
     ax1.set_xticks(np.arange(36))
     ax1.set_title("Link 0")
     ax1.set_xlabel("Channels")
@@ -85,24 +94,98 @@ def scatter_plot_evaluation(data, data_type):
     ax2.set_ylabel("Average peak ADC [a.u.]")
     ax2.grid()
     ax2.legend(bbox_to_anchor=(1.1, 1), loc = 'upper right')
-    plt.show()
+    fig.suptitle(f"Mean-ADC-peak over channels - {data_type}", fontsize=14)
+
+    if args.plot_directory:
+        plt.savefig(os.path.join(args.plot_directory,f'time-scan-evaluation-{data_type}.png'), dpi=400)
+        plt.close()
+    else:
+        plt.show()
+        plt.close()
+
+    fig, (ax1,ax2) = plt.subplots(2,1,figsize=(12,10))
+
+    for calib in calibs:
+
+        link0_rms = data[(data.calib==calib) & (data.link == 0)].rms.to_numpy(dtype=np.float64)
+        link1_rms = data[(data.calib==calib) & (data.link == 1)].rms.to_numpy(dtype=np.float64)
+        ax1.plot(np.arange(36), link0_rms, alpha = 0.5, label=f"CALIB{calib}")
+        ax2.plot(np.arange(36,72), link1_rms, alpha = 0.5, label=f"CALIB{calib}")
+    ax1.set_xticks(np.arange(36))
+    ax1.set_title("Link 0")
+    ax1.set_xlabel("Channels")
+    ax1.set_ylabel("RMS of average peak ADC [a.u.]")
+    ax1.grid()
+    ax1.legend(bbox_to_anchor=(1.1, 1), loc = 'upper right')
+
+    ax2.set_xticks(np.arange(36,72))
+    ax2.set_title("Link 1")
+    ax2.set_xlabel("Channels")
+    ax2.set_ylabel("RMS of average peak ADC [a.u.]")
+    ax2.grid()
+    ax2.legend(bbox_to_anchor=(1.1, 1), loc = 'upper right')
+
+    fig.suptitle(f"Mean-ADC-peak-RMS over channels - {data_type}", fontsize=14)
+
+    if args.plot_directory:
+        plt.savefig(os.path.join(args.plot_directory,f'time-scan-evaluation-rms-{data_type}.png'), dpi=400)
+        plt.close()
+    else:
+        plt.show()
+        plt.close()
 
 def gradient_evaluation(data):
     
-    results = []
-    calibs = np.unique(data.calib.to_numpy(dtype=np.int64))
+    gradient = False # I think the gradient/R2 evaluation doesn't really make much sense, it's probably better to take the RMS of the mean-peak distribution instead
 
-    for calib in calibs:
-        link0_peaks = data[(data.calib==calib) & (data.link == str(0))].peak.to_numpy(dtype=np.float64)
-        link1_peaks = data[(data.calib==calib) & (data.link == str(1))].peak.to_numpy(dtype=np.float64)
+    if gradient:
+
+        results = []
+        calibs = np.unique(data.calib.to_numpy(dtype=np.int64))
+
+        results = {"CALIB" : [], "link_0_gradient" : [], "link_0_R2" : [], "link_1_gradient" : [], "link_1_R2" : []}
+
+        for calib in calibs:
+            link0_peaks = data[(data.calib==calib) & (data.link == 0)].peak.to_numpy(dtype=np.float64)
+            link1_peaks = data[(data.calib==calib) & (data.link == 1)].peak.to_numpy(dtype=np.float64)
+            
+            gradient0, intercept0, r_value0, p_value0, std_err0 = stats.linregress(np.arange(36), link0_peaks)
+            gradient1, intercept1, r_value1, p_value1, std_err1 = stats.linregress(np.arange(36,72), link1_peaks)
+            
+            results["CALIB"].append(calib)
+            results["link_0_gradient"].append(gradient0)
+            results["link_1_gradient"].append(gradient1)
+            results["link_0_R2"].append(r_value0**2)
+            results["link_1_R2"].append(r_value1**2)
         
-        gradient0, intercept0, r_value0, p_value0, std_err0 = stats.linregress(np.arange(36), link0_peaks)
-        gradient1, intercept1, r_value1, p_value1, std_err1 = stats.linregress(np.arange(36,72), link1_peaks)
-        
-        goodness_of_fit = np.array(([[gradient0, r_value0], [gradient1, r_value1]]))
-        results.append([goodness_of_fit, calib])
-    
-    return results
+        if args.csv : 
+            df = pd.DataFrame(results)
+            df.to_csv(args.csv)
+        else:
+            print(results)
+
+    else:
+        results = []
+        calibs = np.unique(data.calib.to_numpy(dtype=np.int64))
+
+        results = {"CALIB" : [], "link_0_RMS" : [], "link_1_RMS" : []}
+
+        for calib in calibs:
+            link0_peaks = data[(data.calib==calib) & (data.link == 0)].peak.to_numpy(dtype=np.float64)
+            link1_peaks = data[(data.calib==calib) & (data.link == 1)].peak.to_numpy(dtype=np.float64)
+
+            link0_rms = root_mean_square(link0_peaks)
+            link1_rms = root_mean_square(link1_peaks)
+
+            results["CALIB"].append(calib)
+            results["link_0_RMS"].append(link0_rms)
+            results["link_1_RMS"].append(link1_rms)
+
+        if args.csv : 
+            df = pd.DataFrame(results)
+            df.to_csv(args.csv)
+        else:
+            print(results)
 
 data = []
 for dataset in args.datasets:
@@ -110,7 +193,7 @@ for dataset in args.datasets:
 data = np.array(data)
 data = data[data[:,2].argsort()]
 
-data_sorted = {"peak" : data[:,0], "calib" : data[:,1], "channel" : data[:,2], "link" : data[:,3]}
+data_sorted = {"peak" : data[:,0], "calib" : data[:,1], "channel" : data[:,2], "link" : data[:,3], "rms" : data[:,5]}
 
 if data[0,4] == 0 : scan_type = "preCC"
 if data[0,4] == 1 : scan_type = "lowrange"
@@ -120,6 +203,4 @@ df = pd.DataFrame(data_sorted)
 
 if args.plot : scatter_plot_evaluation(df, scan_type)
 if args.goodness_of_fit : 
-    fit = gradient_evaluation(df)
-    if args.csv : print("Save to csv - WIP")
-    else: print(fit)
+    gradient_evaluation(df)
