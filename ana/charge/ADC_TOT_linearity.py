c2ed2d12afbdd36f281d9a234c939dd472b491ba
blaise <blazeiro@gmail.com>
2026-04-27 16:03:38 +0200

    Added some quality-of-life features, mainly initial arguments for plot-type choice. Also added a linear fit to the ADC and TOT, with the TOT fitted after a given threshold

--- ana/charge/ADC_TOT_linearity.py
+++ ana/charge/ADC_TOT_linearity.py
@@ -4,10 +4,18 @@ import pandas as pd
 from pathlib import Path
 import argparse
 import matplotlib.cm as cm
+import os
+import scipy
 import matplotlib.colors as mcolors
+
+plot_options = ["ADC-TOT", "TOT-HEATMAP", "TOT-TIME", "ALL"]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('datasets', type=Path, nargs='+', help='Global calib scan file paths of desired board.')
+parser.add_argument('-sd', '--save_directory', type=Path, help='Path to directory, in which the results will be saved. Default is directory before the source path.')
+parser.add_argument('-fl', '--fit_line', action='store_true', help='Fit a linear function to the ADC and TOT and save the results.')
+parser.add_argument('-flt', '--fit_line_threshold', type=int, help='The TOT linear fit will only include values above this threshold.')
+parser.add_argument('-p', '--plot', choices = plot_options, help='Plot options.')
 args = parser.parse_args()
 
 # Folder where the CSV lives
@@ -28,6 +36,8 @@ for i, f in enumerate(args.datasets):
 
 df = pd.concat(dfs, ignore_index=True)
 
+if args.fit_line: fit_data = {'Channel' : [], 'ADC_R2' : [], 'TOT_R2' : []}
+
 for ch, df_ch in df.groupby("channel"):   
 
     df_valid = df_ch.copy()
@@ -40,75 +50,116 @@ for ch, df_ch in df.groupby("channel"):
     df_adc = df_max.copy()
     df_adc.loc[df_adc["tot"].notna(), "adc"] = np.nan #make ADC be NaN where TOT is active (not NaN)
 
-    fig, ax1 = plt.subplots()
-    # ADC region 
-    ax1.scatter(df_adc["calib"], df_adc["adc"], color="red", s=2, label="ADC")
-    ax1.set_xlabel("calib")
-    ax1.set_ylabel("Max ADC [a.u.]", color="red")
-    ax1.set_axisbelow(True)
-    ax1.grid(True)
+    if args.fit_line:
 
-    # TOT region 
-    ax2 = ax1.twinx() #creates second y-axis on the right side
-    ax2.scatter(df_med2["calib"], df_med2["tot"], color="blue", s=2, label="TOT (median) [TDC]")
-    ax2.set_ylabel("Median TOT [TDC]", color="blue")
-    #plt.legend()
-    plt.title(f"Channel {ch}")
-    plt.savefig(output_dir / f"channel_{ch}.png", dpi=300)
-    plt.close()
+        adc_fit_results = scipy.stats.linregress(df_adc["calib"][(df_adc["adc"] < 1023) & (df_adc["adc"] != np.nan)], df_adc["adc"][(df_adc["adc"] < 1023) & (df_adc["adc"] != np.nan)])
+        adc_fit = df_adc["calib"][(df_adc["adc"] < 1023) & (df_adc["adc"] != np.nan)]*adc_fit_results.slope + adc_fit_results.intercept
 
-    # compare raw data to median to max of median 
-    fig, ax = plt.subplots()
-    df_tot = df_ch[df_ch["tot"] > 0] #raw TOT values
+        tot_fit_results = scipy.stats.linregress(df_med2["calib"][df_med2["tot"] > args.fit_line_threshold], df_med2["tot"][df_med2["tot"] > args.fit_line_threshold])
+        tot_fit = df_med2["calib"][df_med2["tot"] > args.fit_line_threshold]*tot_fit_results.slope + tot_fit_results.intercept
 
-    #ax.scatter(df_tot["calib"], df_tot["tot"], color="dimgray", s=20, label="raw TOT") #raw TOT
-    #ax.scatter(df_med[df_med["tot"] > 0]["calib"], df_med[df_med["tot"] > 0]["tot"], color="turquoise", s=15, marker="x", label="median TOT") # median per (calib, time)
-    #ax.scatter(df_max[df_max["tot"] > 0]["calib"], df_max[df_max["tot"] > 0]["tot"], color="red", s=40, marker="x", label="max TOT") #max per calib
+        fit_data['ADC_R2'].append(adc_fit_results.rvalue**2)
+        fit_data['TOT_R2'].append(tot_fit_results.rvalue**2)
+        fit_data['Channel'].append(ch)
 
-    norm = mcolors.Normalize(vmin=df_tot["calib"].min(), vmax=df_tot["calib"].max())
-    cmap = plt.get_cmap("plasma")
+    if (args.plot == "ADC-TOT") or (args.plot == "ALL"):
 
-    sc = ax.scatter(df_tot["calib"], df_tot["tot"], c=df_tot["calib"], cmap=cmap, norm=norm, s=10)
+        fig, ax1 = plt.subplots()
+        # ADC region 
+        if args.fit_line: ax1.plot(df_adc["calib"][(df_adc["adc"] < 1023) & (df_adc["adc"] != np.nan)], adc_fit, color="red", alpha = 0.5, linestyle='--', 
+                                   label=f'ADC fit: y = {adc_fit_results.slope:.3g}x + {adc_fit_results.intercept:.3g}')
+        ax1.scatter(df_adc["calib"], df_adc["adc"], color="red", s=2)
+        ax1.set_xlabel("CALIB")
+        ax1.set_ylabel("Max ADC [a.u.]", color="red")
+        ax1.set_axisbelow(True)
+        ax1.grid(True)
 
-    cbar = plt.colorbar(sc, ax=ax)
-    cbar.set_label("calib")
-    ax.set_xlabel("calib")
-    ax.set_ylabel("TOT [TDC]")
-    ax.legend()
-    plt.title(f"Channel {ch}, TOT distribution")
-    ax.set_axisbelow(True)
-    ax.grid(True)
-    plt.savefig(output_dir / f"channel_{ch}_TOT_raw.png", dpi=300)
-    plt.close()
+        # TOT region 
+        ax2 = ax1.twinx() #creates second y-axis on the right side
+        if args.fit_line: ax2.plot(df_med2["calib"][df_med2["tot"] > args.fit_line_threshold], tot_fit, color="blue", alpha = 0.5, linestyle='--',
+                                   label=f'TOT fit: y = {tot_fit_results.slope:.3g}x + {tot_fit_results.intercept:.3g}')
+        ax2.scatter(df_med2["calib"], df_med2["tot"], color="blue", s=2)
+        ax2.set_ylabel("Median TOT [a.u.]", color="blue")
+        
+        ax1.legend(bbox_to_anchor=(1.0, 0.1), loc = 'lower right')
+        ax2.legend(loc = 'lower right')
+        plt.title(f"Channel {ch}")
+        if args.save_directory:
+            plt.savefig(os.path.join(args.save_directory, f"channel_{ch}.png"), dpi=400)
+            plt.close()
+        else:
+            plt.savefig(output_dir / f"channel_{ch}.png", dpi=300)
+            plt.close()
 
-    # TOT vs time as well, but calibs as lines
+    if (args.plot == "TOT-HEATMAP") or (args.plot == "ALL"):    
 
-    fig, ax = plt.subplots()
+        # compare raw data to median to max of median 
+        fig, ax = plt.subplots()
+        df_tot = df_ch[df_ch["tot"] > 0] #raw TOT values
 
-    # Normalize calib range to [0, 1]
-    norm = mcolors.Normalize(vmin=df_med["calib"].min(), vmax=df_med["calib"].max())
+        #ax.scatter(df_tot["calib"], df_tot["tot"], color="dimgray", s=20, label="raw TOT") #raw TOT
+        #ax.scatter(df_med[df_med["tot"] > 0]["calib"], df_med[df_med["tot"] > 0]["tot"], color="turquoise", s=15, marker="x", label="median TOT") # median per (calib, time)
+        #ax.scatter(df_max[df_max["tot"] > 0]["calib"], df_max[df_max["tot"] > 0]["tot"], color="red", s=40, marker="x", label="max TOT") #max per calib
 
-    cmap = plt.get_cmap("plasma")  # good default
+        norm = mcolors.Normalize(vmin=df_tot["calib"].min(), vmax=df_tot["calib"].max())
+        cmap = plt.get_cmap("plasma")
 
-    for calib, df_c in df_med.groupby("calib"):
+        sc = ax.scatter(df_tot["calib"], df_tot["tot"], c=df_tot["calib"], cmap=cmap, norm=norm, s=10)
 
-        df_c = df_c[df_c["tot"].notna()]
-        if df_c.empty:
-            continue
+        cbar = plt.colorbar(sc, ax=ax)
+        cbar.set_label("CALIB")
+        ax.set_xlabel("CALIB")
+        ax.set_ylabel("TOT [a.u.]")
+        ax.legend()
+        plt.title(f"Channel {ch}, TOT distribution")
+        ax.set_axisbelow(True)
+        ax.grid(True)
+        if args.save_directory:
+            plt.savefig(os.path.join(args.save_directory, f"channel_{ch}_TOT_raw.png"), dpi=400)
+            plt.close()
+        else:
+            plt.savefig(output_dir / f"channel_{ch}_TOT_raw.png", dpi=300)
+            plt.close()
 
-        df_c = df_c.sort_values("time")
+    if (args.plot == "TOT-TIME") or (args.plot == "ALL"):
 
-        ax.plot(df_c["time"], df_c["tot"], color=cmap(norm(calib)), linewidth=1, alpha=0.8)
+        # TOT vs time as well, but calibs as lines
 
-    sm = cm.ScalarMappable(norm=norm, cmap=cmap) #for colorbar
-    sm.set_array([])
-    cbar = plt.colorbar(sm, ax=ax)
-    cbar.set_label("calib")
+        fig, ax = plt.subplots()
 
-    ax.set_xlabel("time")
-    ax.set_ylabel("TOT [TDC]")
-    ax.set_title(f"Channel {ch} – TOT vs time")
-    ax.set_axisbelow(True)
-    ax.grid(True)
-    plt.savefig(output_dir / f"channel_{ch}_TOT_vs_time_lines.png", dpi=300)
-    plt.close()
+        # Normalize calib range to [0, 1]
+        norm = mcolors.Normalize(vmin=df_med["calib"].min(), vmax=df_med["calib"].max())
+
+        cmap = plt.get_cmap("plasma")  # good default
+
+        for calib, df_c in df_med.groupby("calib"):
+
+            df_c = df_c[df_c["tot"].notna()]
+            if df_c.empty:
+                continue
+
+            df_c = df_c.sort_values("time")
+
+            ax.plot(df_c["time"], df_c["tot"], color=cmap(norm(calib)), linewidth=1, alpha=0.8)
+
+        sm = cm.ScalarMappable(norm=norm, cmap=cmap) #for colorbar
+        sm.set_array([])
+        cbar = plt.colorbar(sm, ax=ax)
+        cbar.set_label("CALIB")
+
+        ax.set_xlabel("time")
+        ax.set_ylabel("TOT [a.u.]")
+        ax.set_title(f"Channel {ch} – TOT vs time")
+        ax.set_axisbelow(True)
+        ax.grid(True)
+        if args.save_directory:
+            plt.savefig(os.path.join(args.save_directory, f"channel_{ch}_TOT_vs_time_lines.png"), dpi=400)
+            plt.close()
+        else:
+            plt.savefig(output_dir / f"channel_{ch}_TOT_vs_time_lines.png", dpi=300)
+            plt.close()
+
+if args.fit_line:
+    results_df = pd.DataFrame(fit_data)
+    if args.save_directory : results_df.to_csv(os.path.join(args.save_directory, "fit_results.csv"))
+    else: results_df.to_csv(output_dir / "fit_results.csv")
