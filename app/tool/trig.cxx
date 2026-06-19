a7727b9c0f2df0faed8f5414f060b01f148c57e7
tomeichlersmith <eichl008@umn.edu>
2026-06-19 14:51:49 +0000

    option to unpack align_read words

--- app/tool/trig.cxx
+++ app/tool/trig.cxx
@@ -56,12 +56,31 @@ void trig(const std::string& cmd, Target* target) {
   }
   if (cmd == "ALIGN_READ") {
     bool do_fc = pftool::readline_bool("Generate LINKRESET_ECONT?", true);
+    bool show_raw = pftool::readline_bool("Show raw data [Y] or idle word interpretation [N]? ", false);
     if (do_fc) target->fc().linkreset_econs();
     usleep(2000);
     for (int ilink = 0; ilink < trig->n_elinks(); ilink++) {
       std::vector<uint32_t> val = trig->read_capture_buffer(ilink);
-      printf("%02d : ", ilink);
-      for (auto x : val) printf(" %08x", x);
+      // see Section 20 of the ECON-T manual,
+      // at UMN we have ECON-T-P1 on the ECON Mezzanine
+      // each eTx produces two 16-bit IDLE words per BX
+      // and an IDLE word looks like
+      //  5b BX | 11b Pattern
+      // where BX is a BX counter and the 11b Pattern is set by
+      // formatterbuffer.global.link_reset_pattern ? or idle_pattern?
+      printf("%02d :", ilink);
+      for (auto x : val) {
+        if (show_raw) {
+          printf(" %08x", x);
+        } else {
+          printf(" | %02d %03x %02d %03x",
+                  (x >> (16+11)) & 0x1f,
+                  (x >> 16) & 0x7ff,
+                  (x >> 11) & 0x1f,
+                  (x & 0x7ff)
+                );
+        }
+      }
       printf("\n");
     }
   }
