6b13d10ba2492db4b023faa1ff63f50a6fbfdd65
tomeichlersmith <eichl008@umn.edu>
2026-07-07 17:29:38 +0000

    support for injecting test patterns into serializer
    this is only present on the software side, I'm only ever getting
    constant words when I spy so I suspect the firmware needs to be updated
    to allow for a raw reading or something that doesn't require the format
    as output by the serializer

--- app/tool/tasks/setup/lpgbt_backend_check.cxx
+++ app/tool/tasks/setup/lpgbt_backend_check.cxx
@@ -1,47 +1,139 @@
 #include "lpgbt_backend_check.h"
 
 #include <bitset>
+#include <optional>
 
 #include "pflib/OptoLink.h"
 #include "pflib/lpGBT.h"
+#include "pflib/packing/Hex.h"
+using pflib::packing::hex;
 
 ENABLE_LOGGING();
 
-// each output group {0..3} has 2 bits shifted by group*2
-static const uint16_t ULDATASOURCE1 = 0x129;
 static const uint16_t DPDATAPATTERN[4] = {0x131, 0x130, 0x12f, 0x12e};
 
-enum UpLinkDataSourceCode : int {
-  NORMAL_DATA = 0,
-  PRBS7 = 1,
-  BIN_CNTR_UP = 2,
-  BIN_CNTR_DOWN = 3,
-  CONST_PATTERN = 4,
-  CONST_PATTERN_INV = 5,
-  DL_DATA_LOOPBACK = 6,
+class InjectTestPattern {
+ protected:
+  pflib::lpGBT& lpgbt_;
+  void write_DPDataPattern(uint32_t val) {
+    for (int i_byte{0}; i_byte < 4; i_byte++) {
+      lpgbt_.write(DPDATAPATTERN[i_byte],
+                  ((val >> (i_byte * 8)) & 0xff));
+    }
+  }
+ public:
+  InjectTestPattern(pflib::lpGBT& lpgbt) : lpgbt_{lpgbt} {}
+  virtual ~InjectTestPattern() = default;
+  virtual void check(const std::vector<uint32_t>& spy) = 0;
 };
 
-class ConfigureUpLinkDataSource {
-  pflib::lpGBT& lpgbt_;
-  UpLinkDataSourceCode choice_{0};
-
+class InjectIntoSerializer : public InjectTestPattern {
  public:
-  ConfigureUpLinkDataSource(pflib::lpGBT& l) : lpgbt_{l} {}
-  void choose(UpLinkDataSourceCode choice) { choice_ = choice; }
-  void configure(int ilink) {
-    int grp = ilink;
-    // on lpGBT mezzanine, links 3, 4, 5 are connected to eTx groups 4, 5, 6
-    if (grp > 2) grp++;
-    uint16_t datasource_config_reg = ULDATASOURCE1 + (grp / 2);
-    uint8_t datasource = lpgbt_.read(datasource_config_reg);
-    datasource |= (static_cast<int>(choice_) << ((grp % 2) * 3));
-    lpgbt_.write(datasource_config_reg, datasource);
-    printf("apply data source config 0x%02x on reg 0x%03x\n", datasource,
-           datasource_config_reg);
+  /**
+   * These are the options to replace the serializer
+   * data source.
+   *
+   * Table 14.1
+   */
+  enum Source : int {
+    DATA = 0,
+    PRBS7 = 1,
+    PRBS15 = 2,
+    PRBS23 = 3,
+    PRBS31 = 4,
+    CLK5G12 = 5,
+    CLK2G56 = 6,
+    CLK1G28 = 7,
+    CLK40M = 8,
+    DL_FRAME_10G24 = 9,
+    DL_FRAME_5G12 = 10,
+    DL_FRAME_2G56 = 11,
+    CONST_PATTERN = 12
+  };
+  
+  // ULSerTextPattern is lowset 4 bits of register 0x128
+  static const uint16_t ULSERTESTPATTERN_REG = 0x128;
+
+  InjectIntoSerializer(pflib::lpGBT& lpgbt, Source s, std::optional<uint32_t> known_pattern = {})
+  : InjectTestPattern{lpgbt}, choice_{s}, known_pattern_{known_pattern} {
+    // technically, sets ULECDataSource to 0 (EPORTRX_DATA)
+    // and ULSerTestPattern to our choice
+    lpgbt_.write(ULSERTESTPATTERN_REG, static_cast<int>(choice_));
+    if (known_pattern_) {
+      write_DPDataPattern(known_pattern_.value());
+    }
   }
-  void check(const std::vector<uint32_t> spy) {
+
+  void check(const std::vector<uint32_t>& spy) final {
+    switch(choice_) {
+      default:
+        break;
+    }
+  }
+
+  ~InjectIntoSerializer() {
+    // technically, resets both ULSerTestPattern and ULECDataSource
+    lpgbt_.write(ULSERTESTPATTERN_REG, 0x00);
+    if (known_pattern_.has_value()) {
+      write_DPDataPattern(0x00000000);
+    }
+  }
+
+ private:
+  Source choice_{Source::DATA};
+  std::optional<uint32_t> known_pattern_;
+};
+
+class InjectUpLinkDataSource : public InjectTestPattern {
+ public:
+  // each output group {0..3} has 2 bits shifted by group*2
+  static const uint16_t ULDATASOURCE1 = 0x129;
+
+  /**
+   * These are the "Up Link Pattern Gen" options
+   * to replace the data from a given eRx group.
+   *
+   * Table 14.2
+   */
+  enum Source : int {
+    NORMAL_DATA = 0,
+    PRBS7 = 1,
+    BIN_CNTR_UP = 2,
+    BIN_CNTR_DOWN = 3,
+    CONST_PATTERN = 4,
+    CONST_PATTERN_INV = 5,
+    DL_DATA_LOOPBACK = 6,
+  };
+
+  InjectUpLinkDataSource(
+    pflib::lpGBT& l,
+    Source s,
+    int ilink,
+    std::optional<uint32_t> known_pattern = {}
+  ) : InjectTestPattern{l}, choice_{s}, ilink_{ilink}, known_pattern_{known_pattern} {
+    if (ilink < 0) {
+      for (int i{0}; i < 6; i++) configure(i);
+    } else if (ilink < 6) {
+      configure(ilink);
+    } else {
+      PFEXCEPTION_RAISE("BadILink", "ilink is greater than 6 which is not allowed");
+    }
+    if (known_pattern_.has_value()) {
+      write_DPDataPattern(known_pattern_.value());
+    }
+  }
+  ~InjectUpLinkDataSource() {
+    // reset data source to all zeros for all groups (normal operation)
+    for (int i{0}; i < 4; i++) {
+      lpgbt_.write(ULDATASOURCE1 + i, 0x00);
+    }
+    if (known_pattern_.has_value()) {
+      write_DPDataPattern(0x00000000);
+    }
+  }
+  void check(const std::vector<uint32_t>& spy) final {
     switch (choice_) {
-      case UpLinkDataSourceCode::PRBS7: {
+      case Source::PRBS7: {
         std::bitset<64 * 32> data;
         for (int iword{0}; iword < 64; iword++) {
           for (int ibit{0}; ibit < 32; ibit++) {
@@ -66,18 +158,29 @@ class ConfigureUpLinkDataSource {
         }
         printf("%u / %u bit errors\n", diff.count(), diff.size() - 12);
       } break;
-      case UpLinkDataSourceCode::CONST_PATTERN: {
-        uint32_t const_pattern{0};
-        for (int i_byte{0}; i_byte < 4; i_byte++) {
-          const_pattern |= (lpgbt_.read(DPDATAPATTERN[i_byte]) << (i_byte * 8));
-        }
+      case Source::CONST_PATTERN: {
         printf("%u / %u words matched the const pattern 0x%08x\n",
-               std::count(spy.begin(), spy.end(), const_pattern), spy.size(),
-               const_pattern);
+               std::count(spy.begin(), spy.end(), known_pattern_.value()), spy.size(),
+               known_pattern_.value());
       } break;
       default:
         break;
     }
+  }
+ private:
+  int ilink_;
+  Source choice_{Source::NORMAL_DATA};
+  std::optional<uint32_t> known_pattern_;
+  void configure(int ilink) {
+    int grp = ilink;
+    // on lpGBT mezzanine, links 3, 4, 5 are connected to eTx groups 4, 5, 6
+    if (grp > 2) grp++;
+    uint16_t datasource_config_reg = ULDATASOURCE1 + (grp / 2);
+    uint8_t datasource = lpgbt_.read(datasource_config_reg);
+    datasource |= (static_cast<int>(choice_) << ((grp % 2) * 3));
+    lpgbt_.write(datasource_config_reg, datasource);
+    printf("apply data source config 0x%02x on reg 0x%03x\n", datasource,
+           datasource_config_reg);
   }
 };
 
@@ -86,51 +189,111 @@ void lpgbt_backend_check(Target* target) {
   pflib::lpGBT lpgbt{
       target->get_opto_link(daq ? "DAQ" : "TRG").lpgbt_transport()};
 
-  int output_choice = pftool::readline_int(
-      "Test Choice:\n  1. PRBS7\n  2. Binary Counter\n  3. Const Pattern\n", 3);
-  if (output_choice < 1 or output_choice > 3) {
-    pflib_log(error) << "unrecognized output choice " << output_choice;
-    return;
-  }
-
-  ConfigureUpLinkDataSource conf{lpgbt};
-  if (output_choice == 1) {
-    conf.choose(UpLinkDataSourceCode::PRBS7);
-  } else if (output_choice == 2) {
-    bool up = pftool::readline_bool("Count up (y) or down (n)? ", true);
-    if (up) {
-      conf.choose(UpLinkDataSourceCode::BIN_CNTR_UP);
-    } else {
-      conf.choose(UpLinkDataSourceCode::BIN_CNTR_DOWN);
+  int link = -1;
+  std::unique_ptr<InjectTestPattern> injector;
+  if (pftool::readline_bool("Inject test pattern into serializer (y) or eRx groups (n)? ", true)) {
+    int output_choice = pftool::readline_int(
+      "Test Choice:\n  1. PRBS7\n  2. CLK40M\n  3. Const Pattern\n", 3
+    );
+    if (output_choice < 1 or output_choice > 3) {
+      pflib_log(error) << "unrecognized output choice" << output_choice;
+      return;
     }
-  } else if (output_choice == 3) {
-    uint32_t known_pattern = 0x12345678;
-    known_pattern =
+    if (output_choice == 1) {
+      injector = std::make_unique<InjectIntoSerializer>(
+        lpgbt,
+        InjectIntoSerializer::Source::PRBS7
+      );
+    } else if (output_choice == 2) {
+      injector = std::make_unique<InjectIntoSerializer>(
+        lpgbt,
+        InjectIntoSerializer::Source::CLK40M
+      );
+    } else if (output_choice == 3) {
+      uint32_t known_pattern = 0x12345678;
+      known_pattern =
         pftool::readline_int("32b const pattern to use: ", known_pattern, true);
-    printf("const pattern: 0x%08x\n", known_pattern);
-    for (int i_byte{0}; i_byte < 4; i_byte++) {
-      lpgbt.write(DPDATAPATTERN[i_byte],
-                  ((known_pattern >> (i_byte * 8)) & 0xff));
+      injector = std::make_unique<InjectIntoSerializer>(
+        lpgbt,
+        InjectIntoSerializer::Source::CONST_PATTERN,
+        known_pattern
+      );
     }
-    bool invert = pftool::readline_bool("Invert the constant pattern? ", false);
-    if (invert) {
-      conf.choose(UpLinkDataSourceCode::CONST_PATTERN_INV);
-    } else {
-      conf.choose(UpLinkDataSourceCode::CONST_PATTERN);
+  } else {
+    link = pftool::readline_int("which link (-1 for all)? ", -1);
+    if (link > 5) {
+      pflib_log(error) << "invalid link index";
+      return;
+    }
+
+    int output_choice = pftool::readline_int(
+       "Test Choice:\n  1. PRBS7\n  2. Binary Counter\n  3. Const Pattern\n", 3);
+    if (output_choice < 1 or output_choice > 3) {
+      pflib_log(error) << "unrecognized output choice " << output_choice;
+      return;
+    }
+    if (output_choice == 1) {
+      injector = std::make_unique<InjectUpLinkDataSource>(
+        lpgbt,
+        InjectUpLinkDataSource::Source::PRBS7,
+        link
+      );
+    } else if (output_choice == 2) {
+      bool up = pftool::readline_bool("Count up (y) or down (n)? ", true);
+      if (up) {
+        injector = std::make_unique<InjectUpLinkDataSource>(
+          lpgbt,
+          InjectUpLinkDataSource::Source::BIN_CNTR_UP,
+          link
+        );
+      } else {
+        injector = std::make_unique<InjectUpLinkDataSource>(
+          lpgbt,
+          InjectUpLinkDataSource::Source::BIN_CNTR_DOWN,
+          link
+        );
+      }
+    } else if (output_choice == 3) {
+      uint32_t known_pattern = 0x12345678;
+      known_pattern =
+        pftool::readline_int("32b const pattern to use: ", known_pattern, true);
+      printf("const pattern: 0x%08x\n", known_pattern);
+      bool invert = pftool::readline_bool("Invert the constant pattern? ", false);
+      if (invert) {
+        injector = std::make_unique<InjectUpLinkDataSource>(
+          lpgbt,
+          InjectUpLinkDataSource::Source::CONST_PATTERN_INV,
+          link,
+          known_pattern
+        );
+      } else {
+        injector = std::make_unique<InjectUpLinkDataSource>(
+          lpgbt,
+          InjectUpLinkDataSource::Source::CONST_PATTERN,
+          link,
+          known_pattern
+        );
+      }
     }
   }
 
-  int link = pftool::readline_int("which link (-1 for all)? ", -1);
-  if (link < 0) {
-    for (int ilink{0}; ilink < 6; ilink++) {
-      conf.configure(ilink);
-    }
-  } else if (link > 5) {
-    pflib_log(error) << "invalid link index";
-    return;
-  } else {
-    conf.configure(link);
+
+  /*
+  static const uint16_t DATAPATH_REG = 0x142;
+  uint8_t orig_datapath = lpgbt.read(DATAPATH_REG);
+  pflib_log(info) << "original datapath: " << hex(orig_datapath);
+  uint8_t datapath = orig_datapath;
+  if (pftool::readline_bool("Bypass FEC coder? ", true)) {
+    datapath |= (1 << 0);
   }
+  if (pftool::readline_bool("Bypass scrambler? ", true)) {
+    datapath |= (1 << 1);
+  }
+  if (pftool::readline_bool("Bypass interleaver? ", true)) {
+    datapath |= (1 << 2);
+  }
+  lpgbt.write(DATAPATH_REG, datapath);
+  */
 
   pflib::Elinks& elinks = target->elinks();
   std::vector<std::vector<uint32_t>> spy(6);
@@ -152,14 +315,13 @@ void lpgbt_backend_check(Target* target) {
   if (link < 0) {
     for (int ilink{0}; ilink < 6; ilink++) {
       printf("Link %d:\n", ilink);
-      conf.check(spy[ilink]);
+      injector->check(spy[ilink]);
     }
   } else {
-    conf.check(spy[link]);
+    injector->check(spy[link]);
   }
 
-  // reset data source to all zeros for all groups (normal operation)
-  for (int i{0}; i < 4; i++) {
-    lpgbt.write(ULDATASOURCE1 + i, 0x00);
-  }
+  /*
+  lpgbt.write(DATAPATH_REG, orig_datapath);
+  */
 }
