1a08d7aa12e93fb3c55c339c8b2b808bae3cac8b
haseckew <haseckew@gmail.com>
2026-07-15 11:59:19 -0400

    fixed merge conflicts from previous merge

--- app/tool/algorithm/toa_vref_scan.cxx
+++ app/tool/algorithm/toa_vref_scan.cxx
@@ -9,60 +9,16 @@
 #include "pflib/utility/efficiency.h"
 #include "pflib/utility/string_format.h"
 #include "trim_toa_scan.h"
-<<<<<<< Updated upstream
-=======
-#include <algorithm>
 #include <chrono>
 #include <iomanip>
 #include <sstream>
-    >>>>>>> Stashed changes
 
-    namespace pflib::algorithm {
+
+  namespace pflib::algorithm {
 
   std::map<int, std::map<std::string, std::map<std::string, uint64_t>>>
   toa_vref_scan(Target * tgt, bool scan_all) {
     static auto the_log_{::pflib::logging::get("toa_vref_scan")};
-
-    // Reading out the trim_toa values for the first 5 channels to test,
-    // Cristina's suggestion, bug fix this later
-    //  if (!tgt->roc_ids().empty()) {
-    //    int first_roc = *(tgt->roc_ids().begin());
-    //    pflib::ROC& my_roc = tgt->roc(first_roc);
-
-    //    bool needs_trim_scan = false;
-    //    for (int i = 0; i < 5; i++) {
-    //      std::string ch_str = "CH_" + std::to_string(i);
-    //      auto channel_params = my_roc.getParameters(ch_str);
-
-    //      if (channel_params.find("TRIM_TOA") == channel_params.end()) {
-    //        needs_trim_scan = true;
-    //       break;
-    //      }
-    //    }
-
-    //    if (needs_trim_scan) {
-    //      pflib_log(info) << "TRIM_TOA values missing. Running
-    //      trim_toa_scan...";
-    //    trim_toa_scan(tgt, my_roc, first_roc);
-
-    //    for (int i = 0; i < 5; i++) {
-    //      std::string ch_str = "CH_" + std::to_string(i);
-    //      auto channel_params = my_roc.getParameters(ch_str);
-
-    //      auto it = channel_params.find("TRIM_TOA");
-    //      if (it != channel_params.end()) {
-    //          pflib_log(info) << "TRIM_TOA for channel " << i << ": " <<
-    //          it->second;
-    //      } else {
-    //          pflib_log(info) << "TRIM_TOA not set for channel " << i;
-    //      }
-    //    }
-    //  } else {
-    //      pflib_log(error) << "No ROC IDs found.";
-    //  }
-
-    /// do a run of 100 samples per toa_vref to measure the TOA
-    /// efficiency when looking at pedestal data
 
     static const std::size_t n_events = 100;
 
@@ -84,10 +40,6 @@
     DecodeAndBuffer buffer{n_events, tgt->nrocs() * 2};
 
     // create a .csv file to save efficiency and vref data for analysis
-<<<<<<< Updated upstream
-    std::ofstream csv_file("toa_vref_scan_data.csv");
-    csv_file << "TOA_VREF";
-=======
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
@@ -99,7 +51,6 @@
     std::ofstream csv_file(filename);
     pflib_log(info) << "Saving scan data to file: " << filename;
     csv_file << "TOA_VREF,ROC";
->>>>>>> Stashed changes
     for (int chan = 0; chan < 72; ++chan) {
       csv_file << "," << chan;
     }
