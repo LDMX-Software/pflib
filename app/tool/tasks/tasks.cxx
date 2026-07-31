29e2ac6c8b93fe4d912bf04f886e2cabdf77d8e5
PIERCE TAN <vbe9cs@virginia.edu>
2026-07-31 13:57:40 -0400

    small update

--- app/tool/tasks/tasks.cxx
+++ app/tool/tasks/tasks.cxx
@@ -30,6 +30,7 @@
 #include "trim_toa_scan.h"
 #include "vref_2d_scan.h"
 #include "vt50_scan.h"
+#include "led_bias_scan.h"
 
 namespace {
 auto menu_tasks =
@@ -77,7 +78,8 @@ auto menu_tasks =
         ->line("TRIM_TOA_SCAN",
                "calibrate TRIM_TOA parameters for each channel", trim_toa_scan)
         ->line("TOA_SCAN", "calibrate TRIM_TOA parameters for each channel",
-               toa_scan);
+               toa_scan)
+        ->line("LED_BIAS_SCAN", "Sweeps SiPM and LED DACs", led_bias_scan);
 
 auto menu_expert_tasks =
     menu_tasks->submenu("EXPERT", "low-level but complicated tasks")
