b7f234449f10bf52e442548a8d6668a816be7d54
tomeichlersmith <eichl008@umn.edu>
2026-08-04 10:50:34 -0500

    more safely acquire collection time

--- app/tool/trig/histo.cxx
+++ app/tool/trig/histo.cxx
@@ -13,11 +13,24 @@
 using pflib::utility::string_format;
 using the_clock = std::chrono::high_resolution_clock;
 using a_time_point = std::chrono::time_point<the_clock>;
+static std::optional<a_time_point> time_of_last_clear{};
+
+ENABLE_LOGGING();
+
+std::optional<double> get_collection_time(a_time_point now) {
+  using namespace std::literals;
+  if (time_of_last_clear) {
+    return (now - time_of_last_clear.value()) / 1.0s;
+  } else {
+    pflib_log(warn) << "There hasn't be a CLEAR recently,"
+                       " so we don't know the collection time and"
+                       " the histograms are probably saturated!";
+    return {};
+  }
+}
 
 void histo(const std::string& cmd, Target* tgt) {
-  using namespace std::literals;
   static FWHistoPool hist_pool{0};
-  static a_time_point time_of_last_clear{};
 
   if (cmd == "CLEAR") {
     hist_pool.clear();
@@ -35,15 +48,19 @@ void histo(const std::string& cmd, Target* tgt) {
     ihist = pftool::readline_int("Which histogram?", ihist);
     auto now = the_clock::now();
     std::array<uint32_t, 256> hist = hist_pool.read(ihist);
-    double collection_time = (now - time_of_last_clear) / 1.0s;
-    bool raw_counts =
-        pftool::readline_bool("Show raw counts (y) or rate (n)?", true);
+    std::optional<double> collection_time = get_collection_time(now);
+    pflib_log(info) << "accumulated histogram for " << collection_time.value_or(0) << "s";
+    bool raw_counts = true;
+    if (collection_time) {
+      raw_counts =
+          pftool::readline_bool("Show raw counts (y) or rate (n)?", raw_counts);
+    }
     for (std::size_t i{0}; i < hist.size(); i++) {
-      printf("%3d ", i);
+      printf("%3ld ", i);
       if (raw_counts) {
         printf("%u", hist[i]);
       } else {
-        printf("%0.4e", hist[i] / collection_time);
+        printf("%0.4e", hist[i] / collection_time.value());
       }
       printf("\n");
     }
@@ -55,7 +72,7 @@ void histo(const std::string& cmd, Target* tgt) {
       if (not file.is_open()) {
         PFEXCEPTION_RAISE("FileOpen", "Unable to open " + path);
       }
-      file << FWHistoPool::to_json(hist, ihist, collection_time);
+      file << FWHistoPool::to_json(hist, ihist, collection_time.value_or(0));
     }
   }
 
@@ -69,21 +86,24 @@ void histo(const std::string& cmd, Target* tgt) {
       hists[ihist] = hist_pool.read(ihist);
     }
 
-    double collection_time = (now - time_of_last_clear) / 1.0s;
-    std::cout << collection_time << std::endl;
+    std::optional<double> collection_time = get_collection_time(now);
+    pflib_log(info) << "accumulated histogram for " << collection_time.value_or(0) << "s";
 
     if (pftool::readline_bool("Show histograms in terminal?", true)) {
-      bool raw_counts =
-          pftool::readline_bool("Show raw counts (y) or rate (n)?", true);
+      bool raw_counts = true;
+      if (collection_time) {
+        raw_counts =
+          pftool::readline_bool("Show raw counts (y) or rate (n)?", raw_counts);
+      }
       printf("bin : %10u %10u %10u %10u %10u %10u %10u %10u\n", 0, 1, 2, 3, 4,
              5, 6, 7);
       for (std::size_t i{0}; i < hists[0].size(); i++) {
-        printf("%3d :", i);
+        printf("%3ld :", i);
         for (int ihist{0}; ihist < hists.size(); ihist++) {
           if (raw_counts) {
             printf(" %10u", hists[ihist][i]);
           } else {
-            printf(" %10.4e", hists[ihist][i] / collection_time);
+            printf(" %10.4e", hists[ihist][i] / collection_time.value());
           }
         }
         printf("\n");
@@ -97,7 +117,7 @@ void histo(const std::string& cmd, Target* tgt) {
       if (not file.is_open()) {
         PFEXCEPTION_RAISE("FileOpen", "Unable to open " + path);
       }
-      file << FWHistoPool::to_json(hists, collection_time);
+      file << FWHistoPool::to_json(hists, collection_time.value_or(0));
     }
   }
 }
