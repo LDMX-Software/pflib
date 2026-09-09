de126c3f6a0c768905a451fe860cf9eb629265a9
tomeichlersmith <eichl008@umn.edu>
2026-09-09 18:07:49 +0000

    printout 200MHz clock monitor as well

--- src/pflib/zcu/zcu_optolink.cxx
+++ src/pflib/zcu/zcu_optolink.cxx
@@ -233,9 +233,10 @@ std::map<std::string, uint32_t> ZCUOptoLink::opto_rates() {
     // is not singleLPGBT and not daq, so is trigger link
     static constexpr int RATES_OFFSET = (0xC00 + 4 * 0x20) / 4;
     // same names but ordered differently in registers
-    static const std::array<const char*, 6> cnames = {
+    static const std::array<const char*, 7> cnames = {
         "AXI_CLK",   "CLOCK_40",   "LINK_CLOCK",
-        "LINK_WORD", "LINK_ERROR", "LINK_FECERR"};
+        "LINK_WORD", "LINK_ERROR", "LINK_FECERR",
+        "CLK_200"};
     for (int i{0}; i < cnames.size(); i++) {
       uint32_t val = coder_.read(RATES_OFFSET + i);
       if (i == 5) {
